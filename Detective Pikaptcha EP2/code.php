16d6b855c649791af82daa4ab14f06748b9ff79a
cedricdd <c-d-d@hotmail.fr>
2022-06-01 19:49:29 +0200

    Update code.php

--- Detective Pikaptcha EP2/code.php
+++ Detective Pikaptcha EP2/code.php
@@ -32,8 +32,6 @@ for ($i = 0; $i < $height; $i++) {
 //Replace starting postion
 $map[$py][$px] = 0;
 
-$move = ["UP" => [-1, 0], "RIGHT" => [0, 1], "DOWN" => [1, 0], "LEFT" => [0, -1]];
-
 if($side == "L") {
     $directions = [
         "RIGHT" => ["UP" => [-1, 0], "RIGHT" => [0, 1], "DOWN" => [1, 0], "LEFT" => [0, -1]],
