4330521d150c510b534b9aba8a668ffecc9bf1b5
cedricdd <c-d-d@hotmail.fr>
2022-08-05 17:17:01 +0200

    Update code.php

--- Next car license plate ?/code.php
+++ Next car license plate ?/code.php
@@ -6,10 +6,14 @@ $plate = stream_get_line(STDIN, 9 + 1, "\n");
 
 list($left, $center, $right) = explode("-", $plate);
 
-fscanf(STDIN, "%d", $n);
+$center += intval(fgets(STDIN));
 
-$value = $ar[$right[1]] + ($ar[$right[0]] * 26) + ($ar[$left[1]] * 26 ** 2) + ($ar[$left[0]] * 26 ** 3) + intdiv($center + $n, 999);
-$center = ($center + $n) % 999;
+$value = $ar[$right[1]] + ($ar[$right[0]] * 26) + ($ar[$left[1]] * 26 ** 2) + ($ar[$left[0]] * 26 ** 3);
+
+if($center > 999) {
+    $value += intdiv($center, 999);
+    $center = $center % 999;
+}
 
 $q = intdiv($value, 26 ** 3);
 $left[0] = $a[$q];
