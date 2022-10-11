0747983b59fad93d4fd91df85dd691c147bf52af
cedricdd <c-d-d@hotmail.fr>
2022-10-11 17:51:03 +0200

    Update code.php

--- Blackjack solver/code.php
+++ Blackjack solver/code.php
@@ -2,53 +2,25 @@
 
 const VALUES = [2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 'J' => 10, 'Q' => 10, 'K' => 10];
 
-$bank = 0;
-$bankCards = fgetcsv(STDIN, 0, " ");
-//We have to check the aces last
-usort($bankCards, function($a, $b) {
-    return ($a == "A" ? 1 : 0) <=> ($b == "A" ? 1 : 0);
-});
+function getScore(array $cards): array {
+    //We have to check the aces last
+    usort($cards, function($a, $b) { return ($a == "A" ? 1 : 0) <=> ($b == "A" ? 1 : 0); });
 
-//Bank score
-foreach($bankCards as $card) {
-    if($card === "A") {
-        $bank += ($bank + 11 > 21) ? 1 : 11;
-    } else $bank += VALUES[$card];
+    $score = 0;
+
+    foreach($cards as $card) {
+        if($card === "A") $score += ($score + 11 > 21) ? 1 : 11;
+        else $score += VALUES[$card];
+    }
+
+    return [$score, $score == 21 && count($cards) == 2];
 }
 
-$player = 0;
-$playerCards = fgetcsv(STDIN, 0, " ");
-//We have to check the aces last
-usort($playerCards, function($a, $b) {
-    return ($a == "A" ? 1 : 0) <=> ($b == "A" ? 1 : 0);
-});
+[$bankScore, $bankBlackjack] = getScore(explode(" ", trim(fgets(STDIN))));
+[$playerScore, $playerBlackjack] = getScore(explode(" ", trim(fgets(STDIN))));
 
-//Player score
-foreach($playerCards as $card) {
-    if($card === "A") {
-        $player += ($player + 11 > 21) ? 1 : 11;
-    } else $player += VALUES[$card];
-}
-
-if($player > 21) {
-    if($bank > 21) echo "Draw\n";
-    else echo "Bank\n";
-} elseif($player < 21) {
-    if($bank == $player) echo "Draw\n";
-    elseif($bank > $player && $bank <= 21) echo "Bank\n";
-    else echo "Player";
-} else {
-    if($bank == 21) {
-        if(count($playerCards) == 2) {
-            if(count($bankCards) == 2) echo "Draw\n";
-            else echo "Blackjack!\n"; 
-        } else {
-            if(count($bankCards) == 2) echo "Blackjack!\n"; 
-            else echo "Draw\n";
-        }
-    } else {
-        if(count($playerCards) == 2) echo "Blackjack!\n";
-        else "Player\n";
-    } 
-}
+if($playerBlackjack && !$bankBlackjack) echo "Blackjack!" . PHP_EOL;
+elseif($playerScore > 21 || ($bankScore > $playerScore && $bankScore <= 21)) echo "Bank" . PHP_EOL;
+elseif($playerScore == $bankScore) echo "Draw" . PHP_EOL;
+else echo "Player" . PHP_EOL;
 ?>
