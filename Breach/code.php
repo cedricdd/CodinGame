67dc31236c6dff80943ab483406564a6c77cef06
cedricdd <c-d-d@hotmail.fr>
2022-11-08 17:47:47 +0100

    Update code.php

--- Breach/code.php
+++ Breach/code.php
@@ -1,5 +1,20 @@
 <?php
 
+const periodicTable = [
+    1 => "H", 2 => "He", 3 => "Li", 4 => "Be", 5 => "B", 6 => "C", 7 => "N", 8 => "O", 9 => "F", 10 => "Fe",
+    11 => "Na", 12 => "Mg", 13 => "Al", 14 => "Si", 15 => "P", 16 => "S", 17 => "Cl", 18 => "Ar", 19 => "K", 20 => "Ca",
+    21 => "Sc", 22 => "Ti", 23 => "V", 	24 => "Cr", 25 => "Mn", 26 => "Fe", 27 => "Co", 28 => "Ni", 29 => "Cu", 30 => "Zn",
+    31 => "Ga", 32 => "Ge", 33 => "As", 34 => "Se", 35 => "Br", 36 => "Kr", 37 => "Rb", 38 => "Sr", 39 => "Y", 40 => "Zr",
+    41 => "Nb", 42 => "Mo", 43 => "Tc", 44 => "Ru", 45 => "Rh", 46 => "Pd", 47 => "Ag", 48 => "Cd", 49 => "In", 50 => "Sn",
+    51 => "Sb", 52 => "Te", 53 => "I",  54 => "Xe", 55 => "Cs", 56 => "Ba", 57 => "La", 58 => "Ce", 59 => "Pr", 60 => "Nd",
+    61 => "Pm", 62 => "Sm", 63 => "Eu", 64 => "Gd", 65 => "Tb", 66 => "Dy", 67 => "Ho", 68 => "Er", 69 => "Tm", 70 => "Yb", 
+    71 => "Lu", 72 => "Hf", 73 => "Ta", 74 => "W",  75 => "Re", 76 => "Os", 77 => "Ir", 78 => "Pt", 79 => "Au", 80 => "Hg", 
+    81 => "Tl", 82 => "Pb", 83 => "Bi", 84 => "Po", 85 => "At", 86 => "Rn", 87 => "Fr", 88 => "Ra", 89 => "Ac", 90 => "Th",
+    91 => "Pa", 92 => "U" ,	93 => "Np", 94 => "Pu", 95 => "Am", 96 => "Cm", 97 => "Bk", 98 => "Cf", 99 => "Es", 100 => "Fm", 
+    101 => "Md", 102 => "No", 103 => "Lr", 104 => "Rf", 105 => "Db", 106 => "Sg", 107 => "Bh", 108 => "Hs", 109 => "Mt", 110 => "Ds", 
+    111 => "Rg", 112 => "Cn", 113 => "Nh", 114 => "Fl", 115 => "Mc", 116 => "Lv", 117 => "Ts", 118 => "Og"
+];
+
 // game loop
 while (TRUE)
 {
@@ -53,17 +68,17 @@ while (TRUE)
     elseif($type == "rs_f") {
         echo (ord($lines[0][0]) - 97) . PHP_EOL;
     }
-
+    //Symbol of the chemical element by it's atomic number
     elseif($type == "gs_m") {
-        [, $ascii] = explode(": ", $lines[0]);
-        echo chr($ascii) . chr($ascii - 1) . PHP_EOL;
+        [, $value] = explode(": ", $lines[0]);
+
+        echo periodicTable[$value] . PHP_EOL;
     }
+    //Atomic number of a chemical element
     elseif($type == "ss_m") {
-        $alphabet = array_flip(array_merge(range('Z', 'A')));
+        $table = array_flip(periodicTable);
 
-        //error_log(var_export($alphabet, true)); 
-
-        echo $alphabet[$lines[0]] . PHP_EOL;
+        echo $table[$lines[0]] . PHP_EOL;
     }
     //Number is given in "alarm clock" format
     elseif($type == "ss_asc") {
@@ -99,15 +114,12 @@ while (TRUE)
     }
     //Output a index to change the color of the index, if the color is correct you get a green "g", otherwise you get a red "r" 
     elseif($type == "ss_con") {
-        $a = ["R" => 0, "r" => 1, "G" => 2, "g" => 3, "B" => 4, "b" => 5, "y" => 6, "o" => 7, "p" => 8];
-        $test = array_map(function($position) {
+        $inputs = array_map(function($position) {
             return preg_split("//u", $position, -1, PREG_SPLIT_NO_EMPTY);
         }, explode("...", $lines[0]));
 
-        //error_log(var_export($test, true)); 
-
         for($i = 0; $i < 6; ++$i) {
-            if($test[$i][4] == "r") {
+            if($inputs[$i][4] == "r") {
                 echo ($i + 1) . PHP_EOL;
                 continue 2;
             }
@@ -115,6 +127,20 @@ while (TRUE)
 
         //We found all the colors, validate, no idea why you output 0 for that
         echo 0 . PHP_EOL;
+    }
+    elseif($type == "ss_colv") {
+        $inputs = array_map(function($position) {
+            return preg_split("//u", $position, -1, PREG_SPLIT_NO_EMPTY);
+        }, explode("-", $lines[0]));
+
+        error_log(var_export($inputs, true));
+
+        foreach($inputs as $list) {
+            if(count($list) > 2) {
+                echo $list[1] . PHP_EOL;
+                break;
+            }
+        }
     }
 }
 ?>
