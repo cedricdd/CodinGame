a2ae9b830a27d70573d7556c01b7c5f1038ec8d8
cedricdd <c-d-d@hotmail.fr>
2023-01-23 21:32:31 +0100

    Update code.php

--- A Bunny and Carrots/code.php
+++ A Bunny and Carrots/code.php
@@ -1,36 +1,26 @@
 <?php
 
-function getPerimeter(array $field): int {
-    global $width, $height;
-
-    $perimeter = 0;
-
-    for($y = 0; $y < $height; ++$y) {
-        for($x = 0; $x < $width; ++$x) {
-            if($field[$y][$x] != "C") continue; //Skip if it's not a carrot
-
-            //Check the 4 adjacents positions
-            foreach([[0, 1], [0, -1], [1, 0], [-1, 0]] as [$xm, $ym]) {
-                $checkX = $x + $xm;
-                $checkY = $y + $ym;
-
-                //If we are outside of the field of there's no carrot we increase perimeter
-                if($checkX < 0 || $checkX >= $width || $checkY < 0 || $checkY >= $height || $field[$checkY][$checkX] == "X") ++$perimeter;
-            }
-        }
-    }
-
-    return $perimeter;
-}
-
 fscanf(STDIN, "%d %d", $height, $width);
 fscanf(STDIN, "%d", $t);
 
 $field = array_fill(0, $height, array_fill(0, $width, "C"));
-$cols = array_fill(0, $width, $height);
+$cols = array_fill(0, $width, $height - 1);
+$perimeter = ($width + $height) * 2;
 
 foreach(explode(" ", trim(fgets(STDIN))) as $value) {
     $value -= 1; //Input values are not 0 based
-    $field[--$cols[$value]][$value] = "X"; //Remove the carrot eaten by the rabbit
-    echo getPerimeter($field) . PHP_EOL;
+
+    //Check the 4 adjacents positions
+    foreach([[0, 1], [0, -1], [1, 0], [-1, 0]] as [$xm, $ym]) {
+        $checkX = $value + $xm;
+        $checkY = $cols[$value] + $ym;
+
+        //If we are outside of the field of there's no carrot perimeter decrease, otherwise it increase
+        if($checkX < 0 || $checkX >= $width || $checkY < 0 || $checkY >= $height || $field[$checkY][$checkX] == "X") --$perimeter;
+        else ++$perimeter;
+    }
+    
+    $field[$cols[$value]--][$value] = "X"; //Update field and the Y position for the column
+
+    echo $perimeter . PHP_EOL;
 }
