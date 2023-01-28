c031f87b3c818c41b851bcc20c52da3f7145e58a
cedricdd <c-d-d@hotmail.fr>
2023-01-28 23:58:34 +0100

    Update code.php

--- Logically reasonable inequalities/code.php
+++ Logically reasonable inequalities/code.php
@@ -1,24 +1,22 @@
 <?php
 
-function searchContradiction(string $letter, string $reference): bool {
-    global $inequalities;
-
-    if($letter == $reference) return true;
-
-    $result = false;
-
-    foreach($inequalities[$letter] ?? [] as $letter2) $result |= searchContradiction($letter2, $reference);
-
-    return $result;
-}
+$alphabet = array_combine(range("A", "Z"), array_fill(0, 26, [[], []]));
 
 fscanf(STDIN, "%d", $n);
 for ($i = 0; $i < $n; $i++) {
     [$a, $b] = explode(" > ", trim(fgets(STDIN)));
 
-    if(searchContradiction($b, $a)) exit("contradiction");
+    //We know for sure that B is bigger than A
+    if(isset($alphabet[$b][0][$a])) exit("contradiction");
 
-    $inequalities[$a][] = $b;
+    $alphabet[$a][0][$b] = 1; //A is > than B
+    $alphabet[$b][1][$a] = 1; //B is < than A 
+
+    //Everything bigger than A is also bigger than B
+    foreach($alphabet[$a][1] ?? [] as $letter => $filler) {
+        $alphabet[$letter][0][$b] = 1; 
+        $alphabet[$b][1][$letter] = 1; 
+    }
 }
 
 echo "consistent" . PHP_EOL;
