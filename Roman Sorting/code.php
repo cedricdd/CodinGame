276f35dc2862564e58ba3f54ee3dc86d54b0e00c
cedricdd <c-d-d@hotmail.fr>
2023-02-14 16:59:17 +0100

    Update code.php

--- Roman Sorting/code.php
+++ Roman Sorting/code.php
@@ -1,6 +1,6 @@
 <?php
 
-const ROMAN = [1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", "40" => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"];
+const ROMAN = [1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"];
 
 //Convert an integer to it's Roman-numeral representation
 function convertToRoman(int $n): string {
