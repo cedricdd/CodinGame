74c59f073e89f4afc291c45e36a39266ec66f90c
cedricdd <c-d-d@hotmail.fr>
2023-02-17 21:24:07 +0100

    Update code.php

--- Conditional probabilities/code.php
+++ Conditional probabilities/code.php
@@ -1,24 +1,5 @@
 <?php
 
-$P = [
-    "A" => null,
-    "A AND B" => null,
-    "A AND NOT B" => null,
-    "A GIVEN B" => null,
-    "A GIVEN NOT B" => null,
-    "B" => null,
-    "B GIVEN A" => null,
-    "B GIVEN NOT A" => null,
-    "NOT A" => null,
-    "NOT A AND B" => null,
-    "NOT A AND NOT B" => null,
-    "NOT A GIVEN B" => null,
-    "NOT A GIVEN NOT B" => null,
-    "NOT B" => null,
-    "NOT B GIVEN A" => null,
-    "NOT B GIVEN NOT A" => null,
-];
-
 function GCD(int $a, int $b): int {
     return $b ? GCD($b, $a % $b) : $a;
 }
@@ -28,10 +9,13 @@ function LCM(int $a, int $b): int {
 }
 
 function simplifyFractions(int $a, int $b): array {
-    //Probability is < 0 or > 1
-    if(($a / $b) < 0 || ($a / $b) > 1) die("IMPOSSIBLE");
+    $gcd = GCD(abs($a), abs($b));
 
-    $gcd = GCD($a, $b);
+    //denominator is negative move the negative to numerator
+    if($b < 0) {
+        $a *= -1;
+        $b *= -1;
+    } 
 
     return [$a / $gcd, $b / $gcd];
 }
@@ -58,127 +42,95 @@ function mulFractions(array $a, array $b): array {
     return simplifyFractions($a[0] * $b[0], $a[1] * $b[1]);
 }
 
-for($i = 0; $i < 3; ++$i) {
-    preg_match("/([A-Z ]+) = ([0-9]+)\/([0-9]+)/", trim(fgets(STDIN)), $matches);
-    $P[$matches[1]] = [$matches[2], $matches[3]];
+//Return the list of variable => expression in the string
+function parse(string $input): array {
+
+    $name = "";
+    $value = "";
+    $readingName = 1;
+    $readingValue = 0;
+    $count = 0;
+    $info = [];
+
+    foreach(str_split($input . ";") as $c) {
+
+        switch($c) {
+            //Expression can have nested parantheses, we want the full expression for the variable
+            case "(": ++$count; break;
+            case ")": --$count; break;
+            case "=":
+                //We finished reading a variable name
+                if($readingName == 1) {
+                    $readingName = 0;
+                    $readingValue = 1;
+                    continue 2;
+                }
+            case ";":
+                //We finished reading an expression
+                if($count == 0) {
+                    $readingValue = 0;
+                    $readingName = 1;
+
+                    $info[trim($name, "'")] = trim($value, "'");
+                    [$name, $value] = ["", ""];
+        
+                    continue 2;
+                }
+        }
+
+        if($readingName) $name .= $c;
+        else $value .= $c;
+    }
+
+    return $info;
 }
 
-//Until we can't find anymore values
-do {
-    $found = count(array_filter($P));
+function getValue($value): array {
+    global $variables;
 
-    foreach($P as $name => $values) {
-        if($values != null) continue; //We already know this value
+    //Value is a constant
+    if(is_numeric($value)) $result = [intval($value), 1];
+    //Value is a varirable
+    elseif(preg_match("/^[a-zA-Z0-9]*$/", $value)) {
+        $result = getValue($variables[$value]);
+    } //Value is an operation or a fraction
+    else {
+        $expression = parse(trim($value, ")("));
 
-        switch($name) {
-            case "A":
-                //A = 1 - NOT A
-                if($P["NOT A"] != null) $P[$name] = subFractions([1, 1], $P["NOT A"]);
-                //A = A AND B / B GIVEN A
-                elseif($P["A AND B"] != null && $P["B GIVEN A"] != null) $P[$name] = divFractions($P["A AND B"], $P["B GIVEN A"]);
-                //A = A AND B + A AND NOT B
-                elseif($P["A AND B"] != null && $P["A AND NOT B"] != null) $P[$name] = addFractions($P["A AND B"], $P["A AND NOT B"]);
-                break;
-            case "A AND B":
-                //A AND B = A - A AND NOT B
-                if($P["A AND NOT B"] != null && $P["A"] != null) $P[$name] = subFractions($P["A"], $P["A AND NOT B"]);
-                //A AND B = B * A GIVEN B
-                elseif($P["B"] != null && $P["A GIVEN B"] != null) $P[$name] = mulFractions($P["B"], $P["A GIVEN B"]);
-                //A AND B = A * B GIVEN A
-                elseif($P["A"] != null && $P["B GIVEN A"] != null) $P[$name] = mulFractions($P["A"], $P["B GIVEN A"]);
-                //A AND B = B - NOT A AND B
-                elseif($P["B"] != null && $P["NOT A AND B"] != null) $P[$name] = subFractions($P["B"], $P["NOT A AND B"]);
-                break;
-            case "A AND NOT B":
-                //A AND NOT B = A - A AND B
-                if($P["A AND B"] != null && $P["A"] != null) $P[$name] = subFractions($P["A"], $P["A AND B"]);
-                //A AND NOT B = A GIVEN NOT B * NOT B
-                elseif($P["A GIVEN NOT B"] != null && $P["NOT B"] != null) $P[$name] = mulFractions($P["A GIVEN NOT B"], $P["NOT B"]);
-                //A AND NOT B = NOT B GIVEN A * A
-                elseif($P["NOT B GIVEN A"] != null && $P["A"] != null) $P[$name] = mulFractions($P["NOT B GIVEN A"], $P["A"]);
-                break;
-            case "A GIVEN B":
-                //A GIVEN B = "A AND B / B
-                if($P["B"] != null && $P["A AND B"] != null) $P[$name] = divFractions($P["A AND B"], $P["B"]);
-                //A GIVEN B = 1 - NOT A GIVEN B
-                elseif($P["NOT A GIVEN B"] != null) $P[$name] = subFractions([1, 1], $P["NOT A GIVEN B"]);
-                break;
-            case "A GIVEN NOT B":
-                //A GIVEN NOT B = A AND NOT B / NOT B
-                if($P["A AND NOT B"] != null && $P["NOT B"] != null) $P[$name] = divFractions($P["A AND NOT B"], $P["NOT B"]);
-                //A GIVEN NOT B = 1 - NOT A GIVEN NOT B
-                elseif($P["NOT A GIVEN NOT B"] != null) $P[$name] = subFractions([1, 1], $P["NOT A GIVEN NOT B"]);
-                break;
-            case "B":
-                //B = 1 - NOT B
-                if($P["NOT B"] != null) $P[$name] = subFractions([1, 1], $P["NOT B"]);
-                //B = A AND B / A GIVEN B
-                elseif($P["A AND B"] != null && $P["A GIVEN B"] != null) $P[$name] = divFractions($P["A AND B"], $P["A GIVEN B"]);
-                //B = A AND B + NOT A AND B
-                elseif($P["A AND B"] != null && $P["NOT A AND B"] != null) $P[$name] = addFractions($P["A AND B"], $P["NOT A AND B"]);
-                break;
-            case "B GIVEN A":
-                //B GIVEN A = A AND B / A
-                if($P["A"] != null && $P["A AND B"] != null) $P[$name] = divFractions($P["A AND B"], $P["A"]);
-                //B GIVEN A = 1 - NOT B GIVEN A
-                elseif($P["NOT B GIVEN A"] != null) $P[$name] = subFractions([1, 1], $P["NOT B GIVEN A"]);
-                break;
-            case "B GIVEN NOT A":
-                //B GIVEN NOT A = 1 - NOT B GIVEN NOT A
-                if($P["NOT B GIVEN NOT A"] != null) $P[$name] = subFractions([1, 1], $P["NOT B GIVEN NOT A"]);
-                //B GIVEN NOT A = NOT A AND B / NOT A
-                elseif($P["NOT A AND B"] != null && $P["NOT A"] != null) $P[$name] = divFractions($P["NOT A AND B"], $P["NOT A"]);
-                break;
-            case "NOT A":
-                //NOT A = 1 - A
-                if($P["A"] != null) $P[$name] = subFractions([1, 1], $P["A"]);
-                //NOT A = NOT A AND NOT B + NOT A AND B
-                elseif($P["NOT A AND NOT B"] != null && $P["NOT A AND B"] != null) $P[$name] = addFractions($P["NOT A AND NOT B"], $P["NOT A AND B"]);
-                //NOT A = NOT A AND B / B GIVEN NOT A
-                elseif($P["NOT A AND B"] != null && $P["B GIVEN NOT A"] != null) $P[$name] = divFractions($P["NOT A AND B"], $P["B GIVEN NOT A"]);
-                break;
-            case "NOT A AND B":
-                //NOT A AND B = B - A AND B
-                if($P["B"] != null && $P["A AND B"] != null) $P[$name] = subFractions($P["B"], $P["A AND B"]);
-                //NOT A AND B = B GIVEN NOT A * NOT A
-                elseif($P["B GIVEN NOT A"] != null && $P["NOT A"] != null) $P[$name] = mulFractions($P["B GIVEN NOT A"], $P["NOT A"]);
-                //NOT A AND B = NOT A - NOT A AND NOT B
-                elseif($P["NOT A"] != null && $P["NOT A AND NOT B"] != null) $P[$name] = subFractions($P["NOT A"], $P["NOT A AND NOT B"]);
-                break;
-            case "NOT A AND NOT B":
-                //NOT A AND NOT B = NOT A - NOT A AND B
-                if($P["NOT A"] != null && $P["NOT A AND B"] != null) $P[$name] = subFractions($P["NOT A"], $P["NOT A AND B"]);
-                break;
-            case "NOT A GIVEN B":
-                //NOT A GIVEN B = 1 - A GIVEN B
-                if($P["A GIVEN B"] != null) $P[$name] = subFractions([1, 1], $P["A GIVEN B"]);
-                break;
-            case "NOT A GIVEN NOT B":
-                //NOT A GIVEN NOT B = 1 - A GIVEN NOT B
-                if($P["A GIVEN NOT B"] != null) $P[$name] = subFractions([1, 1], $P["A GIVEN NOT B"]);
-                break;
-            case "NOT B":
-                //NOT B = 1 - B
-                if($P["B"] != null) $P[$name] = subFractions([1, 1], $P["B"]);
-                //NOT B = A AND NOT B / A GIVEN NOT B
-                elseif($P["A AND NOT B"] != null && $P["A GIVEN NOT B"] != null) $P[$name] = divFractions($P["A AND NOT B"], $P["A GIVEN NOT B"]);
-                break;
-            case "NOT B GIVEN A":
-                //NOT B GIVEN A = 1 - B GIVEN A
-                if($P["B GIVEN A"] != null) $P[$name] = subFractions([1, 1], $P["B GIVEN A"]);
-                //NOT B GIVEN A = A AND NOT B / A
-                elseif($P["A"] != null && $P["A AND NOT B"] != null) $P[$name] = divFractions($P["A AND NOT B"], $P["A"]);
-                break;
-            case "NOT B GIVEN NOT A":
-                //NOT B GIVEN NOT A = 1 - B GIVEN NOT A
-                if($P["B GIVEN NOT A"] != null) $P[$name] = subFractions([1, 1], $P["B GIVEN NOT A"]);
-                break;
+        //Operation +-*/
+        if(count($expression) == 3) {
+
+            $num1 = getValue($expression["num1"]);
+            $num2 = getValue($expression["num2"]);
+            
+            switch($expression["operator"]) {
+                case "+": $result = addFractions($num1, $num2); break;
+                case "-": $result = subFractions($num1, $num2); break;
+                case "/": $result = divFractions($num1, $num2); break;
+                case "*": $result = mulFractions($num1, $num2); break;
+            }
+        } //Fraction
+        else {
+            $result = divFractions(getValue($expression["numerator"]), getValue($expression["denominator"]));
         }
     }
-} while($found != count(array_filter($P)));
 
-foreach(array_filter($P) as $name => [$a, $b]) {
-    echo "$name = $a/$b" . PHP_EOL;
+    return $result;
 }
 
-?>
+$CGX = "";
+
+fscanf(STDIN, "%d", $n);
+for ($i = 0; $i < $n; $i++){
+    $CGX .= trim(fgets(STDIN));
+}
+
+//Get the value 'result' & 'vars' if it exist
+preg_match("/^\('result'=([^;]+)(?:;'vars'=\((.*)\))?\)$/", $CGX, $matches);
+
+$variables = parse($matches[2] ?? "");
+
+$result = getValue(trim($matches[1], "'"));
+
+//Print as integer when possible othewise as fraction
+echo ($result[1] == 1 ? $result[0] : ($result[0] . "/" . $result[1])) . PHP_EOL;
