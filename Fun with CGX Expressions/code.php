5fe12ac9302897a95a9c61574c67a1a71162c570
cedricdd <c-d-d@hotmail.fr>
2023-02-23 15:34:18 +0100

    Update code.php

--- Fun with CGX Expressions/code.php
+++ Fun with CGX Expressions/code.php
@@ -1,52 +1,45 @@
 <?php
 
-function LCM(string $a, string $b): string {
-    return bcdiv(bcmul($a, $b), GCD($a, $b));
+function GCD(int $a, int $b): int {
+    return $b ? GCD($b, $a % $b) : $a;
 }
 
-function GCD(string $a, string $b): string {
-    return $b ? GCD($b, bcmod($a, $b)) : $a;
+function LCM(int $a, int $b): int {
+    return $a * $b / GCD($a, $b);
 }
 
-function simplifyFractions(string $a, string $b): array {
+function simplifyFractions(int $a, int $b): array {
+    $gcd = GCD(abs($a), abs($b));
 
-    $modifier = 1;
+    //denominator is negative move the negative to numerator
+    if($b < 0) {
+        $a *= -1;
+        $b *= -1;
+    } 
 
-    if(bccomp($a, 0) == -1) {
-        $a = bcmul($a, "-1");
-        $modifier *= -1;
-    }
-    if(bccomp($b, 0) == -1) {
-        $b = bcmul($b, "-1");
-        $modifier *= -1;
-    }
-
-    $gcd = GCD($a, $b);
-
-    return [bcmul(bcdiv($a, $gcd), $modifier), bcdiv($b, $gcd)];
+    return [$a / $gcd, $b / $gcd];
 }
 
 function subFractions(array $a, array $b): array {
-
     $divisor = LCM($a[1], $b[1]);
-    $dividend = bcsub(bcmul($a[0], bcdiv($divisor, $a[1])), bcmul($b[0], bcdiv($divisor, $b[1])));
+    $dividend = ($a[0] * ($divisor / $a[1])) - ($b[0] * ($divisor / $b[1]));
 
     return simplifyFractions($dividend, $divisor);
 }
 
 function addFractions(array $a, array $b): array {
     $divisor = LCM($a[1], $b[1]);
-    $dividend = bcadd(bcmul($a[0], bcdiv($divisor, $a[1])), bcmul($b[0], bcdiv($divisor, $b[1])));
+    $dividend = ($a[0] * ($divisor / $a[1])) + ($b[0] * ($divisor / $b[1]));
 
     return simplifyFractions($dividend, $divisor);
 }
 
 function divFractions(array $a, array $b): array {
-    return simplifyFractions(bcmul($a[0], $b[1]), bcmul($a[1], $b[0]));
+    return simplifyFractions($a[0] * $b[1], $a[1] * $b[0]);
 }
 
 function mulFractions(array $a, array $b): array {
-    return simplifyFractions(bcmul($a[0], $b[0]), bcmul($a[1], $b[1]));
+    return simplifyFractions($a[0] * $b[0], $a[1] * $b[1]);
 }
 
 //Return the list of variable => expression in the string
