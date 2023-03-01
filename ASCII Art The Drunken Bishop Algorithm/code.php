e803f4559ecbe944042cb54e1b6af2d27c451598
cedricdd <c-d-d@hotmail.fr>
2023-03-01 21:28:36 +0100

    Update code.php

--- ASCII Art The Drunken Bishop Algorithm/code.php
+++ ASCII Art The Drunken Bishop Algorithm/code.php
@@ -27,11 +27,6 @@ foreach(explode(":", trim(fgets(STDIN))) as $hexa) {
     foreach(array_reverse(str_split($binary, 2)) as $direction) {
         [$xm, $ym] = MOVES[$direction];
 
-        if($x == 1 && $y == 1 && $direction == "00") exit("1 $direction");
-        if($x == 17 && $y == 1 && $direction == "01") exit("2 $direction");
-        if($x == 1 && $y == 9 && $direction == "10") exit("3 $direction");
-        if($x == 17 && $y == 9 && $direction == "11") exit("4 $direction");
-
         $x = min(17, max(1, $x + $xm));
         $y = min(9, max(1, $y + $ym));
         
@@ -39,12 +34,14 @@ foreach(explode(":", trim(fgets(STDIN))) as $hexa) {
     }
 }
 
+//Add the symbols to the answer
 foreach($visited as $y2 => $line) {
     foreach($line as $x2 => $count) {
         $answer[$y2][$x2] = SYMBOLS[$count % 15];
     }
 }
 
+//Add starting & end positions
 $answer[5][9] = "S";
 $answer[$y][$x] = "E"; 
 
