783f681cc9f005785ee3be2079d8c89da0b47691
cedricdd <c-d-d@hotmail.fr>
2023-04-21 17:08:34 +0200

    Update code.php

--- Grid climbing/code.php
+++ Grid climbing/code.php
@@ -2,53 +2,46 @@
 
 $start = microtime(1);
 
+//Get the cost after the jump from point A to point B
+function getJumpCost(int $xa, int $ya, int $xb, int $yb): int {
+    global $costs, $map;
+
+    return $costs[max(abs($xa - $xb), abs($ya - $yb)) - 1] + $map[$yb][$xb];
+}
+
 fscanf(STDIN, "%d", $n);
 
 $lastPosition = $n * $n - 1;
-$costs = explode(" ", stream_get_line(STDIN, 256 + 1, "\n"));
+$costs = explode(" ", trim(fgets(STDIN)));
 
 for ($i = 0; $i < $n; $i++) {
-    $map[] = stream_get_line(STDIN, $n + 1, "\n");
+    $map[] = trim(fgets(STDIN));
 }
 
-//Pre-compute the jumps cost, jumping between position ia (xa;ya) && position ib (xb;yb)
-for($ya = 0; $ya < $n; ++$ya) {
-    for($xa = 0; $xa < $n; ++$xa) {
-        $ia = $ya * $n + $xa;
-        for($yb = 0; $yb < $n; ++$yb) {
-            for($xb = 0; $xb < $n; ++$xb) {
-                $ib = $yb * $n + $xb;
-                if(isset($jumps[$ia][$ib]) || $ia == $ib) continue;
+//Initial values are the direct jump from position 0
+for($y = 0; $y < $n; ++$y) {
+    for($x = 0; $x < $n; ++$x) {
+        if($x == 0 && $y == 0) continue;
 
-                $jumpCost = $costs[max(abs($xa - $xb), abs($ya - $yb)) - 1];
-                $jumps[$ia][$ib] = $jumpCost + $map[$yb][$xb];
-                $jumps[$ib][$ia] = $jumpCost + $map[$ya][$xa];
-            }
-        }
+        $jumps[] = [$map[0][0] + getJumpCost(0, 0, $x, $y), $x, $y];
     }
 }
 
-$best = array_fill(0, $n * $n, $map[0][0] + $jumps[0][$lastPosition]); //We start with the value of direct jump from start to end
-$toCheck = [[0, $map[0][0]]]; //We start exploring at 0;0
+while(1) {
+    rsort($jumps); //We're always working on the position we can reach with the lowest cost
 
-while(count($toCheck)) {
-    [$position, $cost] = array_pop($toCheck);
+    [$currentCost, $x, $y] = array_pop($jumps);
 
-    //We can jump to any positions
-    for($i = 1; $i < $n * $n; ++$i) {
-        if($i == $position) continue; //No point in moving to the current place
+    //We have reach the destination
+    if($y * $n + $x == $lastPosition) {
+        error_log(var_export(microtime(1) - $start, true));
+        exit("$currentCost");
+    }
 
-        $moveCost = $cost + $jumps[$position][$i]; //The cost of reaching the new positions
+    //Foreach position we haven't reached yet, check if we can reach them with smaller cost
+    foreach($jumps as $index => [$cost, $xj, $yj]) {
+        $jumpCost = $currentCost + getJumpCost($x, $y, $xj, $yj);
 
-        //This move is only valid if we haven't reached the position with a lower cost already
-        if($moveCost < $best[$i] && $moveCost < $best[$lastPosition]) {
-            $best[$i] = $moveCost;
-            $toCheck[] =  [$i, $moveCost];
-        } 
+        if($jumpCost < $jumps[$index][0]) $jumps[$index][0] = $jumpCost;
     }
 }
-
-echo $best[$lastPosition] . PHP_EOL;
-
-error_log(var_export(microtime(1) - $start, true));
-?>
