220f20361d6998f85106e3c6b7bc2dda8cd1d1ae
cedricdd <c-d-d@hotmail.fr>
2023-04-21 18:22:39 +0200

    Update code.php

--- Learning words/code.php
+++ Learning words/code.php
@@ -14,8 +14,6 @@ for ($i = 0; $i < $N; $i++) {
         
         if(!isset($wordsID[$word])) $wordsID[$word] = count($wordsID); //We keep a list of unique words
     }
-    
-    $counts[$i] = ceil(count($sentences[$i]) / 2); //The amount of words we need to learn in this sentence
 }
 
 foreach($sentences as $i => $listWords) {
@@ -26,84 +24,64 @@ foreach($sentences as $i => $listWords) {
     $sentences[$i] = $wordsByFrequency;
 }
 
-//print_r($sentences);
-
-function solve(array $counts, array $sentences, array $wordsUsage, array $learned, string $hash) {
-    global $answer, $wordsID;
+function solve(array $sentences, int $learned, string $hash) {
+    global $answer, $wordsID, $wordsUsage;
     static $history = [];
     
-    if(count($learned) >= $answer) return; //We have already learned more words than the current best solution
+    if($learned >= $answer) return; //We have already learned more words than the current best solution
     
     if(isset($history[$hash])) return; //We have already tested this combinaison of words
     else $history[$hash] = 1;
     
-    asort($counts);
-    
-    foreach($counts as $index => $count) {
-        //We are done with this sentence
-        if($count == 0) {
-            //Update all the words we didn't use in this sentence
-            foreach($sentences[$index] as $word => $freq) {
-                unset($wordsUsage[$word][$index]);
-        
-                //Update the count of this word in other sentences
-                foreach($wordsUsage[$word] as $indexUsage => $filler) {
-                    $sentences[$indexUsage][$word]--;
-                }
-            }
-            
-            unset($counts[$index]);
+    foreach($sentences as $index => $list) {
+        arsort($list);
+
+        //All the words left for this sentence are only appearing in this sentence, doesn't matter witch one we learn
+        if(reset($list) == 1) {
+            $word = array_key_first($list);
+
+            $hash[$wordsID[$word]] = 1;
+            ++$learned;
+
             unset($sentences[$index]);
         } else {
-            arsort($sentences[$index]);
-    
-            //All the words left for this sentence are only appearing in this sentence, doesn't matter witch ones we learn
-            if(reset($sentences[$index]) == 1) {
-                foreach(array_slice($sentences[$index], 0, $count) as $word => $filler) {
-                    $hash[$wordsID[$word]] = 1;
-                    $learned[$word] = 1;
-                }
-    
-                unset($counts[$index]); //No need to update info, these words don't affect other sentences
-                
-                foreach($sentences[$index] as $word => $filler) unset($wordsUsage[$word]);
-                
-                unset($sentences[$index]);
-            } else {
-                $first = array_key_first($sentences[$index]); //The # of sentences the most used word is used in
-                $affectedSentences = $wordsUsage[$first];
+            foreach($list as $wordUsed => $frequency) {
+                if($frequency == 1) break; //We only do recursive calls for words that appear in at least 2 sentences
+
+                $hashUpdated = $hash;
+                $updatedSentences = $sentences;
         
-                foreach($sentences[$index] as $word => $frequency) {
-                    if($frequency == 1) break; //We only do recursive calls for words that appear in at least 2 sentences
-            
-                    $hashUpdated = $hash;
-                    $updatedCounts = $counts;
-                    $updatedSentences = $sentences;
-                    $wordsUsageUpdated = $wordsUsage;
-            
-                    foreach($wordsUsageUpdated[$word] as $indexUsage => $filler) {
-                        unset($updatedSentences[$indexUsage][$word]);
-                        $updatedCounts[$indexUsage]--;
-                    }
-    
-                    $hashUpdated[$wordsID[$word]] = 1;
-                    unset($wordsUsageUpdated[$word]);
-            
-                    solve($updatedCounts, $updatedSentences, $wordsUsageUpdated, $learned + [$word => 1], $hashUpdated);
+                //All the sentences this word is appearing are now good
+                foreach($wordsUsage[$wordUsed] as $indexUsage => $filler) {
+                    unset($updatedSentences[$indexUsage]);
                 }
+
+                $hashUpdated[$wordsID[$wordUsed]] = 1;
                 
-                return;
+                //Update the count of all the words we didn't not learn for this sentence
+                foreach($sentences[$index] as $wordLeft => $usage) {
+
+                    if($usage == 1 || $wordLeft == $wordUsed) continue;
+
+                    foreach($wordsUsage[$wordLeft] as $indexUsage => $filler) {
+                        if(isset($updatedSentences[$indexUsage])) $updatedSentences[$indexUsage][$wordLeft]--;
+                    }
+                }
+        
+                solve($updatedSentences, $learned + 1, $hashUpdated);
             }
+            
+            return;
         }
     }
     
     //We have found a better solution
-    if(count($learned) < $answer) $answer = count($learned);
+    if($learned < $answer) $answer = $learned;
 }
 
 $answer = INF;
 
-solve($counts, $sentences, $wordsUsage, [], str_repeat("0", count($wordsID)));
+solve($sentences, 0, str_repeat("0", count($wordsID)));
 
 echo $answer . PHP_EOL;
 error_log(microtime(1) - $start);
