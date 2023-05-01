d2cd860bfbb2c9569f2cfff3b357892a9ec36ea8
cedricdd <c-d-d@hotmail.fr>
2023-05-01 23:44:31 +0200

    Update code.php

--- Sudoku Solver/code.php
+++ Sudoku Solver/code.php
@@ -1,159 +1,117 @@
 <?php
-for ($i = 0; $i < 9; $i++) {
-    $line = stream_get_line(STDIN, 9 + 1, "\n");
 
-    error_log(var_export($line, true));
-    $grid[] = str_split($line);
+$start = microtime(1);
+
+$grid = "";
+
+for ($i = 0; $i < 9; $i++) {
+    $grid .= trim(fgets(STDIN));
 }
 
 $toCheck = [];
 
+error_log($grid);
+
 //First step we get all the possibilites for the numbers we have to find
 for($y = 0; $y < 9; ++$y) {
     for($x = 0; $x < 9; ++$x) {
+        $index = $y * 9 + $x;
+
         //This number is missing
-        if($grid[$y][$x] == 0) {
-            $numbers = range(1, 9);
+        if($grid[$index] == 0) {
+            $numbers = array_flip(range(1, 9));
+            $related = [];
 
             //Check row & column
             for($i = 0; $i < 9; ++$i) {
-                if($grid[$y][$i] != 0) unset($numbers[$grid[$y][$i] - 1]);
-                if($grid[$i][$x] != 0) unset($numbers[$grid[$i][$x] - 1]);
+                $indexRow = intval($y * 9 + $i);
+
+                if($grid[$indexRow] == 0) $related[$indexRow] = 1;
+                else unset($numbers[$grid[$indexRow]]);
+
+                $indexCol = intval($i * 9 + $x);
+
+                if($grid[$indexCol] == 0) $related[$indexCol] = 1;
+                else unset($numbers[$grid[$indexCol]]);
             }
 
             //Check the square
             for($y2 = (floor($y / 3) * 3); $y2 < ((floor($y / 3) + 1) * 3); ++$y2) {
                 for($x2 = (floor($x / 3) * 3); $x2 < ((floor($x / 3) + 1) * 3); ++$x2) {
-                    if($grid[$y2][$x2] !== 0) unset($numbers[$grid[$y2][$x2] - 1]); 
+                    $indexSquare = intval($y2 * 9 + $x2);
+
+                    if($grid[$indexSquare] == 0) $related[$indexSquare] = 1;
+                    else unset($numbers[$grid[$indexSquare]]);
                 }
             }
 
-            $listPositions[$y][$x] = $numbers; //We save the possibilites
-            if(count($numbers) == 1) $toCheck[$x . " " . $y] = [$x, $y]; //There's only one possibillity for this position
+            $possibilities[$index] = $numbers; //We save the possibilites
+            $relations[$index] = $related;
+            $counts[$index] = count($numbers);
+            if(count($numbers) == 1) $toCheck[$index] = 1; //There's only one possibillity for this position
         }
     }
 }
 
 
-function setNumbers(&$grid, &$listPositions) {
+function setNumbers(&$grid, &$possibilities, &$relations, &$counts) {
 
     global $toCheck;
 
     //Setting number might leave only 1 possibility for other position
-    while(count($toCheck)) {
+    do {
+        $numberFound = false;
 
-        list($x, $y) = array_pop($toCheck);
-        $value = array_pop($listPositions[$y][$x]);
+        foreach($counts as $index => $count) {
 
-        //Update the grid
-        $grid[$y][$x] = $value;
-        unset($listPositions[$y][$x]);
+            if($count != 1) continue;
 
-        //We can only use this number once in the row
-        for($x2 = 0; $x2 < 9; ++$x2) {
-            if($x2 == $x) continue; //The position we just set
+            $numberFound = true;
 
-            if(isset($listPositions[$y][$x2])) {
-                unset($listPositions[$y][$x2][$value - 1]);
-
-                $count = count($listPositions[$y][$x2]);
-
-                //There are no possibilities left, invalid grid
-                if($count == 0) return -1;
-                //Only one possibility left
-                elseif($count == 1) $toCheck[$x2 . " " . $y] = [$x2, $y];
-            }
-        }
-         
-        //We can only use this number once in the column
-        for($y2 = 0; $y2 < 9; ++$y2) {
-            if($y2 == $y) continue; //The position we just set
-
-            if(isset($listPositions[$y2][$x])) {
-                unset($listPositions[$y2][$x][$value - 1]);
-
-                $count = count($listPositions[$y2][$x]);
-
-                //There are no possibilities left, invalid grid
-                if($count == 0) return -1;
-                //Only one possibility left
-                elseif($count == 1)  $toCheck[$x . " " . $y2] = [$x, $y2];
-            }
-        }
-         
-        //We can only use this number once in the square
-        for($y2 = (floor($y / 3) * 3); $y2 < ((floor($y / 3) + 1) * 3); ++$y2) {
-            for($x2 = (floor($x / 3) * 3); $x2 < ((floor($x / 3) + 1) * 3); ++$x2) {
-
-                if($x2 == $x || $y2 == $y) continue; //Alredy checked
-
-                if(isset($listPositions[$y2][$x2])) {
-                    unset($listPositions[$y2][$x2][$value - 1]);
-
-                    $count = count($listPositions[$y2][$x2]);
-
-                    //There are no possibilities left, invalid grid
-                    if($count == 0) return -1;
-                    //Only one possibility left
-                    elseif($count == 1) $toCheck[$x2 . " " . $y2] = [$x2, $y2];
+            $value = array_key_first($possibilities[$index]);
+    
+            //Update the grid
+            $grid[$index] = $value;
+            unset($possibilities[$index]);
+    
+            //We can only use this number once in the row
+            foreach($relations[$index] as $indexToCheck => $filler) {
+                if(isset($possibilities[$indexToCheck][$value])) {
+                    unset($possibilities[$indexToCheck][$value]);
+    
+                    if(--$counts[$indexToCheck] == 0) return -1;
                 }
             }
+    
+            unset($counts[$index]);
+            unset($toCheck[$index]);
         }
-    }
+    } while ($numberFound);
 
-    //Have we a number on all the positions
-    $numbersLeft = array_sum(array_map(function($array) {
-        return count($array);
-    }, $listPositions));
-
-    if($numbersLeft == 0) return 1;
+    if(count($possibilities) == 0) return 1;
     else return 0;
 }
 
 //Guess a number for the sudoku
-function getGuess(&$listPositions, $fobidden, &$toCheck) {
+function getGuess(array &$counts, array &$possibilities, array &$fobidden, array &$toCheck): bool {
 
-    foreach($listPositions as $y => $line) {    
-        foreach($line as $x => $possibilities) {
-            foreach($possibilities as $value) {
+    $index = array_key_first($counts);
 
-                //We already tried, we got an invalid grid
-                if(isset($fobidden[$x . " " . $y . " " . $value])) continue; 
+    foreach($possibilities[$index] as $value => $filler) {
 
-                $listPositions[$y][$x] = [$value];
-                $toCheck[$x . " " . $y] = [$x, $y];
+        //We already tried, we got an invalid grid
+        if(isset($fobidden[$index][$value])) continue; 
 
-                return [$x, $y, $value];
-            }
+        $fobidden[$index][$value] = 1;
+        $possibilities[$index] = [$value => 1];
+        $counts[$index] = 1;
 
-            //All the possibilites for this position are forbidden, invalid grid
-            return [];
-        }
-
+        return true;
     }
 
+    //All the possibilites for this position are forbidden, invalid grid
+    return false;
 }
-
-//Check if the grid is valid
-function checkGrid($grid) {
-
-    for($y = 0; $y < 9; ++$y) {
-        for($x = 0; $x < 9; ++$x) {
-
-            if(isset($checkR[$y][$grid[$y][$x] - 1])) return false;
-            else $checkR[$y][$grid[$y][$x] - 1] = 1;
-
-            if(isset($checkC[$x][$grid[$y][$x] - 1])) return false;
-            else $checkC[$x][$grid[$y][$x] - 1] = 1;
-
-            if(isset($checkS[intval($x / 3) + (intval($y / 3) * 3)][$grid[$y][$x] - 1])) return false;
-            else $checkS[intval($x / 3) + (intval($y / 3) * 3)][$grid[$y][$x] - 1] = 1;
-        }
-    }
-
-    return true;
-}
-
 
 $backups = [];
 $forbidden = [];
@@ -162,37 +120,36 @@ $forbidden = [];
 while(true) {
 
     //We start by setting all the position with only one possibility
-    $result = setNumbers($grid, $listPositions);
+    $result = setNumbers($grid, $possibilities, $relations, $counts);
 
-    //We have filled all the missing positions
-    if($result == 1) {
-        if(checkGrid($grid)) break; //Solution has been found
-        else $result = -1; //Invalid grid
-    }
+    if($result == 1) break; //Solution has been found
 
     //Until we find a guess to test
     while(true) {
         $toCheck = [];
 
+        //error_log("need to guess");
+
         //Invalid grid, reload last backup
         if($result == -1) {
-            list($grid, $listPositions, $forbidden, $guess) = array_pop($backups);
-            $forbidden[$guess] = 1;
+            [$grid, $possibilities, $relations, $forbidden, $counts] = array_pop($backups);
         }
 
-        $temp = $listPositions;
-        //We have to guess a number
-        $guess = getGuess($listPositions, $forbidden, $toCheck);
+        $temp = $possibilities;
 
         //No possible guess, invalid grid
-        if(count($guess) == 0) $result == -1;
+        if(getGuess($counts, $possibilities, $forbidden, $toCheck) == false) $result == -1;
         else {
+            //error_log("using guess");
+
             //We have a guess, backup info
-            $backups[] = [$grid, $temp, $forbidden, implode(" ", $guess)];
+            $backups[] = [$grid, $temp, $relations, $forbidden, $counts];
             break;
         }
     }
 }
 
-for ($i = 0; $i < 9; $i++) echo implode("", $grid[$i]) . "\n";
-?>
+echo implode("\n", str_split($grid, 9)) . "\n";
+
+
+error_log(microtime(1) - $start);
