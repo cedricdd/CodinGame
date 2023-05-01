374b8759abc1aa0ba6306c65d0e2de049f6998f1
cedricdd <c-d-d@hotmail.fr>
2023-05-02 00:16:12 +0200

    Update code.php

--- Sudoku Solver/code.php
+++ Sudoku Solver/code.php
@@ -1,5 +1,8 @@
 <?php
 
+const REMOVE = [1 => 510, 2 => 509, 3 => 507, 4 => 503, 5 => 495, 6 => 479, 7 => 447, 8 => 383, 9 => 255];
+const VALUES = [1 => 1, 2 => 2, 3 => 4, 4 => 8, 5 => 16, 6 => 32, 7 => 64, 8 => 128, 9 => 256];
+
 $start = microtime(1);
 
 $grid = "";
@@ -19,7 +22,7 @@ for($y = 0; $y < 9; ++$y) {
 
         //This number is missing
         if($grid[$index] == 0) {
-            $numbers = array_flip(range(1, 9));
+            $numbers = 511;
             $related = [];
 
             //Check row & column
@@ -27,12 +30,12 @@ for($y = 0; $y < 9; ++$y) {
                 $indexRow = intval($y * 9 + $i);
 
                 if($grid[$indexRow] == 0) $related[$indexRow] = 1;
-                else unset($numbers[$grid[$indexRow]]);
+                else $numbers &= REMOVE[$grid[$indexRow]];
 
                 $indexCol = intval($i * 9 + $x);
 
                 if($grid[$indexCol] == 0) $related[$indexCol] = 1;
-                else unset($numbers[$grid[$indexCol]]);
+                else $numbers &= REMOVE[$grid[$indexCol]];
             }
 
             //Check the square
@@ -41,72 +44,86 @@ for($y = 0; $y < 9; ++$y) {
                     $indexSquare = intval($y2 * 9 + $x2);
 
                     if($grid[$indexSquare] == 0) $related[$indexSquare] = 1;
-                    else unset($numbers[$grid[$indexSquare]]);
+                    else $numbers &= REMOVE[$grid[$indexSquare]];
                 }
             }
 
+            unset($related[$index]);
+
             $possibilities[$index] = $numbers; //We save the possibilites
             $relations[$index] = $related;
-            $counts[$index] = count($numbers);
-            if(count($numbers) == 1) $toCheck[$index] = 1; //There's only one possibillity for this position
+            $toFind[$index] = 1;
         }
     }
 }
 
 
-function setNumbers(&$grid, &$possibilities, &$relations, &$counts) {
-
-    global $toCheck;
+function setNumbers(string &$grid, array &$possibilities, array &$relations, array &$toFind): int {
 
     //Setting number might leave only 1 possibility for other position
     do {
         $numberFound = false;
 
-        foreach($counts as $index => $count) {
+        foreach($toFind as $index => $filler) {
 
-            if($count != 1) continue;
+            switch($possibilities[$index]) {
+                case 1: $value = 1; break;
+                case 2: $value = 2; break;
+                case 4: $value = 3; break;
+                case 8: $value = 4; break;
+                case 16: $value = 5; break;
+                case 32: $value = 6; break;
+                case 64: $value = 7; break;
+                case 128: $value = 8; break;
+                case 256: $value = 9; break;
+                default: continue 2;
+            }
+
+            //error_log("setting $index as $value");
 
             $numberFound = true;
 
-            $value = array_key_first($possibilities[$index]);
-    
             //Update the grid
             $grid[$index] = $value;
-            unset($possibilities[$index]);
-    
+
             //We can only use this number once in the row
             foreach($relations[$index] as $indexToCheck => $filler) {
-                if(isset($possibilities[$indexToCheck][$value])) {
-                    unset($possibilities[$indexToCheck][$value]);
-    
-                    if(--$counts[$indexToCheck] == 0) return -1;
+                if(($possibilities[$indexToCheck] &= REMOVE[$value]) == 0) {
+                    //error_log("$indexToCheck is now empty");
+                    return -1;
                 }
             }
-    
-            unset($counts[$index]);
-            unset($toCheck[$index]);
+
+            unset($toFind[$index]);
         }
     } while ($numberFound);
 
-    if(count($possibilities) == 0) return 1;
+    if(count($toFind) == 0) return 1;
     else return 0;
 }
 
 //Guess a number for the sudoku
-function getGuess(array &$counts, array &$possibilities, array &$fobidden, array &$toCheck): bool {
+function getGuess(array $toFind, array &$possibilities, array &$fobidden): bool {
 
-    $index = array_key_first($counts);
+    $index = array_key_first($toFind);
+    $numbers = $possibilities[$index];
 
-    foreach($possibilities[$index] as $value => $filler) {
+    //error_log("$index -- $numbers");
+
+    foreach(VALUES as $value => $mask) {
 
         //We already tried, we got an invalid grid
         if(isset($fobidden[$index][$value])) continue; 
 
-        $fobidden[$index][$value] = 1;
-        $possibilities[$index] = [$value => 1];
-        $counts[$index] = 1;
+        if($numbers & $mask) {
+            $fobidden[$index][$value] = 1;
+            $possibilities[$index] = $mask;
 
-        return true;
+            //error_log("we can guess $value for $index");
+            //exit();
+    
+            return true;
+        }
     }
 
     //All the possibilites for this position are forbidden, invalid grid
@@ -120,7 +137,7 @@ $forbidden = [];
 while(true) {
 
     //We start by setting all the position with only one possibility
-    $result = setNumbers($grid, $possibilities, $relations, $counts);
+    $result = setNumbers($grid, $possibilities, $relations, $toFind);
 
     if($result == 1) break; //Solution has been found
 
@@ -132,18 +149,18 @@ while(true) {
 
         //Invalid grid, reload last backup
         if($result == -1) {
-            [$grid, $possibilities, $relations, $forbidden, $counts] = array_pop($backups);
+            [$grid, $possibilities, $relations, $forbidden, $toFind] = array_pop($backups);
         }
 
         $temp = $possibilities;
 
         //No possible guess, invalid grid
-        if(getGuess($counts, $possibilities, $forbidden, $toCheck) == false) $result == -1;
+        if(getGuess($toFind, $possibilities, $forbidden) == false) $result == -1;
         else {
             //error_log("using guess");
 
             //We have a guess, backup info
-            $backups[] = [$grid, $temp, $relations, $forbidden, $counts];
+            $backups[] = [$grid, $temp, $relations, $forbidden, $toFind];
             break;
         }
     }
