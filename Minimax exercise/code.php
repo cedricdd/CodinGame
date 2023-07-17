ffb31a8ca20d1d8ed5f7071783e672225502d118
cedricdd <c-d-d@hotmail.fr>
2023-07-17 17:30:18 +0200

    Update code.php

--- Minimax exercise/code.php
+++ Minimax exercise/code.php
@@ -1,29 +1,43 @@
 <?php
 
-function solve(int $start, int $depth): int {
-    global $leafs, $explored, $D, $B;
+//Minimax algorithm
+function solve(array $values, int $depth, float $alpha, float $beta): int {
+    global $explored, $D, $B;
 
     ++$explored;
 
-    if($depth == $D) return $leafs[$start];
+    //We are on the deepest depth
+    if($depth == $D) return array_pop($values);
 
     $value = ($depth % 2 == 0) ? -INF : INF;
 
+    $length = count($values) / $B;
+
     for($i = 0; $i < $B; ++$i) {
-        if($depth % 2 == 0) $value = max($value, solve($start + ($i * ($B ** ($D - $depth - 1))), $depth + 1));
-        else $value = min($value, solve($start + ($i * ($B ** ($D - $depth - 1))), $depth + 1));
+        //We use the max value
+        if($depth % 2 == 0) {
+            $value = max($value, solve(array_slice($values, $i * $length, $length), $depth + 1, $alpha, $beta));
+
+            if($value >= $beta) return $value; //Cutoff beta
+
+            $alpha = max($alpha, $value);
+        }
+        //We use the min value
+        else {
+            $value = min($value, solve(array_slice($values, $i * $length, $length), $depth + 1, $alpha, $beta));
+
+            if($alpha >= $value) return $value; //Cutoff alpha
+
+            $beta = min($beta, $value);
+        }
     }
 
     return $value;
 }
 
-$start = microtime(1);
-
 fscanf(STDIN, "%d %d", $D, $B);
-$leafs = explode(" ", trim(fgets(STDIN)));
+$values = explode(" ", trim(fgets(STDIN)));
 
 $explored = 0;
 
-echo solve(0, 0) . " " . $explored . PHP_EOL;
-
-error_log(microtime(1) - $start);
+echo solve($values, 0, -INF, INF) . " " . $explored . PHP_EOL;
