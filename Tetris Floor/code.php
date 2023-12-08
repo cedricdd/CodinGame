fc6a736fcca4665e9b6b85d6a8c5b27b8b0e6860
cedricdd <c-d-d@hotmail.fr>
2023-12-08 19:38:01 +0100

    Update code.php

--- Tetris Floor/code.php
+++ Tetris Floor/code.php
@@ -17,7 +17,7 @@ $tetrominoes = [
     14 => [[0, 0], [1, 0], [2, 0], [2, -1]], //L 180
     15 => [[0, 0], [0, 1], [0, 2], [1, 2]], //L 270
     16 => [[0, 0], [1, 0], [2, 0], [2, 1]], //J
-    17 => [[0, 0], [0, 1], [0, 2], [-1, 2]], //J 90
+    17 => [[0, 0], [1, 0], [1, -1], [1, -2]], //J 90
     18 => [[0, 0], [0, 1], [1, 1], [2, 1]], //J 180
     19 => [[0, 0], [1, 0], [0, 1], [0, 2]], //J 270
     20 => [[0, 0], [1, 0], [1, 1], [2, 1]], //Z
@@ -42,20 +42,65 @@ $grid = [
 $w = 10;
 $h = 10;
 
-$availabilities = [
-    0,
-    0,
-    8,
-    9999,
-    9999,
-    0,
-    9999,
+$grid = [
+    ".....",
+    ".....",
+    "..#..",
+    ".....",
+    ".....",
 ];
 
+$w = 5;
+$h = 5;
+
+/*
+$grid = [
+    "......",
+    "......",
+    "......",
+    "......",
+];
+
+$w = 6;
+$h = 4;
+*/
+
+$availabilities = [
+    10,
+    10,
+    10,
+    10,
+    10,
+    10,
+    10,
+];
+
+$prices = [
+  10.0,
+  10.0,
+  10.0,
+  10.0,
+  10.0,
+  1.0,
+  1.0,
+];
+
+$lowestPrice = min($prices);
 $pieceID = 0;
 $counts = array_fill(0, $w * $h, 0);
 $positions = array_fill(0, $w * $h, []);
 $piecesType = [];
+$nbrPieces = 0;
+$bestPrice = INF;
+$bestList = [];
+$output = array_fill(0, $h * 2 + 1, str_repeat(" ", $w * 2 + 1));
+
+for($y = 0; $y < $h * 2 + 1; ++$y) {
+    for($x = 0; $x < $w * 2 + 1; ++$x) {
+        if($y % 2 == 0) $output[$y][$x] = ($x % 2 == 0) ? '+' : '-';
+        elseif($x % 2 == 0) $output[$y][$x] = '|';
+    }
+}
 
 for($y = 0; $y < $h; ++$y) {
     for($x = 0; $x < $w; ++$x) {
@@ -69,13 +114,14 @@ for($y = 0; $y < $h; ++$y) {
             
             continue;
         }
+    
+        $nbrPieces++;
         
-        foreach($tetrominoes as $i => $moves) {
+        foreach($tetrominoes as $pieceType => $moves) {
             
-            $pieceType = intdiv($i, 4);
             $piecePositions = [];
             
-            if($availabilities[$pieceType] == 0) continue;
+            if($availabilities[intdiv($pieceType, 4)] == 0) continue;
             
             foreach($moves as [$xm, $ym]) {
                 $xu = $x + $xm;
@@ -101,7 +147,14 @@ for($y = 0; $y < $h; ++$y) {
     }
 }
 
-//print_r($pieces);
+$nbrPieces /= 4;
+
+
+foreach($positions as $index => $filler) {
+    uksort($positions[$index], function($a, $b) use ($prices, $piecesType) {
+        return $prices[intdiv($piecesType[$a], 4)] <=> $prices[intdiv($piecesType[$b], 4)];
+    });
+}
 
 error_log("we have " . count($pieces) . " pieces for this input");
 
@@ -122,17 +175,99 @@ function generateOutput(array $listPieces) {
     echo implode("\n", $output) . PHP_EOL;
 }
 
-//We are solving by using https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
-function solve(array $pieces, array $positions, array $counts, array $availabilities, array $list) {
+function generateOutput2(array $output, array $listPieces) {
+    global $pieces, $piecesType, $w, $h;
     
-    global $piecesType;
-    static $count = 0;
+    foreach($listPieces as $pieceID) {
+        $position = reset($pieces[$pieceID]);
+        
+        $xs = ($position % $w) * 2 + 1;
+        $ys = intdiv($position, $w) * 2 + 1;
+        
+        error_log("piece $pieceID is of type " . $piecesType[$pieceID] . " -- start at $xs $ys");
+        
+        switch($piecesType[$pieceID]) {
+            case 0:
+                    $output[$ys][$xs + 1] = $output[$ys][$xs + 3] = $output[$ys][$xs + 5] = " ";
+                break;
+            case 1:
+                $output[$ys + 1][$xs] = $output[$ys + 3][$xs] = $output[$ys + 5][$xs] = " ";
+                break;
+            case 4:
+                $output[$ys][$xs + 1] = $output[$ys + 1][$xs] = $output[$ys + 1][$xs + 1] =  $output[$ys + 2][$xs + 1] = $output[$ys + 1][$xs + 2]  = " ";
+                break;
+            case 8:
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] =  $output[$ys + 1][$xs + 2] = " ";
+                break;
+            case 9:
+                $output[$ys + 1][$xs] = $output[$ys + 3][$xs] =  $output[$ys + 2][$xs - 1] = " ";
+                break;
+            case 10:
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] =  $output[$ys - 1][$xs + 2] = " ";
+                break;
+            case 11:
+                $output[$ys + 1][$xs] = $output[$ys + 3][$xs] =  $output[$ys + 2][$xs + 1] = " ";
+                break;
+            case 12:
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] =  $output[$ys + 1][$xs] = " ";
+                break;
+            case 13:
+                $output[$ys][$xs + 1] = $output[$ys + 1][$xs + 2] =  $output[$ys + 3][$xs + 2] = " ";
+                break;
+            case 14:
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] =  $output[$ys - 1][$xs + 4] = " ";
+                break;
+            case 15:
+                $output[$ys + 1][$xs] = $output[$ys + 3][$xs] =  $output[$ys + 4][$xs + 1] = " ";
+                break;
+            case 16:
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] =  $output[$ys + 1][$xs + 4] = " ";
+                break;
+            case 17:
+                $output[$ys][$xs + 1] = $output[$ys - 1][$xs + 2] =  $output[$ys - 3][$xs + 2] = " ";
+                break;
+            case 18:
+                $output[$ys + 1][$xs] = $output[$ys + 2][$xs + 1] =  $output[$ys + 2][$xs + 3] = " ";
+                break;
+            case 19:
+                $output[$ys][$xs + 1] = $output[$ys + 1][$xs] =  $output[$ys + 3][$xs] = " ";
+                break;
+            case 20:
+                $output[$ys][$xs + 1] = $output[$ys + 1][$xs + 2] =  $output[$ys + 2][$xs + 3] = " ";
+                break;
+            case 21:
+                $output[$ys + 1][$xs] = $output[$ys + 2][$xs - 1] =  $output[$ys + 3][$xs - 2] = " ";
+                break;
+            case 24:
+                $output[$ys][$xs + 1] = $output[$ys - 1][$xs + 2] =  $output[$ys - 2][$xs + 3] = " ";
+                break;
+            case 25:
+                $output[$ys + 1][$xs] = $output[$ys + 2][$xs + 1] =  $output[$ys + 3][$xs + 2] = " ";
+                break;
+        }
+    }
+    
+    echo implode("\n", $output) . PHP_EOL;
+}
+
+//We are solving by using https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
+function solve(array $pieces, array $positions, array $counts, array $availabilities, int $nbrPieces, array $list, float $price) {
+    
+    global $piecesType, $prices, $lowestPrice, $bestList, $bestPrice;
     $debug = false;
     
+    if($debug) error_log($nbrPieces . " " . implode("-", $list));
+    
     //The matrix is empty we have found a solution
-    if(count($counts) == 0) {
-        echo "We found a solution!!! " . (++$count) . PHP_EOL;
-        generateOutput($list);
+    if($nbrPieces == 0) {
+        echo "We found a solution!!! $price -- " . PHP_EOL;
+        
+        //We have a new best price, reset the list of solutions
+        if($bestPrice != $price) $bestList = [];
+    
+        $bestList[] = $list;
+        $bestPrice = $price;
+        
         return;
     }
     
@@ -156,36 +291,40 @@ function solve(array $pieces, array $positions, array $counts, array $availabili
     
         if($debug) error_log("at $position we can use the piece $pieceID");
     
-        $pieceType = $piecesType[$pieceID];
+        $pieceType = intdiv($piecesType[$pieceID], 4);
         
         if($availabilities[$pieceType] == 0) continue;
         else $availabilities[$pieceType]--;
+    
+        $piecePrice = $prices[$pieceType];
+        
+        if($price + $piecePrice + ($nbrPieces - 1 * $lowestPrice) > $bestPrice) continue;
+        
+        $list[$nbrPieces] = $pieceID;
         
         //Copy info for recursive
-        $list2 = array_merge($list, [$pieceID]);
         $positions2 = $positions;
         $counts2 = $counts;
         $pieces2 = $pieces;
-        $availabilities2 = $availabilities;
         
         //Foreach columns that have a 1 in the row we are testing we need to remove them
-        foreach($pieces[$pieceID] as $positionID) {
+        foreach($pieces2[$pieceID] as $positionID) {
     
-            if($debug) error_log("piece $pieceID is affecting the position $positionID");
+            //if($debug) error_log("piece $pieceID is affecting the position $positionID");
             
-            if(!isset($positions2[$positionID])) continue; //Column was already removed
+            if(!isset($positions2[$positionID])) continue;
             
             //Foreach rows that have a 1 in the column we are removing we need to remove them
-            foreach ($positions[$positionID] as $pieceID2) {
+            foreach($positions2[$positionID] as $pieceID2) {
     
-                if($debug) error_log("at position $positionID we need to invalid piece $pieceID2");
+                //if($debug) error_log("at position $positionID we need to invalid piece $pieceID2");
                 
                 if(!isset($pieces2[$pieceID2])) continue; //Row was already removed
                 
                 //Foreach columns that have a 1 in the row we are removing we need to update the count and remove the row from it's 1 position
-                foreach($pieces[$pieceID2] as $positionID2) {
+                foreach($pieces2[$pieceID2] as $positionID2) {
     
-                    if($debug) error_log("piece $pieceID2 is updating position $positionID2");
+                    //if($debug) error_log("piece $pieceID2 is updating position $positionID2");
                     
                     --$counts2[$positionID2]; //Update the count of 1s in the column
                     unset($positions2[$positionID2][$pieceID2]); //Remove the row from the position of the 1s in the column
@@ -196,14 +335,23 @@ function solve(array $pieces, array $positions, array $counts, array $availabili
             
             unset($positions2[$positionID]); //Remove the column
             unset($counts2[$positionID]); //Column has been removed, we also need to remove the count
+            
+            if($debug) error_log("unsetting count $positionID");
         }
         
-        solve($pieces2, $positions2, $counts2, $availabilities, $list2);
+        solve($pieces2, $positions2, $counts2, $availabilities, $nbrPieces - 1, $list, $price + $piecePrice);
     
         $availabilities[$pieceType]++;
     }
 }
 
-solve($pieces, $positions, $counts, $availabilities, []);
+solve($pieces, $positions, $counts, $availabilities, $nbrPieces, [], 0.0);
+
+error_log("we have " . count($bestList) . " solutions with $bestPrice");
+
+foreach($bestList as $list) {
+    //generateOutput($list);
+    generateOutput2($output, $list);
+}
 
 error_log(microtime(1) - $start);
