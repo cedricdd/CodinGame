3f21be2de582a5acebd808899624960a5a43dde5
cedricdd <c-d-d@hotmail.fr>
2024-01-04 21:30:44 +0100

    Update code.php

--- Solve for X/code.php
+++ Solve for X/code.php
@@ -95,26 +95,26 @@ function sortMulDiv(string $solution): string {
     return $solution;
 }
 
-//Split by addition/substraction
-function splitAddSub(string $equation): array {
+//Split by multiplication/division
+function splitMulDiv(string $equation): array {
     $count = 0;
     $term = "";
-    $prev = "+";
-    $terms = ["+" => [], "-" => []];
+    $op = "*";
+    $terms = ["*" => [], "/" => []];
 
-    //The equation stars with the negative sign
-    if($equation[0] == "-") {
-        $prev = "-";
+    //The equation stars with the division sign
+    if($equation[0] == "/") {
+        $op = "/";
         $equation = substr($equation, 1);
     }
 
-    foreach(str_split($equation . "+") as $c) {
+    foreach(str_split($equation . "*") as $c) {
         if($c === "(") ++$count;
         elseif($c === ")") --$count;
-        elseif($count == 0 && ($c === "+" || $c === "-")) {
-            $terms[$prev][] = $term;
+        elseif($count == 0 && ($c === "*" || $c === "/")) {
+            $terms[$op][] = $term;
             $term = "";
-            $prev = $c;
+            $op = $c;
             continue;
         } 
 
@@ -124,19 +124,40 @@ function splitAddSub(string $equation): array {
     return $terms;
 }
 
-function solve(string $equation): string {
-    [$left, $right] = explode("=", $equation);
+//Split by addition/substraction
+function splitAddSub(string $equation): array {
+    $count = 0;
+    $term = "";
+    $op = "+";
+    $terms = ["+" => [], "-" => []];
 
-    if(strpos($right, "x") !== false) {
-        $equation = $left;
-        $toSolve = $right;
-    }
-    else {
-        $equation = $right;
-        $toSolve = $left;
+    //The equation stars with the negative sign
+    if($equation[0] == "-") {
+        $op = "-";
+        $equation = substr($equation, 1);
     }
 
-    $terms = splitAddSub($toSolve);
+    foreach(str_split($equation . "+") as $c) {
+        if($c === "(") ++$count;
+        elseif($c === ")") --$count;
+        elseif($count == 0 && ($c === "+" || $c === "-")) {
+            $terms[$op][] = $term;
+            $term = "";
+            $op = $c;
+            continue;
+        } 
+
+        $term .= $c;
+    }
+
+    return $terms;
+}
+
+function solve(string $termX, string $equation): string {
+
+    error_log("Calling solve with $termX -- $equation");
+
+    $terms = splitAddSub($termX);
 
     //Find the term with "x"
     $termX = null;
@@ -170,6 +191,142 @@ function solve(string $equation): string {
         if(count($terms["+"]) > 0) $equation .= "-" .  implode("-", $terms["+"]);
     }
 
+    //We are done
+    if($termX == "x") return $equation;
+
+    $count = 0;
+    $term = "";
+    $op = "*";
+    $start = 0;
+
+    foreach(str_split($termX . "*") as $i => $c) {
+        if($c === "(") ++$count;
+        elseif($c === ")") --$count;
+        elseif($count == 0 && ($c === "*" || $c === "/")) {
+            if(strpos($term, "x") !== false) {
+                //error_log("$term -- $start");
+                
+                if($start > 0) {
+                    $s = substr($termX, 0, $start - 1);
+
+                    error_log("before: $op -- $s");
+                    if($op == "*") $equation = (ctype_digit($equation) ? $equation : "(" . $equation . ")") . "/" . (ctype_digit($s) ? $s : "(" . $s . ")");
+                    else $equation = (ctype_digit($s) ? $s : "(" . $s . ")") . "/" . (ctype_digit($equation) ? $equation : "(" . $equation . ")");
+
+                }
+                if($i < strlen($termX)) {
+                    $s = substr($termX, $i + 1);
+
+                    error_log("after: $c -- " . $s);
+                    if($c == "*") $equation = (ctype_digit($equation) ? $equation : "(" . $equation . ")") . "/" . (ctype_digit($s) ? $s : "(" . $s . ")");
+                    else $equation = (ctype_digit($equation) ? $equation : "(" . $equation . ")") . "*" . (ctype_digit($s) ? $s : "(" . $s . ")");
+                }
+
+                $termX = $term;
+                break;
+            }
+
+            $term = "";
+            $op = $c;
+            $start = $i + 1;
+            continue;
+        } 
+
+        $term .= $c;
+    }
+
+    //We are done
+    if($termX == "x") return $equation;
+    //X is inside parentheses
+    else return solve(substr($termX, 1, -1), $equation);
+}
+
+function removeParentheses(string $equation): string {
+    error_log("Calling parentheses with -- $equation");
+
+    //There's nothing to do, no parentheses
+    if(strpos($equation, "(") === false) return $equation;
+
+    $count = 0;
+    $term = "";
+    $op = "+";
+    $start = 0;
+
+    foreach(str_split($equation . "+") as $i => $c) {
+        if($c === "(") ++$count;
+        elseif($c === ")") --$count;
+        elseif($count == 0 && ($c === "+" || $c === "-" || $c === "*" || $c === "/")) {
+            error_log("working on $term -- $op -- $start -- $i");
+
+            //The term is inside parantheses
+            if($term[0] == "(") {
+                
+                //First we make a recursive call on the part inside the parentheses
+                $term = removeParentheses(substr($term, 1, -1));
+
+                /*
+                //It's just an integer, parantheses are not needed
+                if(ctype_digit($term)) {
+                    error_log("($term) => $term");
+                    $equation = str_replace("(" . $term . ")", $term, $equation);
+                }
+                */
+
+                $termsAddSub = splitAddSub($term);
+                $termsMulDiv = splitMulDiv($term);
+
+                //error_log(var_export($termsAddSub, true));
+
+                //There are no addition or substraction
+                if(count($termsAddSub["+"]) == 1 && $termsAddSub["+"][0] == $term) {
+                    if($op == "/") {
+                        //error_log(var_export($termsMulDiv, true));
+
+                        if($start > 0) $updatedEquation = substr($equation, 0, $start - 1);
+                        if(count($termsMulDiv["/"]) > 0) $updatedEquation .= "*" . implode("*", $termsMulDiv["/"]);
+                        if(count($termsMulDiv["*"]) > 0) $updatedEquation .= "/" . implode("/", $termsMulDiv["*"]);
+                        $updatedEquation .= substr($equation, $i);
+
+                        error_log($updatedEquation);
+                        //exit();
+                    } else {
+                        error_log("($term) => $term");
+                        $updatedEquation = substr($equation, 0, $start) . $term . substr($equation, $i);
+                    }
+
+                    return removeParentheses($updatedEquation);
+                }
+
+                //There are no mulitplication or division
+                if(count($termsMulDiv["*"]) == 1 && $termsMulDiv["*"][0] == $term) {
+                    if($op == "-") {
+                        //error_log(var_export($termsMulDiv, true));
+
+                        if($start > 0) $updatedEquation = substr($equation, 0, $start - 1);
+                        if(count($termsAddSub["-"]) > 0) $updatedEquation .= "+" . implode("+", $termsAddSub["-"]);
+                        if(count($termsAddSub["+"]) > 0) $updatedEquation .= "-" . implode("-", $termsAddSub["+"]);
+                        $updatedEquation .= substr($equation, $i);
+
+                        error_log($updatedEquation);
+                        //exit();
+                    } else {
+                        error_log("($term) => $term");
+                        $updatedEquation = substr($equation, 0, $start) . $term . substr($equation, $i);
+                    }
+
+                    return removeParentheses($updatedEquation);
+                }
+            }
+
+            $start = $i + 1;
+            $term = "";
+            $op = $c;
+            continue;
+        } 
+
+        $term .= $c;
+    }
+
     return $equation;
 }
 
@@ -179,9 +336,24 @@ fscanf(STDIN, "%d", $numEquations);
 for ($i = 0; $i < $numEquations; $i++) {
     $equation = trim(fgets(STDIN));
 
-    $equation = solve($equation);
+    error_log("Equation: $equation");
+
+    [$left, $right] = explode("=", $equation);
+
+    if(strpos($right, "x") !== false) {
+        $equation = $left;
+        $termX = $right;
+    }
+    else {
+        $equation = $right;
+        $termX = $left;
+    }
+
+    $equation = solve($termX, $equation);
+    $equation = removeParentheses($equation);
 
     echo "x=" . sortAddSub($equation) . PHP_EOL;
 }
+
 
 error_log(microtime(1) - $start);
