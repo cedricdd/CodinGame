2f531285d997ebc60b53ce5e60c706d7e605b01f
cedricdd <c-d-d@hotmail.fr>
2024-02-12 16:21:12 +0100

    Update code.php

--- Gravity Tumbler/code.php
+++ Gravity Tumbler/code.php
@@ -2,24 +2,18 @@
 
 fscanf(STDIN, "%d %d", $width, $height);
 fscanf(STDIN, "%d", $count);
-for ($i = 0; $i < $height; $i++) {
-    $line = trim(fgets(STDIN));
-    $sum = substr_count($line, "#");
-    //We push all the # at the end of the line
-    $map[] = str_repeat(".", $width - $sum) . str_repeat("#", $sum);
-}
 
 //Result are repeated, we just need to flip the array once if the number of steps is odd
-if($count & 1) {
-    $rotated = array_fill(0, $width, str_repeat(".", $height));
+$map = ($count & 1) ? array_fill(0, $width, str_repeat(".", $height)) : array_fill(0, $height, str_repeat(".", $width));
 
-    for($y = 0; $y < $height; ++$y) {
-        for($x = 0; $x < $width; ++$x) {
-            $rotated[$x][$y] = $map[$y][$x];
-        }
+for ($i = 0; $i < $height; $i++) {
+    $line = trim(fgets(STDIN));
+    $occ = substr_count($line, "#");
+    
+    for($j = 0; $j < $occ; ++$j) {
+        if($count & 1) $map[$width - $j - 1][$i] = "#"; //Add # at the the of the col
+        else $map[$i][$width - $j - 1] = "#"; //Add # at the the of the line
     }
-
-    $map = $rotated;
 }
 
 echo implode("\n", $map) . PHP_EOL;
