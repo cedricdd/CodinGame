6b46660d56c2aba7ecf995b8371659709d01e2d5
cedricdd <c-d-d@hotmail.fr>
2024-02-20 00:07:15 +0100

    Update code-generate.php

--- Tetris Floor/code-generate.php
+++ Tetris Floor/code-generate.php
@@ -27,77 +27,86 @@ const TETROMINOES = [
 ];
 
 $floor = [
-    "##################################################",
-    "#.....#..#..#....###....##....###....#.....#..#..#",
-    "#..#......#.#.#...#...#.##.#...#...#.#..#......#.#",
-    "#....#.#....#..#..#..#..##..#..#..#..#....#.#....#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#...#.#..#..##...#.#....###...#.#....#...#.#..#..#",
-    "##....#....###.#..#..#.####.#..#..#.###....#....##",
-    "#..#..#.#...#....#.#...###....#.#...##..#..#.#...#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#....#.#....#...#.#.#...##...#.#.#...#....#.#....#",
-    "#.#......#..#.#...#...#.##.#...#...#.#.#......#..#",
-    "#..#..#.....#....###....##....###....#..#..#.....#",
-    "##################################################",
-    "#.....#..#..#....###....##....###....#.....#..#..#",
-    "#..#......#.#.#...#...#.##.#...#...#.#..#......#.#",
-    "#....#.#....#..#..#..#..##..#..#..#..#....#.#....#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#...#.#..#..##...#.#....###...#.#....#...#.#..#..#",
-    "##....#....###.#..#..#.####.#..#..#.###....#....##",
-    "#..#..#.#...#....#.#...###....#.#...##..#..#.#...#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#....#.#....#...#.#.#...##...#.#.#...#....#.#....#",
-    "#.#......#..#.#...#...#.##.#...#...#.#.#......#..#",
-    "#..#..#.....#....###....##....###....#..#..#.....#",
-    "##################################################",
-    "##################################################",
-    "#.....#..#..#....###....##....###....#.....#..#..#",
-    "#..#......#.#.#...#...#.##.#...#...#.#..#......#.#",
-    "#....#.#....#..#..#..#..##..#..#..#..#....#.#....#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#...#.#..#..##...#.#....###...#.#....#...#.#..#..#",
-    "##....#....###.#..#..#.####.#..#..#.###....#....##",
-    "#..#..#.#...#....#.#...###....#.#...##..#..#.#...#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#....#.#....#...#.#.#...##...#.#.#...#....#.#....#",
-    "#.#......#..#.#...#...#.##.#...#...#.#.#......#..#",
-    "#..#..#.....#....###....##....###....#..#..#.....#",
-    "##################################################",
-    "#.....#..#..#....###....##....###....#.....#..#..#",
-    "#..#......#.#.#...#...#.##.#...#...#.#..#......#.#",
-    "#....#.#....#..#..#..#..##..#..#..#..#....#.#....#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#...#.#..#..##...#.#....###...#.#....#...#.#..#..#",
-    "##....#....###.#..#..#.####.#..#..#.###....#....##",
-    "#..#..#.#...#....#.#...###....#.#...##..#..#.#...#",
-    "#..##...##..#...#...#...##...#...#...#..##...##..#",
-    "#....#.#....#...#.#.#...##...#.#.#...#....#.#....#",
-    "#.#......#..#.#...#...#.##.#...#...#.#.#......#..#",
-    "#..#..#.....#....###....##....###....#..#..#.....#",
-    "##################################################",
+    "##############################",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "##############################",
+    "#............................#",
+    "##############################",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "#......#......##......#......#",
+    "##############################",
 ];
-$w = 50;
-$h = 50;
+
+$w = 30;
+$h = 21;
 
 $prices = [
-    27.70,
-6.38,
-27.70,
-57.31,
-69.36,
-83.07,
-13.50,
+    83.73,
+89.16,
+2.41,
+"75.90",
+54.34,
+43.25,
+91.33,
 ];
 
-const USE_OPTIMS = false;
+$floor = [
+    "##################################",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#...#...#...#...##...#...#...#...#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "##################################",
+    "#................................#",
+    "##################################",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#...#...#...#...##...#...#...#...#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "#.......#.......##.......#.......#",
+    "##################################",
+];
+
+$w = 34;
+$h = 19;
+
+$prices = [
+    21.26,
+50.21,
+30.59,
+84.03,
+67.38,
+6.47,
+33.17,
+];
+
+
 
 echo implode(" ", $prices) . PHP_EOL;
 echo implode("\n", $floor) . PHP_EOL;
 
+const USE_OPTIMS = true;
+
 $prices = array_map(function($price) {
-    return str_replace(".", "", $price);
+    return str_replace(".", "", strval($price));
 }, $prices);
 
 $minPrice = min($prices);
@@ -110,7 +119,7 @@ $price = 0;
 for($y = 0; $y < $h; ++$y) {
     for($x = 0; $x < $w; ++$x) {
         if($floor[$y][$x] == "#") continue;
-    
+        
         error_log("We are starting a flood at $x $y");
         
         //We start flood fill to find all the position we're gonna work on
@@ -121,7 +130,7 @@ for($y = 0; $y < $h; ++$y) {
         
         while(count($toExplore)) {
             [$xp, $yp] = array_pop($toExplore);
-    
+            
             if($floor[$yp][$xp] == "#") continue;
             else $floor[$yp][$xp] = "#";
             
@@ -137,16 +146,21 @@ for($y = 0; $y < $h; ++$y) {
                 $toExplore[] = [$xp + $xm, $yp + $ym];
             }
         }
+        
+        $countPositions = count($positions);
+        
+        error_log("we need to fill $countPositions positions");
     
-        error_log("we need to fill " . count($positions) . " positions");
+        if(intdiv($countPositions, 4) != ($countPositions / 4)) {
+            error_log("The number of pieces isn't a multiple of 4");
+            exit();
+        }
         
         $sizeY = $maxY - $minY + 3;
         $sizeX = $maxX - $minX + 3;
         $blockFloor = array_fill(0, $sizeY, str_repeat("#", $sizeX));
         
         foreach($positions as [$xp, $yp]) $blockFloor[$yp - $minY + 1][$xp - $minX + 1] = ".";
-    
-        error_log(implode("\n", $blockFloor));
         
         $blockFloor = implode("", $blockFloor);
         
@@ -175,7 +189,7 @@ for($y = 0; $y < $h; ++$y) {
                         $piecePositions[$newIndex] = $newIndex;
                     }
                     
-                    //For each positions this piece would occupy save the info
+                    //For each position this piece would occupy save the info
                     foreach($piecePositions as $indexPosition) {
                         $counts[$indexPosition] = ($counts[$indexPosition] ?? 0) + 1;
                         
@@ -187,27 +201,28 @@ for($y = 0; $y < $h; ++$y) {
                 }
             }
             
-            //For each positions where there's a single possibility directly use the piece
+            //For each position where there's a single possibility directly use the piece
             if(USE_OPTIMS) {
                 while(($index = key($counts)) !== null) {
-        
+                    
                     if($counts[$index] == 1) {
                         $pieceID = array_key_first($positions[$index]);
-            
+                        
                         $pieceType = intdiv($piecesType[$pieceID], 4);
-    
+                        
                         error_log("we need to set position $index with piece ID $pieceID -- type $pieceType");
-            
+                        
                         $usage[$pieceType]++;
                         $price += $prices[$pieceType];
-            
+                        $countPositions -= 4;
+                        
                         //Work on all the positions of the piece
                         foreach($pieces[$pieceID] as $positionID) {
-                
+                            
                             $blockFloor[$positionID] = "#";
-                
+                            
                             if(!isset($positions[$positionID])) continue;
-                
+                            
                             //Any pieces that was using this position can no longer be used
                             foreach($positions[$positionID] as $pieceID2) {
                                 foreach($pieces[$pieceID2] as $positionID2) {
@@ -215,23 +230,31 @@ for($y = 0; $y < $h; ++$y) {
                                     unset($positions[$positionID2][$pieceID2]);
                                 }
                             }
-                
+                            
                             unset($positions[$positionID]);
                             unset($counts[$positionID]);
                         }
-            
+                        
                         reset($counts); //We restart from the start, adding a piece might create more position with a single possibility
                     } else next($counts);
                 }
             }
             
+            if($countPositions == 0) continue;
+    
+            print_r(str_split($blockFloor, $sizeX));
+            
             //When we work on a position we want to try the cheapest piece first
             foreach($positions as $index => $filler) {
                 uksort($positions[$index], function($a, $b) use ($prices, $piecesType) {
-                    return $prices[intdiv($piecesType[$a], 4)] <=> $prices[intdiv($piecesType[$b], 4)];
+                    $priceA = $prices[intdiv($piecesType[$a], 4)];
+                    $priceB = $prices[intdiv($piecesType[$b], 4)];
+                    
+                    if($priceA == $priceB) return $a <=> $b;
+                    else return $priceA <=> $priceB;
                 });
             }
-    
+            
             error_log("we have " . count($pieces) . " pieces for this input");
             
             $bestPrice = INF; //Best price for the current block
@@ -239,14 +262,14 @@ for($y = 0; $y < $h; ++$y) {
             $solutions = solve($blockFloor, $positions, $counts, array_fill(0, 7, 0), 0.0);
             
             $bestPrice = min(array_keys($solutions));
-    
+            
             error_log("The best price after this block is $bestPrice");
-    
+            
             print_r($solutions[$bestPrice]);
-    
+            
             if(count($solutions[$bestPrice]) > 1) {
                 error_log("There are multiple solutions for this flood, no need to continue");
-        
+                
                 print_r($solutions[$bestPrice]);
                 exit();
             }
@@ -258,7 +281,7 @@ for($y = 0; $y < $h; ++$y) {
         foreach(explode("-", $history[$blockFloor][1]) as $i => $v) $usage[$i] += $v;
         $variations *= $history[$blockFloor][2];
         $price += $history[$blockFloor][0];
-    
+        
         echo(microtime(1) - $start) . PHP_EOL;
     }
 }
@@ -269,22 +292,34 @@ echo "The number of variations is: $variations" . PHP_EOL;
 
 echo(microtime(1) - $start);
 
-function generateOutput(array $output, array $listPieces) {
+function generateOutput(array $listPieces) {
     global $pieces, $piecesType, $w, $h;
+    
+    $output = array_fill(0, $h * 2 + 1, str_repeat(" ", $w * 2 + 1));
+    
+    for($y = 0; $y < $h * 2 + 1; ++$y) {
+        for($x = 0; $x < $w * 2 + 1; ++$x) {
+            if($y % 2 == 0) $output[$y][$x] = ($x % 2 == 0) ? '+' : '-';
+            elseif($x % 2 == 0) $output[$y][$x] = '|';
+        }
+    }
     
     $quantities = array_fill(0, 7, 0);
     
+    
     foreach($listPieces as $pieceID => $filler) {
         $position = reset($pieces[$pieceID]);
-    
+        
         $quantities[intdiv($piecesType[$pieceID], 4)]++;
         
         $xs = ($position % $w) * 2 + 1;
         $ys = intdiv($position, $w) * 2 + 1;
         
+        error_log("$xs $ys " . $piecesType[$pieceID]);
+        
         switch($piecesType[$pieceID]) {
             case 0:
-                    $output[$ys][$xs + 1] = $output[$ys][$xs + 3] = $output[$ys][$xs + 5] = " ";
+                $output[$ys][$xs + 1] = $output[$ys][$xs + 3] = $output[$ys][$xs + 5] = " ";
                 break;
             case 1:
                 $output[$ys + 1][$xs] = $output[$ys + 3][$xs] = $output[$ys + 5][$xs] = " ";
@@ -351,7 +386,7 @@ function generateOutput(array $output, array $listPieces) {
 function solve(string $floor, array $positions, array $counts, array $usage, int $price, array $list = []): array {
     
     static $history = [];
-    global $pieces, $piecesType, $prices, $minPrice, $output, $bestPrice;
+    global $pieces, $piecesType, $prices, $minPrice, $bestPrice;
     
     $results = [];
     $hashUsage = implode("-", $usage);
@@ -361,7 +396,7 @@ function solve(string $floor, array $positions, array $counts, array $usage, int
     if($positionsLeft == 0) {
         $bestPrice = min($bestPrice, $price);
         
-        //if($price ==  86803) generateOutput($output, $list);
+//        if($price == 20764) generateOutput($list);
         
         return [strval($price) => [$hashUsage => 1]];
     }
@@ -369,7 +404,7 @@ function solve(string $floor, array $positions, array $counts, array $usage, int
     if(USE_OPTIMS && isset($history[$floor][$hashUsage])) {
         return $history[$floor][$hashUsage];
     }
-
+    
     if(USE_OPTIMS && $price + ($minPrice * ($positionsLeft / 4)) > $bestPrice) {
         return $results;
     }
@@ -423,7 +458,7 @@ function solve(string $floor, array $positions, array $counts, array $usage, int
                 $results[$priceSolution][$index] = ($results[$priceSolution][$index] ?? 0) + $count;
             }
         }
-    
+        
         //Reset the values
         $usage[$pieceType]--;
         unset($list[$pieceID]);
