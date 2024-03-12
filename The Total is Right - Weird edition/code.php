4c40ed7270c8b7a64ec0a771b40644e94e73ca16
cedricdd <c-d-d@hotmail.fr>
2024-03-12 18:25:59 +0100

    Update code.php

--- The Total is Right - Weird edition/code.php
+++ The Total is Right - Weird edition/code.php
@@ -10,43 +10,53 @@ $count = 2;
 $history = [$a => 1];
 
 while(true) {
-    for($i = 1; $i < $count; ++$i) {
+    for($i = 1; $i <= ($count >> 1); ++$i) {
         foreach($list[$i] as $value1 => $filler) {
             foreach($list[$count - $i] as $value2 => $filler) {
-                //Addition
+                //Order of addition doesn't matter
                 $value = $value1 + $value2;
                 if(!isset($history[$value])) {
-                    $history[$value] = 1;
                     $list[$count][$value] = 1;
-                }
-                //Substraction
-                $value = $value1 - $value2;
-                if(!isset($history[$value])) {
                     $history[$value] = 1;
-                    $list[$count][$value] = 1;
                 }
-                //Multiplication
+
+                //Order of multiplication doesn't matter
                 $value = $value1 * $value2;
                 if(!isset($history[$value])) {
-                    $history[$value] = 1;
                     $list[$count][$value] = 1;
+                    $history[$value] = 1;
                 }
-                //Division, can't divide by 0 and we an integer result
-                if($value2 != 0 && $value1 % $value2 == 0) {
+
+                //Substraction
+                $value = abs($value1 - $value2);
+                if(!isset($history[$value])) {
+                    $list[$count][$value] = 1;
+                    $history[$value] = 1;
+                }
+
+                //We can't divide by 0 and the result needs to be an integer
+                if ($value2 != 0 && $value1 % $value2 == 0) {
                     $value = $value1 / $value2;
                     if(!isset($history[$value])) {
-                        $history[$value] = 1;
                         $list[$count][$value] = 1;
+                        $history[$value] = 1;
+                    }
+                }
+                if ($value1 != 0 && $value2 % $value1 == 0) {
+                    $value = $value2 / $value1;
+                    if(!isset($history[$value])) {
+                        $list[$count][$value] = 1;
+                        $history[$value] = 1;
                     }
                 }
             }
+
+            if(isset($list[$count][$N])) break 3; //We have found the goal
         }         
     }
 
-    //We reach the goal
-    if(isset($list[$count][$N])) {
-        error_log(microtime(1) - $start);
-        exit("$count");
-    }
-    else ++$count;
+    ++$count;
 }
+
+echo $count . PHP_EOL;
+error_log(microtime(1) - $start);
