0a5d885828f37a567c329c9af0ad983bd4a11ba6
cedricdd <c-d-d@hotmail.fr>
2024-03-15 18:41:33 +0100

    Update code.php

--- Eggs/code.php
+++ Eggs/code.php
@@ -1,44 +1,30 @@
 <?php
 
 function placeEggs(int $position, int $nbrEggs, int $gridA, int $gridB) {
-    global $w, $h, $size, $results;
-    static $count = 0;
+    global $w, $h, $results;
 
     if($nbrEggs == 0) {
-        $pa = log($gridA & -$gridA) / log(2);
-        $pb = log($gridB & -$gridB) / log(2);
+        /** 
+         * x & -x sets every bits but the rightmost to 0, which is the first dice encountered by the player.
+         * We know the result is a power of 2 so we use binary logarithm to get the position of the bit set to 1.
+         */
+        $pa = log($gridA & -$gridA, 2);
+        $pb = log($gridB & -$gridB, 2);
 
-        if($pa == $pb) {
-            error_log("$count -- GridA: $gridA " . str_pad(decbin($gridA), $size, '0', STR_PAD_LEFT) . " -- " . $pa);
-            error_log("$count -- GridB: $gridB " . str_pad(decbin($gridB), $size, '0', STR_PAD_LEFT) . " -- " . $pb);
-        }
-
-        
-        ++$count;
-
-        if($pa < $pb) {
-            $results['A']++;
-            error_log("A wins");
-        }
-        elseif($pb < $pa) {
-            error_log("B wins");
-            $results['B']++;
-        }
-        else {
-            error_log("tie");
-            $results['T']++;
-        }
+        if($pa < $pb) $results['A']++; //Alice wins
+        elseif($pb < $pa) $results['B']++; //Bob wins
+        else $results['T']++; //It's a tie
 
         return;
     }
-    if($position == $size) return;
+    if($position == $w * $h) return;
 
     //Don't place an egg at $position
     placeEggs($position + 1, $nbrEggs, $gridA, $gridB);
-    
+
     //Place an egg at $position
-    $gridA |= (1 << (($position % $h) * $w + intdiv($position, $h)));
-    $gridB |= (1 << $position);
+    $gridA |= (1 << intdiv($position, $w) + ($position % $w) * $h); //Find the equivalent position for Alice
+    $gridB |= (1 << $position); //Bob follows the 'natural' path
 
     placeEggs($position + 1, $nbrEggs - 1, $gridA, $gridB);
 }
@@ -50,11 +36,8 @@ fscanf(STDIN, "%d", $w);
 fscanf(STDIN, "%d", $n);
 
 $results = ['A' => 0, 'B' => 0, 'T' => 0];
-$size = $w * $h;
 
 placeEggs(0, $n, 0, 0);
-
-error_log(var_export($results, true));
 
 $total = array_sum($results);
 echo number_format($results['A'] / $total * 100, 2) . "%" . PHP_EOL;
