29946dff0fcc71bf20abf2a15ce95f70cb4f0d12
cedricdd <c-d-d@hotmail.fr>
2024-04-18 18:08:04 +0200

    Create code.php

---
+++ Divide the factorial/code.php
@@ -0,0 +1,47 @@
+<?php
+
+//Find the largest porwer of $p that divides $fact! using Legendre’s formula
+function findPowerPrime(int $fact, int $p): int { 
+    $res = 0; 
+
+    while ($fact > 0) {         
+        $res += intval($fact / $p); 
+        $fact = intval($fact / $p); 
+    } 
+
+    return $res; 
+} 
+
+//Return the primes factor decompostion of an integer $n
+function primeFactors(int $n): array {
+    $factors = [];
+
+    while($n % 2 == 0) {
+        $factors[2] = ($factors[2] ?? 0) + 1;
+        $n /= 2;
+    }
+ 
+    for ($i = 3; $i <= sqrt($n); $i += 2) {
+        while ($n % $i == 0) {
+            $factors[$i] = ($factors[$i] ?? 0) + 1;
+            $n /= $i;
+        }
+    }
+
+    if($n > 2) $factors[$n] = 1;
+
+    return $factors;
+}
+
+fscanf(STDIN, "%d %d", $A, $B);
+
+$factors = primeFactors($A);
+
+$largestPower = INF;
+
+//https://en.wikipedia.org/wiki/Legendre%27s_formula
+foreach($factors as $prime => $exponent) {
+    $largestPower = min($largestPower, intval(findPowerPrime($B, $prime) / $exponent));
+}
+
+echo $largestPower . PHP_EOL;
