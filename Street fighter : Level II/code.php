90d23faf530ac9ff8f15842b93e6b4018ecceccb
cedricdd <c-d-d@hotmail.fr>
2024-05-05 16:57:28 +0200

    Update code.php

--- Street fighter : Level II/code.php
+++ Street fighter : Level II/code.php
@@ -1,44 +1,43 @@
 <?php
 
 const CHAMPIONS = [
-    "KEN" => ["Name" => "KEN", "Life" => 25, "PUNCH" => 6, "KICK" => 5, "Rage" => 0],
-    "RYU" => ["Name" => "RYU", "Life" => 25, "PUNCH" => 4, "KICK" => 5, "Rage" => 0],
-    "TANK" => ["Name" => "TANK", "Life" => 50, "PUNCH" => 2, "KICK" => 2, "Rage" => 0],
-    "VLAD" => ["Name" => "VLAD", "Life" => 30, "PUNCH" => 3, "KICK" => 3, "Rage" => 0],
-    "JADE" => ["Name" => "JADE", "Life" => 20, "PUNCH" => 2, "KICK" => 7, "Rage" => 0],
-    "ANNA" => ["Name" => "ANNA", "Life" => 18, "PUNCH" => 9, "KICK" => 1, "Rage" => 0],
-    "JUN" => ["Name" => "JUN", "Life" => 60, "PUNCH" => 2, "KICK" => 1, "Rage" => 0],
-    "VI" => ["Name" => "VI", "Life" => 35, "PUNCH" => 12, "KICK" => 0, "Rage" => 0],
-    "JINX" => ["Name" => "JINX", "Life" => 22, "PUNCH" => 4, "KICK" => 4, "Rage" => 0],
-    "TIFA" => ["Name" => "TIFA", "Life" => 38, "PUNCH" => 5, "KICK" => 5, "Rage" => 0],
-    "ZED" => ["Name" => "ZED", "Life" => 27, "PUNCH" => 4, "KICK" => 5, "Rage" => 0],
-    "SETT" => ["Name" => "SETT", "Life" => 39, "PUNCH" => 8, "KICK" => 0, "Rage" => 0],
+    "KEN" => ["Name" => "KEN", "Life" => 25, "PUNCH" => 6, "KICK" => 5, "Hits" => 0, "Rage" => 0],
+    "RYU" => ["Name" => "RYU", "Life" => 25, "PUNCH" => 4, "KICK" => 5, "Hits" => 0, "Rage" => 0],
+    "TANK" => ["Name" => "TANK", "Life" => 50, "PUNCH" => 2, "KICK" => 2, "Hits" => 0, "Rage" => 0],
+    "VLAD" => ["Name" => "VLAD", "Life" => 30, "PUNCH" => 3, "KICK" => 3, "Hits" => 0, "Rage" => 0],
+    "JADE" => ["Name" => "JADE", "Life" => 20, "PUNCH" => 2, "KICK" => 7, "Hits" => 0, "Rage" => 0],
+    "ANNA" => ["Name" => "ANNA", "Life" => 18, "PUNCH" => 9, "KICK" => 1, "Hits" => 0, "Rage" => 0],
+    "JUN" => ["Name" => "JUN", "Life" => 60, "PUNCH" => 2, "KICK" => 1, "Hits" => 0, "Rage" => 0],
+    "VI" => ["Name" => "VI", "Life" => 35, "PUNCH" => 12, "KICK" => 0, "Hits" => 0, "Rage" => 0],
+    "JINX" => ["Name" => "JINX", "Life" => 22, "PUNCH" => 4, "KICK" => 4, "Hits" => 0, "Rage" => 0],
+    "TIFA" => ["Name" => "TIFA", "Life" => 38, "PUNCH" => 5, "KICK" => 5, "Hits" => 0, "Rage" => 0],
+    "ZED" => ["Name" => "ZED", "Life" => 27, "PUNCH" => 4, "KICK" => 5, "Hits" => 0, "Rage" => 0],
+    "SETT" => ["Name" => "SETT", "Life" => 39, "PUNCH" => 8, "KICK" => 0, "Hits" => 0, "Rage" => 0],
 ];
 
-function getPermutations(array $fighters, array $permutation, array &$permutations) {
-    if(count($fighters) == 0) {
+//Generate all the order for the fighters in the team
+function getPermutations(array $fighters, int $count, array $permutation, array &$permutations) {
+    global $nbrInTeam; 
+
+    if($count == $nbrInTeam) {
         $permutations[] = $permutation;
         return;
     }
 
     foreach($fighters as $i => $fighter) {
-        $permutation[] = $fighters[$i];
+        $permutation[$count] = $fighters[$i];
 
         unset($fighters[$i]);
 
-        getPermutations($fighters, $permutation, $permutations);
+        getPermutations($fighters, $count + 1, $permutation, $permutations);
 
         $fighters[$i] = $fighter;
-
-        array_pop($permutation);
     }
 }
 
 function solve(array $team1, array $team2, array $attacks): int {
-    $hits1 = 0;
-    $hits2 = 0;
-    $hitsT1 = 0;
-    $hitsT2 = 0;
+    $hitsTeam1 = 0;
+    $hitsTeam2 = 0;
 
     foreach($attacks as $i => [$d, $attack]) {
         $attackedID = ($d == "<" ? "1" : "2");
@@ -63,7 +62,7 @@ function solve(array $team1, array $team2, array $attacks): int {
                     ${"team" . $attackedID}[0]["Rage"] = 0;
                     break;
                 case "JADE":
-                    $damage = ${'hits' . $attackerID} * ${"team" . $attackerID}[0]["Rage"];
+                    $damage = ${"team" . $attackerID}[0]["Hits"] * ${"team" . $attackerID}[0]["Rage"];
                     break;
                 case "ANNA":
                     $damage = (18 - ${"team" . $attackerID}[0]["Life"]) * ${"team" . $attackerID}[0]["Rage"];
@@ -94,41 +93,41 @@ function solve(array $team1, array $team2, array $attacks): int {
             ${"team" . $attackerID}[0]["Rage"] = 0;
         } else $damage = ${"team" . $attackerID}[0][$attack];
 
-        ${"team" . $attackedID}[0]["Life"] -= $damage;
-        ${"team" . $attackedID}[0]["Rage"]++;
-        ${"hits" . $attackerID}++;
+        ${"team" . $attackedID}[0]["Life"] -= $damage; //Aply damages
+        ${"team" . $attackedID}[0]["Rage"]++; //Increase rage
+        ${"team" . $attackerID}[0]["Hits"]++; //Increase hits
 
-        // error_log("$d $attack $attackerName -- $damage");
-        // error_log(var_export(implode(" ", $team1[0]), true));
-        // error_log(var_export(implode(" ", $team2[0]), true));
+        if($team1[0]["Life"] <= 0) { //A fighter in team 1 is dead
+            $hitsTeam1 += $team1[0]["Hits"];
 
-        if($team1[0]["Life"] <= 0) {
             array_shift($team1);
-            $hitsT1 += $hits1;
-            $hits1 = 0;
         }
-        if($team2[0]["Life"] <= 0) {
+        if($team2[0]["Life"] <= 0) { //A fighter in team 2 is dead
+            $hitsTeam2 += $team2[0]["Hits"];
+
             array_shift($team2);
-            $hitsT2 += $hits2;
-            $hits2 = 0;
         }
     
         if(count($team1) == 0 || count($team2) == 0) break; //All of the contestants of a team are dead
     }
 
-    $hitsT1 += $hits1;
-    $hitsT2 += $hits2;
+    if(count($team1)) $hitsTeam1 += $team1[0]["Hits"];
+    if(count($team2)) $hitsTeam2 += $team2[0]["Hits"];
 
-    if(count($team2) == 0) return $hitsT1;
-    else return PHP_INT_MAX;
+    if(count($team1)) return $hitsTeam1; //Team 1 won
+    else return PHP_INT_MAX; //Team 2 won
 }
 
 $start = microtime(1);
 
-[$t1, $t2] = explode(" ", trim(fgets(STDIN)));
+[$t1, $t2] = array_map(function($team) {
+    return explode(";", $team);
+}, explode(" ", trim(fgets(STDIN))));
+
+$nbrInTeam = count($t1);
 
 $permutations = [];
-getPermutations(explode(";", $t1), [], $permutations);
+getPermutations($t1, 0, [], $permutations);
 
 fscanf(STDIN, "%d", $N);
 for ($i = 0; $i < $N; $i++) {
@@ -139,8 +138,9 @@ for ($i = 0; $i < $N; $i++) {
 
 $solution = [PHP_INT_MAX, ""];
 
-foreach(explode(";", $t2) as $name) $team2[] = CHAMPIONS[$name];
+foreach($t2 as $name) $team2[] = CHAMPIONS[$name];
 
+//Test all the permutations of fighters
 foreach($permutations as $p1) {
     $team1 = [];
 
@@ -148,14 +148,11 @@ foreach($permutations as $p1) {
     
     $turnsToWin = solve($team1, $team2, $attacks);
 
-    error_log(implode(";", $p1) . " -- " . $turnsToWin);
-
     if($turnsToWin < $solution[0]) $solution = [$turnsToWin, implode(";", $p1)];
 }
 
-//Team 2 won
-if($solution[0] == PHP_INT_MAX) echo "We always lose" . PHP_EOL;
-//Team 1 won
-else echo $solution[1] . " beats " . $t2 . " in " . $solution[0] . " hits" . PHP_EOL;
+
+if($solution[0] == PHP_INT_MAX) echo "We always lose" . PHP_EOL; //Team 2 always wins
+else echo $solution[1] . " beat " . implode(";", $t2) . " in " . $solution[0] . " hits" . PHP_EOL; //Team 1 can win
 
 error_log(microtime(1) - $start);
