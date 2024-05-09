6343792e931a2117bc1730a62bdf862c47cfd0db
cedricdd <c-d-d@hotmail.fr>
2024-05-09 18:05:42 +0200

    Update code.php

--- Can you save the forest - Episode 1/code.php
+++ Can you save the forest - Episode 1/code.php
@@ -1,76 +1,41 @@
 <?php
 
-// $maxBurnedForest: the maximum number of forest tiles that you are allowed to let burn
 fscanf(STDIN, "%d", $maxBurnedForest);
 
-error_log("Max is $maxBurnedForest");
-
-function solve(array $fires, array $burnt, array $actions, string $hash) {
-    global $neighbors, $maxBurnedForest, $solution;
-    static $history = [];
-
-    $countBurnt = count($burnt);
-
-    // error_log("we currently have $countBurnt");
-    // error_log($hash);
-
-    if($countBurnt > $maxBurnedForest) return;
-    if($countBurnt >= $solution[0]) return;
-    if($solution[0] != INF) return;
-
-    if(count($fires) == 0) {
-        error_log("We have a solution with $countBurnt");
-        error_log(implode("-", $actions));
-        $solution = [$countBurnt, $actions];
-        return;
-    }
-
-    if(isset($history[$hash])) {
-        // error_log("using history -- $hash");
-        return;
-    }
-    else $history[$hash] = 1;
-
-
-    $countAction = count($actions);
-
-    foreach($fires as $index => $c) {
-        // error_log("using water on $index");
-
-        $actions[$countAction] = $index;
-
-        $hash2 = $hash;
-        $burnt2 = $burnt;
-        $fires2 = $fires;
-        
-        unset($fires2[$index]);
-        $hash2[$index] = '^';
-
-        foreach($fires2 as $index2 => $c2) {
-            if($c2 < 3) {
-                $fires2[$index2]++; 
-                $hash2[$index2] = ($c2 + 1);
-            }
-            else {
-                unset($fires2[$index2]);
-                $burnt2[$index2] = 1;
-                $hash2[$index2] = '*';
-
-                foreach($neighbors[$index2] as $nIndex) {
-                    if(isset($burnt2[$nIndex])) continue;
-                    if(isset($fires2[$nIndex])) continue;
-
-                    $fires2[$nIndex] = 1;
-                    $hash2[$nIndex] = '1';
-                }
+//Simulate the turn
+function simulateTurn(array $fires, int $indexWater, array $burnt, string $hash): array {
+    global $neighbors;
+    
+    //This fire is extinguish 
+    unset($fires[$indexWater]);
+    $hash[$indexWater] = '^';
+    
+    foreach($fires as $indexFire => $count) {
+        //Fire keeps evolving
+        if($count < 3) {
+            $fires[$indexFire]++;
+            $hash[$indexFire] = ($count + 1);
+        } //Fire spreads
+        else {
+            unset($fires[$indexFire]);
+            $burnt[$indexFire] = 1;
+            $hash[$indexFire] = '*';
+            
+            foreach($neighbors[$indexFire] as $nIndex) {
+                if(isset($burnt[$nIndex])) continue; //Position already burnt
+                if(isset($fires[$nIndex])) continue; //Position is already burning
+                
+                $fires[$nIndex] = 1;
+                $hash[$nIndex] = '1';
             }
         }
-
-        solve($fires2, $burnt2, $actions, $hash2);
     }
+    
+    return [$fires, $burnt, $hash];
 }
 
-$solution = [INF, []];
+$solutionBurnt = INF;
+$solutionActions = [];
 
 while (TRUE) {
     $start = microtime(1);
@@ -90,8 +55,9 @@ while (TRUE) {
 
             $index = $y * 10 + $x;
 
-            if(ctype_digit($map[$y][$x])) $fires[$index] = $map[$y][$x];
+            if(ctype_digit($map[$y][$x])) $fires[$index] = $map[$y][$x]; //There's a fire there
 
+            //Generate the neighbors positions
             if($x > 0 && $map[$y][$x - 1] != ".") $neighbors[$index][] = $index - 1;
             if($x < 9 && $map[$y][$x + 1] != ".") $neighbors[$index][] = $index + 1;
             if($y > 0 && $map[$y - 1][$x] != ".") $neighbors[$index][] = $index - 10;
@@ -99,14 +65,49 @@ while (TRUE) {
         }
     }
 
-    // error_log(var_export($neighbors, true));
-    // error_log(var_export($fires, true));
+    if(count($solutionActions) == 0) {
+        $checks = ["....^^.......^^1^......^3^^....^^.^^.^^.^1^^^^^1^^^^3^^^^^3^.^^.^^.^^....^^1^......^3^^.......^^...." => [$fires, [], []]];
+ 
+        $actionIndex = 0;
+        
+        while(count($checks)) {
+            $newChecks = [];
+            
+            foreach($checks as $hash => [$fires, $burnt, $actions]) {
+                //Check what happens if we extinguish the fire at fireIndex
+                foreach($fires as $fireIndex => $count) {
+                    [$fires2, $burnt2, $hash2] = simulateTurn($fires, $fireIndex, $burnt, $hash);
+            
+                    $actions[$actionIndex] = $fireIndex;
+                    $countBurnt = count($burnt2);
+                    $sumFires = array_sum($fires2);
+                    
+                    if($countBurnt >= $solutionBurnt) continue; //Current solution saves more positions, we can skip
+                    
+                    if($countBurnt <= $maxBurnedForest) {
+                        //All the fires are out
+                        if($sumFires == 0) {
+                            error_log("We have a solution with $countBurnt");
 
-    // exit();
+                            $solutionBurnt = $countBurnt;
+                            $solutionActions = $actions;
+                        } else $newChecks[$hash2] = [$fires2, $burnt2, $actions, $sumFires];
+                    }
+                }
+            }
+            
+            ++$actionIndex;
 
-    if(count($solution[1]) == 0) solve($fires, [], [], implode("", $map));
-
-    $index = array_shift($solution[1]);
+            //Sort by best scores
+            uasort($newChecks, function ($a, $b) {
+                return $a[3] <=> $b[3];
+            });
+            
+            $checks = array_slice($newChecks, 0, 250); //We only keep the 250 best
+        }
+    }
+    
+    $index = array_shift($solutionActions);
     echo ($index % 10) . " " . (intdiv($index, 10)) . PHP_EOL;
 
     error_log(microtime(1) - $start);
