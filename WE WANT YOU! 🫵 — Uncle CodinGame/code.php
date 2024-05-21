5a59462376b09cd86edeef56b5085c29e4fb9b02
cedricdd <c-d-d@hotmail.fr>
2024-05-21 22:56:40 +0200

    Create code.php

---
+++ WE WANT YOU! 🫵 — Uncle CodinGame/code.php
@@ -0,0 +1,25 @@
+<?php
+
+$forumKeyword = trim(fgets(STDIN));
+
+fscanf(STDIN, "%d", $n);
+
+for ($i = 0; $i < $n; $i++) {
+    [$name, $comment] = explode(":", trim(fgets(STDIN)));
+
+    if(!isset($users[$name])) $users[$name] = 0;
+
+    if(strpos($comment, $forumKeyword) === false) $users[$name]++;
+}
+
+$users = array_filter($users); //Remove users that are not getting banned
+
+if(count($users) == 0) echo "NO USERS TO BAN." . PHP_EOL;
+else {
+    foreach($users as $name => $count) {
+        if($count == 1) echo $name . " HAS BEEN TEMPORARILY BANNED." . PHP_EOL;
+    }
+    foreach($users as $name => $count) {
+        if($count > 1) echo $name . "'s ACCOUNT HAS BEEN PERMANENTLY SUSPENDED!" . PHP_EOL;
+    }
+}
