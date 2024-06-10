bc5d76cfd2fdbd799ddea79525c67405137a56e1
cedricdd <c-d-d@hotmail.fr>
2024-06-11 01:31:22 +0200

    Update code.php

--- Strobogrammatic Numbers/code.php
+++ Strobogrammatic Numbers/code.php
@@ -1,51 +1,48 @@
 <?php
 
-const SINGLE = [0, 1, 8];
-const PAIR = [[0, 0], [1, 1], [6, 9], [8, 8], [9, 6]];
-
+//Check if a single number is a Strobogrammatic Number
 function isStrobogrammiticNumber(string $number): int {
     if(strrev($number) == strtr($number, "01689", "01986")) return 1;
     else return 0;
 }
 
-function getCountTotal(int $size, bool $isInside = false): int {
+//Get how many Strobogrammatic Numbers of a given size exist
+function getCountTotal(int $size, bool $leadingZeroAllowed = false): int {
+    if($size <= 0) return 1;
+
     $count = (($size & 1) == 0) ? 1 : 3;
 
-    // error_log("start with $count -- $size");
-
     for($i = ($size >> 1); $i > 0; --$i) {
-        if($i == 1 && $isInside == false) $count *= 4;
+        if($i == 1 && $leadingZeroAllowed == false) $count *= 4; //We don't want '0' as first digit
         else $count *= 5;
     }
 
-    error_log("2 with $size with have " . $count);
     return $count;
 }
 
-function getCountUpTo(int $size, int $max, bool $isInside = false): int {
+//Get how many Strobogrammatic Numbers of a given size exist up to a given max
+function getCountUpTo(int $position, string $set, string $max): int {
     $count = 0;
-    $pair = [[0, 0], [1, 1], [6, 9], [8, 8], [9, 6]];
 
-    if($isInside) array_shift($pair);
+    $size = strlen($max) - ($position * 2); //How many positions are still 'open'
 
-    $firstDigit = strval($max)[0];
+    if($size <= 0) return intval($set) <= intval($max) ? 1 : 0; //We filled up all the positions
 
-    error_log("getCountUpTo - $size - $max - $firstDigit");
+    $full = getCountTotal($size - 2, true); 
 
-    $full = $size > 2 ? getCountTotal($size - 2, true) : 1;
+    foreach([[0, 0], [1, 1], [6, 9], [8, 8], [9, 6]] as $index => [$start, $end]) {
+        if($index == 0 && $position == 0) continue; //We don't want to '0' as first digit
+        if($size == 1 && $start != $end)  continue; //We only have 1 position not set
 
-    error_log("full is $full");
+        if($start < $max[$position]) $count += $full; //All these numbers will be below the max
+        //We are using the same digit as in the max number, need to call recursive to find how many are below the max
+        elseif($start == $max[$position]) {
+            $set[$position] = $start;
+            $set[($position + 1) * -1] = $end;
 
-    foreach($pair as [$start, $end]) {
-        if($start < $firstDigit) {
-            $count += $full;
-            error_log("$start < " . $firstDigit);
+            return $count + getCountUpTo($position + 1, $set, $max);
         }
-        elseif($start == $firstDigit) {
-            if($start == $max) $count += 1;
-            else $count += getCountUpTo($size - 2, intval(substr($max, 1, -1)));
-        }
-        elseif($start > $firstDigit) break;
+        else break; //All these numbers will be above the max
     }
 
     return $count;
@@ -54,31 +51,30 @@ function getCountUpTo(int $size, int $max, bool $isInside = false): int {
 $start = microtime(1);
 
 $low = stream_get_line(STDIN, 4096 + 1, "\n");
+$sLow = strlen($low);
 $high = stream_get_line(STDIN, 4096 + 1, "\n");
+$sHigh = strlen($high);
 
-error_log("$low $high");
-
+//We are only working on a single number
 if($low == $high) {
     echo isStrobogrammiticNumber(strval($low)) . PHP_EOL;
     exit();
 }
 
-$sLow = strlen($low);
-$sHigh = strlen($high);
-
-$total = getCountUpTo($sLow, $low, true) * -1;
-
-error_log("initial is $total");
+$total = 0; 
 
 //Add everything where we count all the numbers of size $size
 for($size = $sLow; $size < $sHigh; ++$size) {
     $check = getCountTotal($size);
 
     $total += $check;
-
-    error_log("size is $size -- check is $check -- total is $total");
 }
 
-echo $total + getCountUpTo($sHigh, $high, true) . PHP_EOL;
+//Remove all the number of the same size as min but lower than min
+$total -= getCountUpTo(0, str_repeat('0', $sLow), $low);
+//Add all the number of the same size as max but lower than max
+$total += getCountUpTo(0, str_repeat('0', $sHigh), $high);
+
+echo $total . PHP_EOL;
 
 error_log(microtime(1) - $start);
