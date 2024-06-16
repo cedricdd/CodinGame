c09be09c14077c32e7bf7c228b5ac4e865f11f96
cedricdd <c-d-d@hotmail.fr>
2024-06-16 19:00:22 +0200

    Create code.php

---
+++ Magic Coloring/code.php
@@ -0,0 +1,41 @@
+<?php
+
+fscanf(STDIN, "%d %d", $w, $h);
+for ($i = 0; $i < $h; $i++) {
+    $grid[] = trim(fgets(STDIN));
+}
+
+$colors = array_fill(1, 9, 0);
+
+for($y = 0; $y < $h; ++$y) {
+    for($x = 0; $x < $w; ++$x) {
+        if($grid[$y][$x] == 0) continue;
+
+        $value = $grid[$y][$x];
+        $toCheck = [[$x, $y]];
+
+        while($toCheck) {
+            [$x2, $y2] = array_pop($toCheck);
+
+            if($grid[$y2][$x2] == $value) {
+                $grid[$y2][$x2] = '0';
+
+                foreach([[0, 1], [0, -1], [1, 0], [-1, 0]] as [$xm, $ym]) {
+                    $xu = $x2 + $xm;
+                    $yu = $y2 + $ym;
+
+                    if($xu >= 0 && $xu < $w && $yu >= 0 && $yu < $h) $toCheck[] = [$xu, $yu];
+                }
+            }
+        }
+
+        $colors[$value] += 1;
+    }
+}
+
+$colors = array_filter($colors);
+
+if(count($colors) == 0) echo "No coloring today" . PHP_EOL;
+else array_walk($colors, function ($value, $key) {
+        echo "$key -> $value" . PHP_EOL;
+    });
