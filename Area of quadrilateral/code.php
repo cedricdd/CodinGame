0edf8c5fb28a12d710777c5f2d921e8d951845a5
cedricdd <c-d-d@hotmail.fr>
2024-06-29 22:12:36 +0200

    Create code.php

---
+++ Area of quadrilateral/code.php
@@ -0,0 +1,13 @@
+https://www.codingame.com/contribute/view/99279c5aecc380d8ba1fae9221690ab06b5b6
+
+Goal
+Given the (x, y) coordinates of four points on a Cartesian plane (in no particular order) that form a quadrilateral where each internal angle is smaller than 180 degrees, find the area of the quadrilateral, rounded down.
+
+Input
+Lines 1 to 4: Each line contains two space-separated integers x and y for the coordinates of one point of the quadrilateral.
+
+Output
+The area of the quadrilateral.
+
+Constraints
+-600 < x, y < 600
