daf47204dadc0dc93c1dfe6d447809b546f266dd
cedricdd <c-d-d@hotmail.fr>
2024-07-30 17:38:22 +0200

    Update code.php

--- Lazzie Come Home/code.php
+++ Lazzie Come Home/code.php
@@ -9,13 +9,12 @@ class MinPriorityQueue extends SplPriorityQueue {
     }
 }
 
-function solve(array $map, int $posX, int $posY, int $targetX, int $targetY): array {
+//Find the path from start to target
+function solve(array $map, int $posX, int $posY, int $targetX, int $targetY): string {
     $queue = new MinPriorityQueue();
-    $queue->insert([0, $posX, $posY, []], 0);
+    $queue->insert([0, $posX, $posY, ""], 0);
 
     $history = [];
-
-    error_log("Tartet: $targetX $targetY");
 
     while($queue->count()) {
         [$steps, $x, $y, $moves] = $queue->extract();
@@ -23,18 +22,14 @@ function solve(array $map, int $posX, int $posY, int $targetX, int $targetY): ar
         if(isset($history[$y][$x])) continue;
         else $history[$y][$x] = 1;
 
-        // error_log("we are at $x $y - $steps - " . implode('', $moves));
-
-
-        if($x == $targetX && $y == $targetY) return array_reverse($moves);
+        if($x == $targetX && $y == $targetY) return $moves;
 
         foreach(MOVES as $d => [$xm, $ym]) {
             $xu = $x + $xm;
             $yu = $y + $ym;
-            $moves[$steps] = $d;
 
             if(($map[$yu][$xu] ?? '.') != '#') {
-                $queue->insert([$steps + 1, $xu, $yu, $moves], $steps + abs($xu - $targetX) + abs($yu - $targetY));
+                $queue->insert([$steps + 1, $xu, $yu, $moves . $d], $steps + abs($xu - $targetX) + abs($yu - $targetY));
             }
         }
     }
@@ -44,21 +39,20 @@ function solve(array $map, int $posX, int $posY, int $targetX, int $targetY): ar
 
 fscanf(STDIN, "%d %d %d", $visionRange, $goalX, $goalY);
 
-error_log("$visionRange - $goalX $goalY");
-$range = $visionRange >> 1;
-
 $posX = 0;
 $posY = 0;
 $map = [];
 $moves = [];
 $unknown = [];
+$range = $visionRange >> 1;
 
 while (TRUE) {
     for ($y = -$range; $y <= $range; ++$y) {
         $line = trim(fgets(STDIN));
         
         foreach(str_split($line) as $x => $c) {
-            $map[$posY + $y][$posX - $range + $x] = $c;
+            //Positions outside vision don't give any info and can be previously known positions
+            if($c != '?') $map[$posY + $y][$posX - $range + $x] = $c;
         }
 
         error_log($line);
@@ -67,19 +61,19 @@ while (TRUE) {
     $start = microtime(1);
 
     error_log("We are at $posX $posY");
+    error_log("Tartet: $targetX $targetY");
 
     $moves = solve($map, $posX, $posY, $goalX, $goalY);
 
-    $move = array_pop($moves);
-
-    switch($move) {
+    //Move player
+    switch($moves[0]) {
         case 'N':   $posY--;  break;
         case 'S':   $posY++;  break;
         case 'W':   $posX--;  break;
         case 'E':   $posX++;  break;
     }
 
-    echo $move . PHP_EOL;
+    echo $moves . PHP_EOL;
 
     error_log(microtime(1) - $start);
 }
