3680b43b3d972fc0346643b47ae713743c4aade4
cedricdd <c-d-d@hotmail.fr>
2024-08-02 19:24:54 +0200

    Update code.php

--- Pixel Polygons/code.php
+++ Pixel Polygons/code.php
@@ -1,39 +1,29 @@
 <?php
 
-const CHECK = [[1, 0, 1, -1], [0, 1, 1, 1], [-1, 0, -1, 1], [0, -1, -1, -1]];
+$map = "";
+$corners = 0;
 
 fscanf(STDIN, "%d", $N);
 for ($i = 0; $i < $N; $i++) {
-    $map[] = trim(fgets(STDIN));
+    $map .= trim(fgets(STDIN));
 }
 
-error_log(var_export($map, true));
+//We are searching for all the corners, we have 1 side for each corner
+$patterns = [
+    "(?<=\.\..{" . ($N - 2) . "})#(?=\.)",
+    "#(?=\..{" . ($N - 2) . "}\.\.)",
+    "(?<=\.)#(?=.{" . ($N - 2) . "}\.\.)",
+    "(?<=\.\..{" . ($N - 2) . "}\.)#",
+    "#(?=#.{" . ($N - 2) . "}#\.)",
+    "#(?=\..{" . ($N - 2) . "}##)",
+    "#(?=#.{" . ($N - 2) . "}\.#)",
+    "(?<=\.)#(?=.{" . ($N - 2) . "}##)",
+];
 
-//Find the first # starting at top left
-$position = strpos(implode("", $map), '#');
+foreach($patterns as $pattern) {
+    preg_match_all("/" . $pattern . "/", $map, $matches);
 
-[$x, $y] = [$position % $N, intdiv($position, $N)];
-
-error_log("$x $y");
-
-$d = 0;
-$sides = 0;
-$history = [];
-
-while(true) {
-    if(isset($history["$x $y $d"])) break;
-    else $history["$x $y $d"] = 1;
-
-    [$x1, $y1, $x2, $y2] = CHECK[$d];
-
-    while($map[$y + $y1][$x + $x1] == '#' && $map[$y + $y2][$x + $x2] == '.') {
-        $x += $x1;
-        $y += $y1;
-    }
-
-    ++$sides;
-    $d = ($d + 1) % 4;
-    error_log("$x $y $d");
+    $corners += count($matches[0]);
 }
 
-echo $sides . PHP_EOL;
+echo $corners . PHP_EOL;
