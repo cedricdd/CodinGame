fee77c7e71d54e9c4c9e1e5e382ec1f8365d48db
cedricdd <c-d-d@hotmail.fr>
2024-08-11 23:40:36 +0200

    Update code.php

--- Einstein's riddle solver/code.php
+++ Einstein's riddle solver/code.php
@@ -1,102 +1,67 @@
 <?php
 
-function updateOutput(string $attribute, int $index, string $characteristic, int $value) {
-    global $output;
-
-    // error_log("setting $attribute with $characteristic - $value");
+//We are updating the combinaisons, using a relation
+function updateOutput(string $header, int $index, string $characteristic, int $value) {
+    global $combinaison, $headers;
 
     if($value == 1) {
-        $output[$attribute][$index] = [$characteristic => 1];
+        $combinaison[$header][$index] = [$characteristic => 1];
 
-        // error_log("$characteristic IS with $attribute");
+        foreach($headers as $header2) {
+            if($header == $header2) continue;
 
-        foreach($output as $attribute2 => $filler) {
-            if($attribute == $attribute2 || !isset($output[$attribute2][$index][$characteristic])) continue;
-
-            unset($output[$attribute2][$index][$characteristic]);
-
-            if(count($output[$attribute2][$index]) == 1) {
-                $characteristic2 = array_key_first($output[$attribute2][$index]);
-
-                // error_log("$characteristic2 IS only left for $attribute2");
-
-                updateOutput($attribute2, $index, $characteristic2, 1);
-            }
+            updateOutput($header2, $index, $characteristic, 0); //This characteristic can't be associated with anything else
         }
+    } elseif(isset($combinaison[$header][$index][$characteristic])) {
+        unset($combinaison[$header][$index][$characteristic]);
 
-        applyRelations($attribute, $characteristic);
-    } elseif(isset($output[$attribute][$index][$characteristic])) {
-        unset($output[$attribute][$index][$characteristic]);
-
-        // error_log("$characteristic is NOT with $attribute");
-
-        if(count($output[$attribute][$index]) == 1) {
-            $characteristic2 = array_key_first($output[$attribute][$index]);
-
-            // error_log("$characteristic2 IS only left for $attribute");
-
-            updateOutput($attribute, $index, $characteristic2, 1);
+        //We know only have 1 characteristic left
+        if(count($combinaison[$header][$index]) == 1) {
+            updateOutput($header, $index, array_key_first($combinaison[$header][$index]), 1);
         }
     }
 }
 
-function applyRelations(string $attribute, string $name) {
-    global $indexes, $characteristics, $output;
-    static $history = [];
-
-    if(isset($history[$attribute][$name])) return;
-    else $history[$attribute][$name] = 1;
-
-    error_log("!!!!!using $name on $attribute");
-
-    foreach($characteristics[$name] as $characteristic => $value) {
-        $index = $indexes[$characteristic];
-
-        if($index == -1) continue;
-
-        updateOutput($attribute, $index, $characteristic, $value);
-    }
-}
-
-function isNotWith(array &$characteristics, array &$history, string $a, string $b, bool $debug =false) {
-    // if($debug) error_log("$a is NOT with $b");
-
+//We know that $a is not associated with $b
+function isNotWith(array &$characteristics, string $a, string $b) {
     if(isset($characteristics[$a][$b])) return;
     else $characteristics[$a][$b] = 0;
 
     foreach($characteristics[$b] as $c => $value) {
         if($c == $a) continue;
 
+        //Everything associated with $b will also NOT be associatd with $a
         if($value == 1) {
-            isNotWith($characteristics, $history, $a, $c, $debug);
-            isNotWith($characteristics, $history, $c, $a, $debug);
+            isNotWith($characteristics, $a, $c);
+            isNotWith($characteristics, $c, $a);
         }
     }
 }
 
-function isWith(array &$characteristics, array &$history, string $a, string $b, bool $debug =false) {
-    // if($debug) error_log("$a is with $b");
-
+//We know that $a is associated with $b
+function isWith(array &$characteristics, string $a, string $b) {
     if(isset($characteristics[$a][$b])) return;
     else $characteristics[$a][$b] = 1;
 
     foreach($characteristics[$b] as $c => $value) {
         if($c == $a) continue;
 
+        //Everything associated with $b will also BE associatd with $a
         if($value == 1){
-            isWith($characteristics, $history, $a, $c, $debug);
-            isWith($characteristics, $history, $c, $a, $debug);
-        }
+            isWith($characteristics, $a, $c);
+            isWith($characteristics, $c, $a);
+        } //Everything NOT associated with $b will also NOT be associatd with $a
         else {
-            isNotWith($characteristics, $history, $a, $c, $debug);
-            isNotWith($characteristics, $history, $c, $a, $debug);
+            isNotWith($characteristics, $a, $c);
+            isNotWith($characteristics, $c, $a);
         }
     }
 }
 
 $start = microtime(1);
 
-$links = [];
+$history = [];
+$forbidden = [];
 $indexes = [];
 $characteristics = [];
 
@@ -113,59 +78,123 @@ for ($i = 0; $i < $nbCharacteristics; $i++) {
     $infos[] = array_flip($info);
 }
 
-// error_log(var_export($characteristics, true));
-$test = 4;
-
 fscanf(STDIN, "%d", $nbLinks);
 for ($i = 0; $i < $nbLinks; $i++) {
     preg_match("/(.*) ([&!]) (.*)/", trim(fgets(STDIN)), $link);
 
     [, $a, $op, $b] = $link;
 
-    // error_log("$a $op $b");
-
-    $history = [];
-
     if($op == '&') {
-        isWith($characteristics, $history, $a, $b, $i == $test ? 1 : 0);
-        isWith($characteristics, $history, $b, $a, $i == $test ? 1 : 0);
+        isWith($characteristics, $a, $b);
+        isWith($characteristics, $b, $a);
     }
     else {
-        isNotWith($characteristics, $history, $a, $b, $i == $test ? 1 : 0);
-        isNotWith($characteristics, $history, $b, $a, $i == $test ? 1 : 0);
+        isNotWith($characteristics, $a, $b);
+        isNotWith($characteristics, $b, $a);
+    }
+}
+
+//We use the first category as header
+$headers = array_keys(array_shift($infos));
+$default = array_fill(0, $nbPeople, $infos);
+$combinaison = array_combine($headers, $default);
+
+ksort($combinaison);
+
+foreach($headers as $header) {
+    foreach($characteristics[$header] as $characteristic => $value) {
+        //We use every relations we know with the header
+        updateOutput($header, $indexes[$characteristic], $characteristic, $value);
+    }
+}
+
+while(true) {
+    $finished = true;
+    $improved = false;
+    $failed = false;
+
+    foreach($combinaison as $header => $list) {
+        foreach($list as $index => $possibilities) {
+            $count = count($possibilities);
+
+            //We have nothing left, one of the guess we make was wrong
+            if($count == 0) {
+                $failed = true;
+                break 2;
+            } elseif($count > 1) {
+                foreach($possibilities as $possibility => $filler) {
+                    //Check if we can drop the possibility
+                    foreach($characteristics[$possibility] as $characteristic => $value) {
+                        $index2 = $indexes[$characteristic];
+
+                        //We need this characteristic
+                        if($value == 1 && !isset($combinaison[$header][$index2][$characteristic])) {
+                            updateOutput($header, $index, $possibility, 0); //We can drop that characteristic
+
+                            $improved = true;
+                        }
+                        
+                        //We can't have this characteristic as certain
+                        if($value == 0 && isset($combinaison[$header][$index2][$characteristic]) && count($combinaison[$header][$index2]) == 1) {
+                            updateOutput($header, $index, $possibility, 0); //We can drop that characteristic
+
+                            $improved = true;
+                        }
+                    }
+
+                    //If we can't use that characteristic anywhere else, it has to go here
+                    foreach($headers as $header2) {
+                        if($header == $header2) continue;
+
+                        if(isset($combinaison[$header2][$index][$possibility])) continue 2;
+                    }
+
+                    updateOutput($header, $index, $possibility, 1); //We can drop that characteristic
+
+                    $improved = true;
+                }
+
+                if(count($possibilities) > 1) $finished = false;
+            }
+        }
     }
 
-    // if($i == $test - 1 || $i == $test) {
-    //     foreach($characteristics as $t => $l) {
-    //         $c = [];
-    //         foreach($l as $p => $m) {
-    //             $c[] = $p . " - " . $m;
-    //         }
-
-    //         error_log("$t " . implode(" | ", $c)) . PHP_EOL;
-    //     }
-
-    //     error_log("\n\n");
-    // }
-}
-
-foreach($characteristics as $t => $l) {
-    $c = [];
-    foreach($l as $p => $m) {
-        $c[] = $p . " - " . $m;
+    if($finished) break;
+    elseif($failed) { //We need to reload and make another guess
+        [$combinaison, $forbidden] = array_pop($history);
+        $improved = false;
     }
+    
+    //We need to make a guess
+    if(!$improved) {
+        foreach($combinaison as $headerName => $list) {
+            foreach($list as $index => $possibilities) {
+                //We can make a guess here
+                if(count($possibilities) > 1) {
+                    foreach($possibilities as $possibility => $filler) {
+                        if(!isset($forbidden[$headerName][$possibility])) {
+                            $forbidden[$headerName][$possibility] = 1;
 
-    error_log("$t " . implode(" | ", $c)) . PHP_EOL;
+                            $history[] = [$combinaison, $forbidden];
+
+                            updateOutput($headerName, $index, $possibility, 1); //We can drop that characteristic
+
+                            break 3;
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
 }
 
-foreach($infos[0] as $attribute => $filler) {
-    $output[$attribute] = array_slice($infos, 1);
+$output[] = implode(" ", array_keys($combinaison));
+
+for($i = 0; $i < $nbCharacteristics - 1; ++$i) {
+    $output[] = implode(" ", array_map("array_key_first", array_column($combinaison, $i)));
 }
 
-foreach($output as $attribute => $filler) applyRelations($attribute, $attribute);
+echo implode("\n", $output);
 
-// ksort($output);
-
-// error_log(var_export($output, true));
-
-error_log(microtime(1) - $start);
+error_log("\n" . (microtime(1) - $start));
