3d86f9cfea662ebea3270d1c5e2f8420862a6f56
cedricdd <c-d-d@hotmail.fr>
2024-08-21 21:25:37 +0200

    Update code.php

--- Find the gradient/code.php
+++ Find the gradient/code.php
@@ -6,12 +6,21 @@ for ($i = 0; $i < $h; $i++) {
     $pavement[] = stream_get_line(STDIN, 100 + 1, "\n");
 }
 
+error_log(var_export($pavement, true));
+
 //Find pattern on width
 for($pw = 1; $pw <= $w; ++$pw) {
     if($w % $pw != 0) continue; //We know that we only have full pattern 
 
-    if(count(array_unique(str_split($pavement[0], $pw))) == 1) break;
+    //Check if it works for all the lines
+    for($y = 0; $y < $h; ++$y) {
+        if(count(array_unique(str_split($pavement[$y], $pw))) != 1) continue 2;
+    }
+
+    break;
 }
+
+error_log("width $pw");
 
 foreach($pavement as &$line) $line = substr($line, 0, $pw);
 
