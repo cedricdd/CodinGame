e0024b4281ce8c362ed3680b1b94a8886fe68ec0
cedricdd <c-d-d@hotmail.fr>
2024-09-26 22:06:19 +0200

    Update code.php

--- Wave Function Collapse/code.php
+++ Wave Function Collapse/code.php
@@ -6,111 +6,87 @@ fscanf(STDIN, "%d %d", $W1, $H1);
 for ($i = 0; $i < $H1; $i++) {
     $line = trim(fgets(STDIN));
 
-    if($i > 0 && $i < $H1 - 1) {
-        $characters += array_flip(str_split(substr($line, 1, -1)));
-    }
+    //We save all the possible characters for the unknown positions
+    if($i > 0 && $i < $H1 - 1) $characters += array_flip(str_split(substr($line, 1, -1)));
 
     $prototype[] = $line;
 }
 
-error_log(var_export($characters, true));
+//Generate all the patches
+for($y = 1; $y < $H1 - 1; ++$y) {
+    for($x = 1; $x < $W1 - 1; ++$x) {
+        $patch = $prototype[$y - 1][$x - 1] . $prototype[$y - 1][$x] . $prototype[$y - 1][$x + 1] 
+            . $prototype[$y][$x - 1] . $prototype[$y][$x] . $prototype[$y][$x + 1] 
+            . $prototype[$y + 1][$x - 1] . $prototype[$y + 1][$x] . $prototype[$y + 1][$x + 1];
 
-for($y = 1; $y < $H1 - 3; ++$y) {
-    for($x = 1; $x < $W1 - 3; ++$x) {
-        $patch = [];
-
-        for($y2 = 0; $y2 < 3; ++$y2) {
-            for($x2 = 0; $x2 < 3; ++$x2) {
-                $patch[$y2][$x2] = $prototype[$y + $y2][$x + $x2];
-            }
-        }
-
-        $patches[] = $patch;
+        $patches[$patch] = $patch; //We don't need duplicate patches
     }
 }
 
-// error_log(var_export($patches, true));
+$patches = array_values($patches);
+$patchesIDs = array_keys($patches);
 
 fscanf(STDIN, "%d %d", $W2, $H2);
 for ($i = 0; $i < $H2; $i++) {
     $partial [] = str_split(trim(fgets(STDIN)));
 }
 
-// error_log(var_export($partial, true));
-
+//Set all the possibilites for each positions
 for($y = 0; $y < $H2; ++$y) {
     for($x = 0; $x < $W2; ++$x) {
         $possibilities[$y * $W2 + $x] = $partial[$y][$x] == '?' ? $characters : [$partial[$y][$x] => 1];
-    }
-}
 
-// error_log(var_export($possibilities, true));
+        //At this position we will try to constrain patches
+        if($y > 0 && $y < $H2 - 1 && $x > 0 && $x < $W2 - 1) {
+            $index = $y * $W2 + $x;
 
-// ksort($possibilities);
-// $test = "";
-// foreach($possibilities as $list) {
-//     if(count($list) > 1) $test .= "?";
-//     else $test .= array_key_first($list);
-// }
-
-// echo implode(PHP_EOL, str_split($test, $W2)) . PHP_EOL;
-
-$testi = 0;
-
-while($testi++ < 10) {
-    for($y = 1; $y <= $H2 - 3; ++$y) {
-        for($x = 1; $x < $W2 - 3; ++$x) {
-            // if($x == 1 && $y == 2){
-            //     error_log("at $x $y");
-            // }
-    
-            $updatedPossibilities = [];
-    
-            //TODO skip is all unknown
-            //TODO saves possible patches starting at each positions
-            foreach($patches as $index => $patch) {
-                // if($x == 1 && $y == 2) {
-                //     error_log("testing $index");
-                //     foreach($patch as $y2 => $line) {
-                //         error_log(implode("", $line));
-                //     }
-                // }
-
-                foreach($patch as $y2 => $line) {
-                    for($x2 = 0; $x2 < 3; ++$x2) {
-                        if(!isset($possibilities[($y + $y2) * $W2 + $x + $x2][$line[$x2]])) {
-                            // if($x == 1 && $y == 2) {
-                            //     error_log("$x2 $y2 should be \"" . $line[$x2] . "\"");
-                            //     error_log(var_export($possibilities[($y + $y2) * $W2 + $x + $x2], true));
-                            // }
-                            
-                            continue 3;
-                        }
-                    }
-                }
-    
-                // if($x == 1 && $y == 2) error_log("at $x $y with can have:");
-                // if($x == 1 && $y == 2) error_log(var_export($patch, true));
-    
-                foreach($patch as $y2 => $line) {
-                    for($x2 = 0; $x2 < 3; ++$x2) {
-                        $updatedPossibilities[($y + $y2) * $W2 + $x + $x2][$line[$x2]] = 1;
-                    }
-                }
-    
-                // error_log(var_export($updatedPossibilities, true));
-            }
-    
-            $possibilities = $updatedPossibilities + $possibilities;
+            $checks[$index] = [
+                $patchesIDs, 
+                [
+                    $index - $W2 - 1, $index - $W2, $index - $W2 + 1, 
+                    $index - 1, $index, $index + 1, 
+                    $index + $W2 - 1, $index + $W2, $index + $W2 + 1, 
+                ]
+            ];
         }
     }
 }
 
-ksort($possibilities);
-$test = "";
-foreach($possibilities as $list) {
-    if(count($list) > 1) $test .= "?";
-    else $test .= array_key_first($list);
-}
+do {
+    $reduced = false;
 
-echo implode(PHP_EOL, str_split($test, $W2)) . PHP_EOL;
+    foreach($checks as $test => [$patchesIDs, $positions]) {
+        $count = count($patchesIDs);
+
+        if($count <= 1) continue; //No need to work on this position anymore
+        else {
+            $updatedPossibilities = [];
+
+            //Check all the patches that can still be placed here
+            foreach($patchesIDs as $id) {
+                $patch = $patches[$id];
+
+                for($index = 0; $index < 9; ++$index) {
+                    //We know the patch can't go here
+                    if(!isset($possibilities[$positions[$index]][$patch[$index]])) {
+                        unset($checks[$test][0][$id]); //No need to check this patch here anymore
+
+                        $reduced = true;
+                        
+                        continue 2;
+                    }
+                }
+
+                //This patch can go here, constrain the symbols
+                for($index = 0; $index < 9; ++$index) $updatedPossibilities[$positions[$index]][$patch[$index]] = 1;
+            }
+
+            //The new constrains replace the old ones for the positions we worked on
+            $possibilities = $updatedPossibilities + $possibilities;
+        }
+    }
+} while($reduced);
+
+ksort($possibilities); //We need to sort in proper order to display
+
+echo implode(PHP_EOL, str_split(implode("", array_map("array_key_first", $possibilities)), $W2)) . PHP_EOL;
