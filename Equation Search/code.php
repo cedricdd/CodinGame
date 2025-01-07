0304d9fc2eca61f3662728f09c3caad4e9bf8ac9
cedricdd <c-d-d@hotmail.fr>
2025-01-07 18:40:36 +0100

    Update code.php

--- Equation Search/code.php
+++ Equation Search/code.php
@@ -1,5 +1,29 @@
 <?php
 
+const POSSIBILITIES = [
+    4  => [[1, 3, '+'], [2, 2, '+'], [1, 4, 'x'], [2, 2, 'x']],
+    5  => [[1, 4, '+'], [2, 3, '+'], [1, 5, 'x']],
+    6  => [[1, 5, '+'], [2, 4, '+'], [3, 3, '+'], [1, 6, 'x'], [2, 3, 'x']],
+    7  => [[1, 6, '+'], [2, 5, '+'], [3, 4, '+'], [1, 7, 'x']],
+    8  => [[1, 7, '+'], [2, 6, '+'], [3, 5, '+'], [4, 4, '+'], [1, 8, 'x'], [2, 4, 'x']],
+    9  => [[1, 8, '+'], [2, 7, '+'], [3, 6, '+'], [4, 5, '+'], [1, 9, 'x'], [3, 3, 'x']],
+    10 => [[1, 9, '+'], [2, 8, '+'], [3, 7, '+'], [4, 6, '+'], [5, 5, '+'], [2, 5, 'x']],
+    11 => [[2, 9, '+'], [3, 8, '+'], [4, 7, '+'], [5, 6, '+']],
+    12 => [[3, 9, '+'], [4, 8, '+'], [5, 7, '+'], [6, 6, '+'], [2, 6, 'x'], [3, 4, 'x']],
+    13 => [[4, 9, '+'], [5, 8, '+'], [6, 7, '+']],
+    14 => [[5, 9, '+'], [6, 8, '+'], [7, 7, '+'], [2, 7, 'x']],
+    15 => [[6, 9, '+'], [7, 8, '+'], [3, 5, 'x']],
+    16 => [[7, 9, '+'], [8, 8, '+'], [2, 8, 'x'], [4, 4, 'x']],
+    17 => [[8, 9, '+']],
+    18 => [[9, 9, '+'], [2, 9, 'x'], [3, 6, 'x']],
+    20 => [[4, 5, 'x']],
+    21 => [[3, 7, 'x']],
+    24 => [[3, 8, 'x'], [4, 6, 'x']],
+    25 => [[5, 5, 'x']],
+    27 => [[3, 9, 'x']],
+    28 => [[4, 7, 'x']],
+];
+
 function solve(array $digits, int $index, array $equations): int {
     global $results, $right, $nbrEquations;
 
@@ -9,8 +33,9 @@ function solve(array $digits, int $index, array $equations): int {
 
     if(isset($history[$hash][$index])) return $history[$hash][$index];
 
+    //We have created all the equations
     if($index == $nbrEquations) {
-        $results = $equations;
+        $results = $equations; //We only want to display it if there's only 1 set, we don't of overwritting in other cases
 
         return 1;
     }
@@ -18,37 +43,21 @@ function solve(array $digits, int $index, array $equations): int {
     $goal = $right[$index];
     $total = 0;
 
-    // error_log("working on $goal -- $index");
-
-    foreach($digits as $d => $c) {
-        $diff = $goal - $d;
+    //Check all the ways to reach the goal
+    foreach(POSSIBILITIES[$goal] as [$d1, $d2, $sign]) {
+        if(!isset($digits[$d1]) || !isset($digits[$d2])) continue; //We need one of both
 
         $digits2 = $digits;
+        
+        if($digits2[$d1] == 1) unset($digits2[$d1]);
+        else $digits2[$d1]--;
 
-        if($digits2[$d] == 1) unset($digits2[$d]);
-        else $digits2[$d]--;
+        //If d1 == d2 we might not have another one left
+        if(isset($digits2[$d2])) {
+            if($digits2[$d2] == 1) unset($digits2[$d2]);
+            else $digits2[$d2]--;
 
-        if($diff >= $d && isset($digits2[$diff])) {
-            if($digits2[$diff] == 1) unset($digits2[$diff]);
-            else $digits2[$diff]--;
-
-            $total += solve($digits2, $index + 1, $equations + [$index => "$d + $diff = $goal"]);
-        }
-
-        if($goal % $d == 0) {
-            $div = $goal / $d;
-
-            $digits2 = $digits;
-
-            if($digits2[$d] == 1) unset($digits2[$d]);
-            else $digits2[$d]--;
-
-            if($div >= $d && isset($digits2[$div])) {
-                if($digits2[$div] == 1) unset($digits2[$div]);
-                else $digits2[$div]--;
-    
-                $total += solve($digits2, $index + 1, $equations + [$index => "$d x $div = $goal"]);
-            }
+            $total += solve($digits2, $index + 1, $equations + [$index => "$d1 $sign $d2 = $goal"]);
         }
     }
 
