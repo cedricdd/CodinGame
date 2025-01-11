87f596fb7fafbf85e3e21bd1e84fc913c22f71eb
cedricdd <c-d-d@hotmail.fr>
2025-01-11 19:34:14 +0100

    Update code.php

--- Ye_ An_th_r W_rd Se_rch/code.php
+++ Ye_ An_th_r W_rd Se_rch/code.php
@@ -1,10 +1,11 @@
 <?php
 
 function display(array $placements) {
-    global $positions, $width, $height;
+    global $positions, $width, $height, $start;
 
     $output = array_fill(0, $height, str_repeat(" ", $width));
 
+    //Place all the words
     foreach($placements as $placementID => $filler) {
         foreach($positions[$placementID] as $position => $letter) {
             $output[intdiv($position, $width)][$position % $width] = $letter;
@@ -12,10 +13,20 @@ function display(array $placements) {
     }
 
     echo implode(PHP_EOL, $output) . PHP_EOL;
+
+    error_log(microtime(1) - $start);
+
+    exit();
 }
 
-function solve(array $counts, array $placements, array $used) {
+function solve(array $counts, array $placementsPossible, array $placementsUsed) {
     global $conflicts;
+
+    if(!$counts) {
+        display($placementsUsed);
+
+        return;
+    }
 
     //Work on the word with the less possible placement
     $wordID = null;
@@ -28,35 +39,23 @@ function solve(array $counts, array $placements, array $used) {
         }
     }
 
-    if($wordID === null) {
-        error_log("we are done");
-        error_log(var_export($used, 1));
-
-        display($used);
-
-        return;
-    }
-
-    foreach($placements[$wordID] as $placementID1 => $filler1) {
-        // error_log("working on $wordID - $placementID1");
-
+    foreach($placementsPossible[$wordID] as $placementID1 => $filler1) {
         $counts2 = $counts;
-        $placements2 = $placements;
+        $placementsPossible2 = $placementsPossible;
 
         unset($counts2[$wordID]);
-        unset($placements2[$wordID]);
+        unset($placementsPossible2[$wordID]);
 
+        //Remove all the placements that are no longer possible
         foreach(($conflicts[$placementID1] ?? []) as $placementID2 => $wordID2) {
-            // error_log("we can't use $placementID2 for $wordID2 anymore");
-
-            if(isset($placements2[$wordID2][$placementID2])) {
+            if(isset($placementsPossible2[$wordID2][$placementID2])) {
                 if($counts2[$wordID2]-- == 0) continue 2;
                 
-                unset($placements2[$wordID2][$placementID2]);
+                unset($placementsPossible2[$wordID2][$placementID2]);
             }
         }
 
-        solve($counts2, $placements2, $used + [$placementID1 => 1]);
+        solve($counts2, $placementsPossible2, $placementsUsed + [$placementID1 => 1]);
     }
 }
 
@@ -74,120 +73,54 @@ $placements = [];
 $positions = [];
 $conflicts = [];
 $counts = [];
-$ID = 0;
+$placementID = 0;
 
 foreach(explode(" ", trim(fgets(STDIN))) as $index => $word) {
     $l = strlen($word);
 
-    //Horizontal - Right
-    for($y = 0; $y < $height; ++$y) {
-        for($x = 0; $x <= $width - $l; ++$x) {
-            $pos = [];
+    $directions = [
+        [0, $height, 0, $width - $l + 1, 0, 1], //Horizontal, left to right
+        [0, $height, $l - 1, $width, 0, -1], //Horizontal, right to left
+        [0, $height - $l + 1, 0, $width, 1, 0], //Vertical, top to bottom
+        [$l - 1, $height, 0, $width, -1, 0], //Vertical, bottom to top
+        [0, $height - $l + 1, 0, $width - $l + 1, 1, 1], //Diagonal, to bottom-right
+        [0, $height - $l + 1, $l - 1, $width, 1, -1], //Diagonal, to bottom-left
+        [$l - 1, $height, 0, $width - $l + 1, -1, 1], //Diagonal, to top-right
+        [$l - 1, $height, $l - 1, $width, -1, -1], //Diagonal, to top-left
+    ];
 
-            for($i = 0; $i < $l; ++$i) {
-                if($grid[$y][$x + $i] != '.' && $grid[$y][$x + $i] != $word[$i]) continue 2;
+    foreach($directions as $test => [$minY, $maxY, $minX, $maxX, $cy, $cx]) {
+        //Check all the starting positions for that direction
+        for($y = $minY; $y < $maxY; ++$y) {
+            for($x = $minX; $x < $maxX; ++$x) {
+                $pos = [];
+    
+                //Check if the word can be written here
+                for($i = 0; $i < $l; ++$i) {
 
-                $pos[$y * $width + $x + $i] = $word[$i];
+                    //There's a letter that doesn't match
+                    if($grid[$y + ($cy * $i)][$x + ($cx * $i)] != '.' && $grid[$y + ($cy * $i)][$x + ($cx * $i)] != $word[$i]) continue 2;
+    
+                    $pos[($y + ($cy * $i)) * $width + $x + ($cx * $i)] = $word[$i];
+                }
+    
+                //We have found a place to add this word
+                $placements[$index][$placementID] = 1;
+    
+                foreach($pos as $i => $c) {
+                    $positions[$placementID][$i] = $c;
+                    $lettersByPosition[$i][$placementID] = $c;
+                }
+    
+                $wordByPlacementID[$placementID] = $index;
+    
+                ++$placementID;
             }
-
-            // error_log("$word can start at $x $y - horizontal");
-
-            $placements[$index][$ID] = 1;
-
-            foreach($pos as $i => $c) {
-                $positions[$ID][$i] = $c;
-                $lettersByPosition[$i][$ID] = $c;
-            }
-
-            $wordByPlacementID[$ID] = $index;
-
-            ++$ID;
-        }
-    }
-
-    //Horizontal - Left
-    for($y = 0; $y < $height; ++$y) {
-        for($x = $width - 1; $x >= $l - 1; --$x) {
-            $pos = [];
-
-            for($i = 0; $i < $l; ++$i) {
-                if($grid[$y][$x - $i] != '.' && $grid[$y][$x - $i] != $word[$i]) continue 2;
-
-                $pos[$y * $width + $x - $i] = $word[$i];
-            }
-
-            // error_log("$word can start at $x $y - horizontal");
-
-            $placements[$index][$ID] = 1;
-
-            foreach($pos as $i => $c) {
-                $positions[$ID][$i] = $c;
-                $lettersByPosition[$i][$ID] = $c;
-            }
-
-            $wordByPlacementID[$ID] = $index;
-
-            ++$ID;
-        }
-    }
-
-    //Vertical - Down
-    for($y = 0; $y <= $height - $l; ++$y) {
-        for($x = 0; $x < $width; ++$x) {
-            $pos = [];
-
-            for($i = 0; $i < $l; ++$i) {
-                if($grid[$y + $i][$x] != '.' && $grid[$y + $i][$x] != $word[$i]) continue 2;
-
-                $pos[($y + $i) * $width + $x] = $word[$i];
-            }
-
-            // error_log("$word can start at $x $y - vertical");
-
-            $placements[$index][$ID] = 1;
-
-            foreach($pos as $i => $c) {
-                $positions[$ID][$i] = $c;
-                $lettersByPosition[$i][$ID] = $c;
-            }
-
-            $wordByPlacementID[$ID] = $index;
-
-            ++$ID;
-        }
-    }
-
-    //Vertical - Up
-    for($y = $height - 1; $y >= $l - 1; --$y) {
-        for($x = 0; $x < $width; ++$x) {
-            $pos = [];
-
-            for($i = 0; $i < $l; ++$i) {
-                if($grid[$y - $i][$x] != '.' && $grid[$y - $i][$x] != $word[$i]) continue 2;
-
-                $pos[($y - $i) * $width + $x] = $word[$i];
-            }
-
-            // error_log("$word can start at $x $y - vertical");
-
-            $placements[$index][$ID] = 1;
-
-            foreach($pos as $i => $c) {
-                $positions[$ID][$i] = $c;
-                $lettersByPosition[$i][$ID] = $c;
-            }
-
-            $wordByPlacementID[$ID] = $index;
-
-            ++$ID;
         }
     }
 }
 
-// error_log(var_export($placements, 1));
-// error_log(var_export($positions, 1));
-// error_log(var_export($lettersByPosition, 1));
-
+//Find all the placements that become impossible once you place a given word
 foreach($placements as $wordID => $list) {
     foreach($list as $placementID1 => $filler) {
         foreach($positions[$placementID1] as $positionIndex => $letter1) {
@@ -200,8 +133,4 @@ foreach($placements as $wordID => $list) {
     $counts[$wordID] = count($list);
 }
 
-// error_log(var_export($conflicts, 1));
-
 solve($counts, $placements, []);
-
-error_log(microtime(1) - $start);
