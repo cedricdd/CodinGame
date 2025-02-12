49b477f167a1d9a05e1773acd059791f3ab8db71
cedricdd <c-d-d@hotmail.fr>
2025-02-13 00:45:27 +0100

    Update code.php

--- Binary Extension/code.php
+++ Binary Extension/code.php
@@ -1,9 +1,9 @@
 <?php
 
-
-function findPath() {
+function findPath(): array {
     global $width, $height, $bombs, $goals, $n;
 
+    $solutions = [[]];
     $root = $width >> 1;
 
     //The root is a node we need to use
@@ -14,15 +14,16 @@ function findPath() {
         $forcedNodes[$index] = 1;
     }
 
-    foreach($goals as $index => $filler) {
-        $paths = [[$index => 1]];
-        $solutions = [];
-        $nodeUSed = count($forcedNodes);
+    foreach($goals as $indexGoal => $filler) {
+        $paths = [[[$indexGoal => 1], count($forcedNodes)]];
+        $goalSolutions = [];
+
+        error_log("starting with " . count($forcedNodes));
 
         while($paths) {
             $newPaths = [];
 
-            foreach($paths as $path) {
+            foreach($paths as [$path, $nodeUsed]) {
                 $index = array_key_last($path);
                 $x = $index % $width;
                 $y = intdiv($index, $width);
@@ -30,33 +31,36 @@ function findPath() {
                 // error_log("we are at $x $y");
 
                 if($y == 0) {
-                    if($x == $root) $solutions[] = $path;
+                    if($x == $root) $goalSolutions[] = $path;
                     continue;
                 }
 
+                if($nodeUsed > $n) continue; //We don't have enough number for this path
+
                 //Left
                 $indexL = $index - $width - 1;
-                if($x > 0 && !isset($bombs[$indexL])) $newPaths[] = $path + [$indexL => 1];
+                if($x > 0 && !isset($bombs[$indexL])) $newPaths[] = [$path + [$indexL => 1], $nodeUsed + (isset($forcedNodes[$indexL]) ? 0 : 1)];
                 //Right
                 $indexR = $index - $width + 1;
-                if($x < $width - 1 && !isset($bombs[$indexR]))  $newPaths[] = $path + [$indexR => 1];
-                
+                if($x < $width - 1 && !isset($bombs[$indexR]))  $newPaths[] = [$path + [$indexR => 1], $nodeUsed + (isset($forcedNodes[$indexR]) ? 0 : 1)];
             }
 
             $paths = $newPaths;
         }
 
-        error_log("for $x $y");
-        error_log(var_export($solutions, 1));
+        // error_log("for $indexGoal");
+        // error_log(var_export($goalSolutions, 1));
+
+        $counts = [];
 
         //Any nodes in all the solutions is a forced node;
-        foreach($solutions as $path) {
+        foreach($goalSolutions as $path) {
             foreach($path as $index => $filler) {
                 @$counts[$index]++;
             }
         }
 
-        $countSolution = count($solutions);
+        $countSolution = count($goalSolutions);
 
         foreach($counts as $index => $count) {
             if($count == $countSolution) {
@@ -64,7 +68,24 @@ function findPath() {
                 $forcedNodes[$index] = 1;
             }
         }
+
+        //We merge the solution of this goal with our existing solutions
+        $newSolutions = [];
+
+        foreach($solutions as $s1) {
+            foreach($goalSolutions as $s2) {
+                $mergedSolution = $s1 + $s2;
+
+                if(count($mergedSolution) <= $n) $newSolutions[] = $mergedSolution;
+            }
+        }
+
+        $solutions = $newSolutions;
+
+        // error_log(var_export($solutions, 1));
     }
+
+    return $solutions;
 }
 
 fscanf(STDIN, "%d %d %d %d %d", $width, $height, $n, $bombsCount, $goalsCount);
@@ -87,7 +108,11 @@ for ($i = 0; $i < $goalsCount; $i++) {
     $goals[$y * $width + $x] = 1;
 }
 
-error_log(var_export($bombs, 1));
-error_log(var_export($goals, 1));
+// error_log(var_export($bombs, 1));
+// error_log(var_export($goals, 1));
 
-findPath();
+$paths = findPath();
+
+error_log(var_export($paths, 1));
+
+error_log(microtime(1) - $start);
