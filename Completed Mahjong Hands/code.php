4d67f66ec165da2f49ad320088992fb3e9e72aba
cedricdd <c-d-d@hotmail.fr>
2025-03-06 22:12:40 +0100

    Update code.php

--- Completed Mahjong Hands/code.php
+++ Completed Mahjong Hands/code.php
@@ -9,47 +9,44 @@ function addTiles(string $tiles, array &$hand) {
     }
 }
 
+function checkWinning(int $pairs, int $sets, array $hand) {
+    foreach($hand as $suit => $info) {
+        foreach($info as $digit => &$count) {
+            if($hand[$suit][$digit] == 0) continue;
+    
+            //We are making a set with 3 tiles in a connected run
+            if($suit != 'z' && $digit <= 7 && $hand[$suit][$digit] && $hand[$suit][($digit + 1)] && $hand[$suit][($digit + 2)]) {
+                $hand2 = $hand;
+                $hand2[$suit][$digit]--;
+                $hand2[$suit][$digit + 1]--;
+                $hand2[$suit][$digit + 2]--;
+
+                checkWinning($pairs, $sets + 1, $hand2);
+            }
+    
+            switch($hand[$suit][$digit]) {
+                case 0: continue 2;
+                case 1:
+                    if($suit != 'z' && !($digit == 1 || $digit == 9)) return;
+                    break;
+                case 2: ++$pairs; break;
+                case 3: ++$sets; break;
+                default: return;
+            }
+
+            $hand[$suit][$digit] = 0;
+        }
+    }
+
+    if(($sets == 4 && $pairs == 1) || $pairs == 7 || $pairs == 1) exit("TRUE") . PHP_EOL;
+}
+
 $hand = ['m' => array_fill(1, 9, 0), 'p' => array_fill(1, 9, 0), 's' => array_fill(1, 9, 0), 'z' => array_fill(1, 7, 0)];
 
 preg_match_all("/([1-9]+[mpsz])/", trim(fgets(STDIN)), $matches);
 
 foreach($matches[0] as $tiles) addTiles($tiles, $hand);
 
-error_log(var_export($hand, 1));
+checkWinning(0, 0, $hand); //Check if the hand is a winning hand
 
-$pairs = 0;
-$sets = 0;
-
-foreach($hand as $suit => $info) {
-    foreach($info as $digit => &$count) {
-        if($hand[$suit][$digit] == 0) continue;
-
-        if($suit != 'z' && $digit <= 7) {
-            while($hand[$suit][$digit] && $hand[$suit][($digit + 1)] && $hand[$suit][($digit + 2)]) {
-                $sets++;
-
-                $hand[$suit][$digit]--;
-                $hand[$suit][$digit + 1]--;
-                $hand[$suit][$digit + 2]--;
-            }
-        }
-
-        switch($hand[$suit][$digit]) {
-            case 0: continue 2;
-            case 1:
-                if($suit != 'z' && !($digit == 1 || $digit == 9)) {
-                    error_log("We have $suit - $digit - " . $hand[$suit][$digit]);
-                    exit("FALSE");
-                }
-                break;
-            case 2: ++$pairs; break;
-            case 3: ++$sets; break;
-            default: exit("FALSE");
-        }
-    }
-}
-
-error_log("$pairs $sets");
-
-if(($sets == 4 && $pairs == 1) || $pairs == 7 || $pairs == 1) echo "TRUE" . PHP_EOL;
-else echo "FALSE" . PHP_EOL;
+echo "FALSE" . PHP_EOL;
