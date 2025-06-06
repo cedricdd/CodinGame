be1e8f88fd8ec956b50f41e7ff841bebd80b56e5
cedricdd <c-d-d@hotmail.fr>
2025-06-06 21:31:38 +0200

    Update code.php

--- Smash or Pass/code.php
+++ Smash or Pass/code.php
@@ -1,31 +1,56 @@
 <?php
 
+const MOD = 1000000007;
+
+$start = microtime(1);
+
 fscanf(STDIN, "%d %d", $n, $k);
-$inputs = explode(" ", trim(fgets(STDIN)));
+$inputs = array_map('intval', explode(" ", trim(fgets(STDIN))));
 
 rsort($inputs);
 
-error_log(var_export($inputs, 1));
+$list = [0 => [0 => [0 => 1]]];
 
-$list = [[0, 0, 0]];
-
-foreach($inputs as $input) {
-    error_log("working on $input");
+foreach($inputs as $i => $input) {
     $newList = [];
 
-    foreach($list as [$smashed, $total, $left]) {
-        //We can choose not to smash it
-        if($total + $input <= $k) {
-            $newList[] = [$smashed, $total + $input, $left + 1];
-        }
+    foreach($list as $smashed => $list2) {
+        foreach($list2 as $skipped => $list3) {
+            foreach($list3 as $total => $count) {
+                //We can choose not to smash it
+                if($total + $input + $smashed <= $k) {
+                    $newList[$smashed][$skipped + 1][$total + $input + $smashed] = ($newList[$smashed][$skipped + 1][$total + $input + $smashed] ?? 0) + $count;
+                }
 
-        //Invalid we can't do anything with it
-        if($total + $left <= $k) {
-            $newList[] = [$smashed + 1, $total + $left, $left];
+                //We can choose to skip it
+                if($total + $skipped <= $k) {
+                    $newList[$smashed + 1][$skipped][$total + $skipped] = ($newList[$smashed + 1][$skipped][$total + $skipped] ?? 0) + $count;
+                }
+            }
         }
     }
 
     $list = $newList;
 }
 
-error_log(var_export($list, 1));
+$biggest = 0;
+$ways = 0;
+
+//Find the biggest size and how many ways we can reach it
+foreach($list as $smashed => $list2) {
+    foreach($list2 as $skipped => $list3) {
+        foreach($list3 as $total => $count) {
+            if($total > $biggest) {
+                $biggest = $total;
+                $ways = $count % MOD;
+            } elseif($total == $biggest) {
+                $ways += $count;
+                $ways %= MOD;
+            }
+        }
+    }
+}
+
+echo $biggest . " " . $ways . PHP_EOL;
+
+error_log(microtime(1) - $start);
