6d7746183d578ec55ee4c509a2e17c4b897097c8
cedricdd <c-d-d@hotmail.fr>
2025-06-17 14:57:09 +0200

    Create code.php

---
+++ City Lights Part 1/code.php
@@ -0,0 +1,34 @@
+<?php
+
+$toCharacters = array_merge(range(0, 9), range('A', 'Z'));
+$toValues = array_flip($toCharacters);
+
+fscanf(STDIN, "%d", $w);
+fscanf(STDIN, "%d", $h);
+
+$output = array_fill(0, $h, array_fill(0, $w, 0));
+
+for ($y = 0; $y < $h; $y++) {
+    foreach(str_split(trim(fgets(STDIN))) as $x => $c) {
+        if($c !== '.') {
+            $v = $toValues[$c];
+
+            for($y2 = max(0, $y - $v); $y2 < min($y + $v, $h); ++$y2) {
+                for($x2 = max(0, $x - $v); $x2 < min($x + $v, $w); ++$x2) {
+                    $output[$y2][$x2] += max(0, $v - round(sqrt((($x - $x2) ** 2) + (($y - $y2) ** 2))));
+                    $output[$y2][$x2] = min($output[$y2][$x2], 35);
+                }
+            }
+        }
+    }
+}
+
+foreach($output as $values) {
+    $line = "";
+
+    for($i = 0; $i < $w; ++$i) {
+        $line .= $toCharacters[$values[$i]];
+    }
+
+    echo $line . PHP_EOL;
+}
