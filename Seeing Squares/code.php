994907dfb28cc13e2abc9ea4f7bfb4172b61948d
cedricdd <c-d-d@hotmail.fr>
2025-08-17 21:22:17 +0200

    Update code.php

--- Seeing Squares/code.php
+++ Seeing Squares/code.php
@@ -1,22 +1,28 @@
 <?php
 
 $rows = "";
+$start = microtime(1);
 
 fscanf(STDIN, "%d %d", $r, $c);
 for ($i = 0; $i < $r; $i++) {
-    $rows .= stream_get_line(STDIN, $c + 1, "\n");
+    $rows .= stream_get_line(STDIN, $c + 1, "\n") . '#';
 }
 
-error_log(var_export(str_split($rows, $c), 1));
+foreach(str_split($rows, $c) as $line) error_log(var_export($line, 1));
 
+$max = min($c, $r * 2);
+++$c; //We add a character for the rexeg to clearly separate lines
 $count = 0;
 
-preg_match_all("/(?=\+---\+.{". ($c - 5) ."}\|.{3}\|.{". ($c - 5) ."}\+---\+)/", $rows, $matches);
+//Test all the possible size of square
+for($i = 3; $i <= $max; $i += 2) {
+    $pattern = "/(?=\+[\+\-]{" . ($i - 2) . "}\+.{". ($c - $i) ."}" . str_repeat("[\+\|].{" . ($i - 2) . "}[\+\|].{". ($c - $i) ."}", (($i >> 1) - 1)) . "\+[\+\-]{" . ($i - 2) . "}\+)/";
 
-$count += count($matches[0]);
+    preg_match_all($pattern, $rows, $matches);
 
-preg_match_all("/(?=\+---[\+\-]---\+.{". ($c - 9) ."}\|.{7}\|.{". ($c - 9) ."}[\+\|].{7}[\+\|].{". ($c - 9) ."}\|.{7}\|.{". ($c - 9) ."}\+---[\+\-]---\+)/", $rows, $matches);
-
-$count += count($matches[0]);
+    $count += count($matches[0]);
+}
 
 echo $count . PHP_EOL;
+
+error_log(microtime(1) - $start);
