3894ba06f6a31c7eb5236a38d336b30d63c8d056
cedricdd <c-d-d@hotmail.fr>
2025-08-23 19:48:22 +0200

    Add initial implementation of Connect the Colours game

---
+++ Connect the Colours - Part 1/code.php
@@ -0,0 +1,63 @@
+<?php
+
+fscanf(STDIN, "%d %d", $h, $w);
+
+$grid = [];
+$index = 0;
+$neighbors = [];
+$size = $h * $w;
+$start = microtime(1);
+
+for ($y = 0; $y < $h; ++$y) {
+    $line = stream_get_line(STDIN, $w + 1, "\n");
+
+    foreach(str_split($line) as $c) {
+        if($c != '.' && $c != 'H' && $c != 'V' && $c != 'X') $colors[$c] = [];
+    }
+
+    $grid[] = $line;
+}
+
+error_log(var_export($grid, 1));
+
+for($index = 0, $y = 0; $y < $h; ++$y) {
+    for($x = 0; $x < $w; ++$x, ++$index) {
+        $c = $grid[$y][$x];
+
+        if($x > 0) {
+            $left = $grid[$y][$x - 1];
+            if($left != 'X' && $left != 'V' && $c != 'V' && (!isset($colors[$c]) || !isset($colors[$left]) || $c == $left)) $neighbors[$index]['L'] = $index - 1;
+        }
+        if($x < $w - 1) {
+            $right = $grid[$y][$x + 1];
+            if($right != 'X' && $right != 'V' && $c != 'V' && (!isset($colors[$c]) || !isset($colors[$right]) || $c == $right)) $neighbors[$index]['R'] = $index + 1;
+        }
+        if($y > 0) {
+            $up = $grid[$y - 1][$x];
+            if($up != 'X' && $up != 'H' && $c != 'H' && (!isset($colors[$c]) || !isset($colors[$up]) || $c == $up)) $neighbors[$index]['U'] = $index - $w;
+        }
+        if($y < $h - 1) {
+            $down = $grid[$y + 1][$x];
+            if($down != 'X' && $down != 'H' && $c != 'H' && (!isset($colors[$c]) || !isset($colors[$down]) || $c == $down)) $neighbors[$index]['D'] = $index + $w;
+        }
+    }
+}
+
+// error_log(var_export($neighbors, 1));
+
+$checkpoints = [];
+
+fscanf(STDIN, "%d", $k);
+for ($i = 0; $i < $k; $i++) {
+    fscanf(STDIN, "%d %d %d", $x, $y, $c);
+
+    error_log("$x $y $c");
+
+    $checkpoints[$y * $w + $x] = $c;
+}
+
+error_log(var_export($checkpoints, 1));
+
+while (TRUE) {
+    echo("x1 y1 x2 y2 colour_identifier\n");
+}
