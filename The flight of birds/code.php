5a2609178b172af2fb01ed09438b3ac8fc8db18f
cedricdd <c-d-d@hotmail.fr>
2025-09-10 22:16:54 +0200

    Refactor bird collision detection logic

--- The flight of birds/code.php
+++ The flight of birds/code.php
@@ -1,6 +1,12 @@
 <?php
 
-function checkBirdsCollide(array &$birds, int $id1, int $id2, float $d): ?float {
+const PRECISION = 12;
+const THRESHOLD = 10 ** -12;
+
+/**
+ * Check if two birds are going to be at a distance of $d of each others
+ */
+function checkBirdsColliding(array &$birds, int $id1, int $id2, float $d): ?float {
     [$x1, $y1, $vx1, $vy1] = $birds[$id1];
     [$x2, $y2, $vx2, $vy2] = $birds[$id2];
 
@@ -9,36 +15,25 @@ function checkBirdsCollide(array &$birds, int $id1, int $id2, float $d): ?float 
     $vx = $vx2 - $vx1;
     $vy = $vy2 - $vy1;
 
-    // error_log("x $x - y $y - vx $vx - vy $vy");
-
     $a = $vx * $vx + $vy * $vy;
     $b = 2 * ($x * $vx + $y * $vy);
     $c = $x * $x + $y * $y - $d * $d;
 
-    // error_log("$a - $b - $c");
-
     if($a > 0) {
         $delta = $b * $b - 4 * $a * $c;
 
-        // error_log("delta is $delta");
-
-        if($delta == 0) {
-            $t = ($b * -1) / (2 * $a);
-
-            // error_log("delta is 0 => $t is $t");
-
-            if($t > 0) return $t;
-        } elseif($delta > 0) {
+        if($delta > 0) {
             $t1 = (($b * -1) + sqrt($delta)) / (2 * $a);
             $t2 = (($b * -1) - sqrt($delta)) / (2 * $a);
 
-            // error_log("T1 $t1 - T2 $t2");
+            //Make sure float doesn't mess up a 0 value
+            if($t1 > 0.0 && $t1 < THRESHOLD) $t1 = 0.0;;
+            if($t2 > 0.0 && $t2 < THRESHOLD) $t2 = 0.0;;
 
-            if($t1 > 0 && $t2 <= 0) return $t1;
-            if($t2 > 0 && $t1 <= 0) return $t2;
-            if($t1 > 0 && $t2 > 0)  return min($t1, $t2);
+            //If one of the value is negative the birds are not heading to each others
+            if($t1 >= 0.0 && $t2 >= 0.0)  return min($t1, $t2);
         }
-    } else error_log("$a is 0!!");
+    } 
 
     return null;
 }
@@ -57,111 +52,151 @@ function getTimeBeforeReachingBorder(array &$birds, int $id, string $borderID): 
     $dy = $y2 - $y1;
     $L = sqrt($dx * $dx + $dy * $dy);
 
-    // cross product (b-a) x (p0 - a)
     $cross0 = $dx * ($y - $y1) - $dy * ($x - $x1);
-    // cross product (b-a) x v
     $crossV = $dx * $vy - $dy * $vx;
 
     $target = $d * $L;
     
-    $t1 = ($target - $cross0) / $crossV;
-    $t2 = (-$target - $cross0) / $crossV;
+    $time = min(($target - $cross0) / $crossV, (-$target - $cross0) / $crossV);
 
-    // error_log("$id will be at $d from $borderID at $t1 & $t2");
+    //Make sure float doesn't mess up a 0 value
+    if($time < THRESHOLD) $time = 0.0;;
 
-    return min($t1, $t2);
+    return $time;
 }
 
 function solve(array $birds, int $timeLeft, float $d) {
-    $totalTime = 0.0;
+    $collisionBird = 0;
+    $collisionBorder = 0;
+    $history = [];
+    $start = microtime(1);
 
     while(true) {
-        foreach($birds as [$x, $y, $vx, $vy]) error_log("$x $y -- $vx $vy");
-    
-        $firstCollision = [INF];
+        $collisionTime = INF;
+        $collisions = [];
 
         //Find how long before each birds hits a border
-        foreach($birds as $id => [$x, $y, $vx, $vy]) {
+        foreach($birds as $id1 => [$x, $y, $vx, $vy]) {
             //Bird will hit the left wall
             if($vx < 0) {
-                $t = getTimeBeforeReachingBorder($birds, $id, 'L');
+                $t = getTimeBeforeReachingBorder($birds, $id1, 'L');
 
-                if($t < $firstCollision[0]) $firstCollision = [$t, $id, 'L'];
+                if($t < $collisionTime) {
+                    $collisionTime = $t;
+                    $collisions = [[$id1, 'L']];
+                } elseif($t == $collisionTime) $collisions[] = [$id1, 'L'];
             }
             //Bird will hi the right wall
             if($vx > 0) {
-                $t = getTimeBeforeReachingBorder($birds, $id, 'R');
+                $t = getTimeBeforeReachingBorder($birds, $id1, 'R');
 
-                if($t < $firstCollision[0]) $firstCollision = [$t, $id, 'R'];
+                if($t < $collisionTime) {
+                    $collisionTime = $t;
+                    $collisions = [[$id1, 'R']];
+                } elseif($t == $collisionTime) $collisions[] = [$id1, 'R'];
             }
             //Bird will hit the top wall
             if($vy < 0) {
-                $t = getTimeBeforeReachingBorder($birds, $id, 'T');
+                $t = getTimeBeforeReachingBorder($birds, $id1, 'T');
 
-                if($t < $firstCollision[0]) $firstCollision = [$t, $id, 'T'];
+                if($t < $collisionTime) {
+                    $collisionTime = $t;
+                    $collisions = [[$id1, 'T']];
+                } elseif($t == $collisionTime) $collisions[] = [$id1, 'T'];
             }
             //Bird will hit the bottom wall
             if($vy > 0) {
-                $t = getTimeBeforeReachingBorder($birds, $id, 'B');
+                $t = getTimeBeforeReachingBorder($birds, $id1, 'B');
 
-                if($t < $firstCollision[0]) $firstCollision = [$t, $id, 'B'];
+                if($t < $collisionTime) {
+                    $collisionTime = $t;
+                    $collisions = [[$id1, 'B']];
+                } elseif($t == $collisionTime) $collisions[] = [$id1, 'B'] ;
             }
 
             foreach($birds as $id2 => $filler) {
-                if($id2 > $id) {
-                    $t = checkBirdsCollide($birds, $id, $id2, $d);
-                    $t = round($t, 8);
+                if($id2 > $id1) {
+                    $t = checkBirdsColliding($birds, $id1, $id2, $d);
 
-                    // error_log("Checing $id & $id2 => T is $t");
-
-                    if($t && $t < $firstCollision[0]) $firstCollision = [$t, $id, $id2];
+                    // The birds are going to going to be too close from each others
+                    if($t !== null) {
+                        if($t < $collisionTime) {
+                            $collisionTime = $t;
+                            $collisions = [[$id1, $id2]];
+                        } elseif($t == $collisionTime) $collisions[] = [$id1, $id2] ;
+                    }
                 }
             }
         }
 
-        error_log(var_export($firstCollision, 1));
+        //We need to stop before the next collision
+        if($collisionTime > $timeLeft) $collisionTime = $timeLeft;
 
-        [$t, $id1, $id2] = $firstCollision;
+        //Update the position of the birds
+        if($collisionTime > 0.0) {
+            foreach($birds as $id => [$x, $y, $vx, $vy]) {
+                $birds[$id][0] += $vx * $collisionTime;
+                $birds[$id][1] += $vy * $collisionTime;
+            }
 
-        if($t > $timeLeft) $t = $timeLeft;
-
-        foreach($birds as $id => [$x, $y, $vx, $vy]) {
-            $birds[$id][0] += $vx * $t;
-            $birds[$id][1] += $vy * $t;
+            $timeLeft -= $collisionTime;
         }
 
-        $timeLeft -= $t;
-
-        if($timeLeft == 0) {
-            error_log("We are over");
+        //We reached the moment of the shot
+        if($timeLeft == 0.0) {
+            error_log("We are over - Border Collision: $collisionBorder - Birds Collision: $collisionBird - In " . (microtime(1) - $start));
             foreach($birds as $id => [$x, $y]) echo $id . " [" . round($x, 0, PHP_ROUND_HALF_DOWN) . "," . round($y, 0, PHP_ROUND_HALF_DOWN) . "]" . PHP_EOL;
             exit();
         }
 
-        //Hitting a border
-        if(!is_numeric($id2)) {
-            switch($id2) {
-                case 'T':
-                case 'B': $birds[$id1][3] *= -1; break;
-                case 'L':
-                case 'R': $birds[$id1][2] *= -1; break;
+        $hashInfo = [];
+
+        foreach($collisions as [$id1, $id2]) {
+            //Collision with border
+            if(!is_numeric($id2)) {
+                switch($id2) {
+                    case 'T':
+                    case 'B': $birds[$id1][3] *= -1; break;
+                    case 'L':
+                    case 'R': $birds[$id1][2] *= -1; break;
+                }
+
+                ++$collisionBorder;
+            } //Collision with two birds
+            else {
+                //The reflection is done with the line passing by the current position of the two birds
+                $line = [$birds[$id2][0] - $birds[$id1][0], $birds[$id2][1] - $birds[$id1][1]];
+                $len = sqrt($line[0] * $line[0] + $line[1] * $line[1]);
+                $line = [$line[0] / $len, $line[1] / $len];
+
+                //Update the speed of the first bird
+                $dotProduct = $birds[$id1][2] * $line[0] + $birds[$id1][3] * $line[1];
+
+                $birds[$id1][2] -= 2 * $dotProduct * $line[0];
+                $birds[$id1][3] -= 2 * $dotProduct * $line[1];
+
+                //Update the speed of the second bird
+                $dotProduct = $birds[$id2][2] * $line[0] + $birds[$id2][3] * $line[1];
+
+                $birds[$id2][2] -= 2 * $dotProduct * $line[0];
+                $birds[$id2][3] -= 2 * $dotProduct * $line[1];
+
+                ++$collisionBird;
             }
-        } else {
-            //The reflection is done with the line passing by the current position of the two birds
-            $line = [$birds[$id2][0] - $birds[$id1][0], $birds[$id2][1] - $birds[$id1][1]];
-            $len = sqrt($line[0] * $line[0] + $line[1] * $line[1]);
-            $line = [$line[0] / $len, $line[1] / $len];
 
-            $dotProduct = $birds[$id1][2] * $line[0] + $birds[$id1][3] * $line[1];
+            if($id1 > $id2) [$id1, $id2] = [$id2, $id1];
 
-            $birds[$id1][2] -= 2 * $dotProduct * $line[0];
-            $birds[$id1][3] -= 2 * $dotProduct * $line[1];
-
-            $dotProduct = $birds[$id2][2] * $line[0] + $birds[$id2][3] * $line[1];
-
-            $birds[$id2][2] -= 2 * $dotProduct * $line[0];
-            $birds[$id2][3] -= 2 * $dotProduct * $line[1];
+            $hashInfo[$id1 . "-" . $id2] = [$birds[$id1][0] ?? $id1, $birds[$id1][1] ?? $id1, $birds[$id2][0] ?? $id2, $birds[$id2][1] ?? $id2];
+            
         }
+
+        //Generate hash to detect infinite loop
+        ksort($hashInfo);
+
+        $hash = hash('sha256', json_encode($hashInfo));
+
+        if(isset($history[$timeLeft][$hash])) exit("No movement possible!"); // We are in a loop, we can't reach the moment of the shot
+        else $history[$timeLeft][$hash] = 1;
     }
 }
 
@@ -169,8 +204,6 @@ fscanf(STDIN, "%d %d", $h, $w);
 fscanf(STDIN, "%f", $t);
 fscanf(STDIN, "%f", $d);
 fscanf(STDIN, "%d", $n);
-
-if($t != intval($t)) exit("T is not an int!");
 
 error_log("SIZE: $w * $h - TIME: $t - DIST $d");
 
@@ -188,7 +221,5 @@ $borders = [
     'L' => [0, 0, 0, $h],
     'R' => [$w, 0, $w, $h],
 ];
-
-// error_log(var_export($borders, 1));
 
 solve($birds, $t, $d);
