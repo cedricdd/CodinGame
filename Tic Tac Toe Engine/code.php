fd9fd20e6f690532d43199372ad2b2bc6cb05f23
cedricdd <c-d-d@hotmail.fr>
2025-12-18 19:07:08 +0100

    Update code.php

--- Tic Tac Toe Engine/code.php
+++ Tic Tac Toe Engine/code.php
@@ -1,106 +1,62 @@
 <?php
 
-const CHECK = [4, 0, 2, 6, 8, 1, 3, 5, 7];
 const WIN = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-function checkWinner(string $ticTacToe): bool {
-    foreach(WIN as [$i, $j, $k]) {
-        if($ticTacToe[$i] != '.' && $ticTacToe[$i] == $ticTacToe[$j] && $ticTacToe[$j] == $ticTacToe[$k]) return true;
+function simulate(string $grid, string $player):array {
+    global $engine;
+
+    //Check is a player has won
+    foreach(WIN as [$i1, $i2, $i3]) {
+        if($grid[$i1] == $grid[$i2] && $grid[$i2] == $grid[$i3] && $grid[$i3] != '.') {
+            if($grid[$i1] == 'X') return [1, 0, 0, $grid];
+            else return [0, 1, 0, $grid];
+        }
     }
 
-    return false;
+    if(strpos($grid, ".") === false) return [0, 0, 1, $grid]; //Grid is full, it's a draw
+    
+    if($grid == ".........") return [0, 0, 1, "...." . $player . "...."]; //Gris is completly empty, best is center
+
+    //Try to add based on cells value
+    foreach([4, 0, 2, 6, 8, 1, 3, 5, 7] as $index) {
+        if($grid[$index] != '.') continue;
+
+        $grid[$index] = $player;
+
+        //We want X to win
+        if($player == 'X') {
+            $result = simulate($grid, 'O');
+
+            //It's the first result or X will win  or it will be a draw
+            if(!isset($best) || ($result[0] > $best[0] || ($result[0] == $best[0] && $result[2] > $best[2]))) {
+                $best = [$result[0], $result[1], $result[2], $grid];
+            }
+        } //We want Y to win
+        else {
+            $result = simulate($grid, 'X');
+
+            //It's the first result or Y will win  or it will be a draw
+            if(!isset($best) || ($result[1] > $best[1] || ($result[1] == $best[1] && $result[2] > $best[2]))) {
+                $best = [$result[0], $result[1], $result[2], $grid];
+            }
+        }
+        
+        $grid[$index] = '.';
+    }
+
+    return $best;
 }
 
-$ticTacToe = "";
-
-$m = stream_get_line(STDIN, 1 + 1, "\n");
-$o = ($m == "X" ? "O" : "X");
+$engine = stream_get_line(STDIN, 1 + 1, "\n");
+$grid = "";
+$start = microtime(1);
 
 for ($i = 0; $i < 3; $i++) {
-    $ticTacToe .= stream_get_line(STDIN, 3 + 1, "\n");
+    $grid .= stream_get_line(STDIN, 3 + 1, "\n");
 }
 
-error_log("Next $m");
+[, , , $grid] = simulate($grid, $engine);
 
-//Game is over
-if(strpos($ticTacToe, ".") === false) exit(implode(PHP_EOL, str_split($ticTacToe, 3)));
+echo implode(PHP_EOL, str_split($grid, 3)) . PHP_EOL;
 
-//Someone already won
-if(checkWinner($ticTacToe)) exit(implode(PHP_EOL, str_split($ticTacToe, 3)));
-
-//Empty grid
-if($ticTacToe == str_repeat(".", 9)) exit(implode(PHP_EOL, str_split("....X....", 3)));
-
-//Player can win in 1 move
-foreach(CHECK as $i) {
-    if($ticTacToe[$i] == '.') {
-        $ticTacToe[$i] = $m;
-
-        if(checkWinner($ticTacToe)) exit(implode(PHP_EOL, str_split($ticTacToe, 3)));
-
-        $ticTacToe[$i] = '.';
-    }
-}
-
-//Adversary can win in 1 more
-foreach(CHECK as $i) {
-    if($ticTacToe[$i] == '.') {
-        $ticTacToe[$i] = $o;
-
-        if(checkWinner($ticTacToe)) {
-            $ticTacToe[$i] = $m;
-
-            exit(implode(PHP_EOL, str_split($ticTacToe, 3)));
-        }
-
-        $ticTacToe[$i] = '.';
-    }
-}
-
-$bestIndex = null;
-$bestBlock = 10;
-
-//Find the best move
-foreach(CHECK as $index) {
-    if($ticTacToe[$index] == '.') {
-        $ticTacToe2 = $ticTacToe;
-        $ticTacToe2[$index] = $m;
-        $win = [];
-
-        foreach(WIN as $indexWin => [$i, $j, $k]) {
-            $a = count_chars($ticTacToe2[$i] . $ticTacToe2[$j] . $ticTacToe2[$k], 1);
-            // error_log(var_export($a, 1));
-
-            if(($a[46] ?? 0) == 1 && ($a[ord($m)] ?? 0) == 2) $win[] = $indexWin;
-        }
-
-        if(count($win) == 2) exit(implode(PHP_EOL, str_split($ticTacToe2, 3)));
-        elseif(count($win) == 1) {
-            [$i, $j, $k] = WIN[array_pop($win)];
-
-            //The opp will block the win
-            if($ticTacToe2[$i] == '.') $ticTacToe2[$i] = $o;
-            if($ticTacToe2[$j] == '.') $ticTacToe2[$j] = $o;
-            if($ticTacToe2[$k] == '.') $ticTacToe2[$k] = $o;
-
-            $oppWin = 0;
-
-            foreach(WIN as [$i, $j, $k]) {
-                $a = count_chars($ticTacToe2[$i] . $ticTacToe2[$j] . $ticTacToe2[$k], 1);
-                // error_log(var_export($a, 1));
-
-                if(($a[46] ?? 0) == 1 && ($a[ord($o)] ?? 0) == 2) ++$oppWin;
-            }
-
-            error_log("at $index opp win is $oppWin");
-
-            if($oppWin < $bestBlock) {
-                $bestIndex = $index;
-                $bestBlock = $oppWin;
-            }
-        }
-    }
-}
-
-$ticTacToe[$bestIndex] = $m;
-echo implode(PHP_EOL, str_split($ticTacToe, 3));
+error_log(microtime(1) - $start);
