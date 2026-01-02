d3357f1b7c4fe292735364c86192ebf2d26a8fbf
cedricdd <c-d-d@hotmail.fr>
2026-01-02 17:49:09 +0100

    Update code.php

--- Simple Diff Tool/code.php
+++ Simple Diff Tool/code.php
@@ -8,36 +8,49 @@ fscanf(STDIN, "%s", $type);
 fscanf(STDIN, "%d", $nbLinesV1);
 for ($i = 0; $i < $nbLinesV1; $i++){
     $line = stream_get_line(STDIN, 200 + 1, "\n");
-    $original[sha1($line)] = $line;
+    $original[] = $line;
 }
 
 fscanf(STDIN, "%d", $nbLinesV2);
 for ($i = 0; $i < $nbLinesV2; $i++) {
     $line = stream_get_line(STDIN, 200 + 1, "\n");
-    $revised[sha1($line)] = $line;
+    $revised[] = $line;
 }
+
+$min = min($nbLinesV1, $nbLinesV2);
+$max = max($nbLinesV1, $nbLinesV2);
 
 $changes = [];
 
 if($type == "BY_NUMBER") {
-    reset($original);
-    reset($revised);
+    for($i = 0; $i < $min; ++$i) {
+        if($original[$i] != $revised[$i]) $changes[] = "CHANGE: " . $original[$i] . " ---> " . $revised[$i];
+    }
 
-    while(current($original) || current($revised)) {
-        if(!current($original)) $changes[] = "ADD: " . current($revised);
-        elseif(!current($revised)) $changes[] = "DELETE: " . current($original);
-        elseif(current($original) != current($revised)) $changes[] = "CHANGE: " . current($original) . " ---> " . current($revised);
-
-        next($original);
-        next($revised);
+    for($i = $min; $i < $max; ++$i) {
+        if(!isset($original[$i])) $changes[] = "ADD: " . $revised[$i];
+        else $changes[] = "DELETE: " . $original[$i];
     }
 } else {
-    foreach($original as $hash => $line) {
-        if(!isset($revised[$hash])) $changes[] = "DELETE: " . $line;
+    $hashes = [];
+
+    foreach($original as $index => $line) {
+        $hashes[sha1($line)] = $index;
     }
 
-    foreach($revised as $hash => $line) {
-        if(!isset($original[$hash])) $changes[] = "ADD: " . $line;
+    foreach($revised as $index => $line) {
+        $hash = sha1($line);
+
+        if(!isset($hashes[$hash])) $changes[] = "ADD: " . $line;
+        else {
+            if($hashes[$hash] != $index) $changes[] = "MOVE: " . $line . " @:" . ($hashes[$hash] + 1) . " >>> @:" . ($index + 1);
+
+            unset($hashes[$hash]);
+        }
+    }
+
+    foreach($hashes as $index) {
+        $changes[] = "DELETE: " . $original[$index];
     }
 }
 
