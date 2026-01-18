5f44d19d0188db0bee27271978bb00a19fc14b41
cedricdd <c-d-d@hotmail.fr>
2026-01-18 16:01:24 +0100

    Improve movement logic and symmetry handling
    Refactor movement logic and add symmetry generation for solutions.

--- Cubax Folding/code.php
+++ Cubax Folding/code.php
@@ -26,7 +26,6 @@ for($z = 0; $z < $N; ++$z) {
     for($y = 0; $y < $N; ++$y) {
         for($x = 0; $x < $N; ++$x) {
             $index = $z * $layerSize + $y * $N + $x;
-            // error_log("$x $y $z -- $index");
 
             $distances[$index] = abs($x - ($N - 1)) + abs($y - ($N - 1)) + abs($z - ($N - 1));
 
@@ -70,7 +69,20 @@ function solve(int $index, int $p, int $hashCube, string $solution) {
     $last = $solution[-1] ?? "";
 
     foreach($moves[$index][$size] ?? [] as $dir => [$index2, $hash]) {
-        if($last != $dir && ($hashCube & $hash) == 0) {
+        if($last == $dir) continue; //We need a 90°
+
+        /**
+         * On first turn we can only move U, R or F and on second turn we will always have two possibilities:
+         * - After U we can move R or F
+         * - After R we can move U or F
+         * - After F we can move U or R
+         * This gives us 6 possibilities, if we find a solution with one of the possibility we can "generate" 5 more solutions by using symmetry.
+         * So we only focus on 1 of the possibility to prune a lot of branches.
+         */
+        if($p == 0 && $dir != 'U') continue;
+        if($p == 1 && $dir != 'F') continue;
+
+        if(($hashCube & $hash) == 0) {
             solve($index2, $p + 1, $hashCube | $hash, $solution . $dir);
         }
     }
@@ -86,6 +98,24 @@ foreach($blocks as $i => $v) {
 }
 
 solve(0, 0, 0, "");
+
+//Base solution (first U then F) we group moves with their inverse
+$base = "UDFBRL";
+$symmetries = [
+    "UDRLFB", //First U then R
+    "FBRLUD", //First F then R
+    "FBUDRL", //First F then U
+    "RLUDFB", //First R then U
+    "RLFBUD", //First R then F
+];
+
+//Generate the symmetry solution
+foreach($solutions as $solution) {
+    foreach($symmetries as $symmetry) 
+        $solutions[] = strtr($solution, $base, $symmetry);
+}
+
+sort($solutions);
 
 echo implode(PHP_EOL, $solutions) . PHP_EOL;
 
