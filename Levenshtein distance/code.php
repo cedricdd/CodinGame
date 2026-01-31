391ea7971718a187d5c86532d41e149918e851d6
cedricdd <c-d-d@hotmail.fr>
2026-01-31 23:55:01 +0100

    Refactor Levenshtein distance calculation method
    Refactored the Levenshtein distance calculation from a recursive approach to a dynamic programming approach for improved performance.

--- Levenshtein distance/code.php
+++ Levenshtein distance/code.php
@@ -1,24 +1,5 @@
 <?php
 
-function solve(int $i1, int $i2): int {
-    global $word1, $word2, $size1, $size2;
-    static $history = [];
-
-    if(isset($history[$i1][$i2])) return $history[$i1][$i2];
-
-    if($i1 == $size1) return $history[$i1][$i2] = $size2 - $i2;
-    if($i2 == $size2) return $history[$i1][$i2] = $size1 - $i1;
-
-    //Identical letters
-    if($word1[$i1] == $word2[$i2]) return $history[$i1][$i2] = solve($i1 + 1, $i2 + 1);
-
-    return $history[$i1][$i2] = 1 + min(
-        solve($i1 + 1, $i2),     // delete
-        solve($i1, $i2 + 1),     // insert
-        solve($i1 + 1, $i2 + 1)  // replace
-    );
-}
-
 $start = microtime(1);
 
 $word1 = stream_get_line(STDIN, 256 + 1, "\n");
@@ -27,6 +8,22 @@ $word2 = stream_get_line(STDIN, 256 + 1, "\n");
 $size1 = strlen($word1);
 $size2 = strlen($word2);
 
-echo solve(0, 0) . PHP_EOL;
+$temp = array_fill(0, $size2, 0);
+$dp = array_fill(0, $size1, $temp);
+
+for($i = 0; $i <= $size1; ++$i) $dp[$i][0] = $i;
+for($j = 0; $j <= $size2; ++$j) $dp[0][$j] = $j;
+
+for($i = 1; $i <= $size1; ++$i) {
+    for($j = 1; $j <= $size2; ++$j) {
+        $dp[$i][$j] = min(
+            1 + $dp[$i][$j - 1],
+            1 + $dp[$i - 1][$j],
+            $dp[$i - 1][$j - 1] + ($word1[$i - 1] == $word2[$j - 1] ? 0 : 1)
+        );
+    }
+}
+
+echo $dp[$size1][$size2] . PHP_EOL;
 
 error_log(microtime(1) - $start);
