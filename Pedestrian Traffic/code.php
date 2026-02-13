60fb170fa551a29b94a7a473d602e9107f5d61b7
cedricdd <c-d-d@hotmail.fr>
2026-02-13 01:03:01 +0100

    Refactor pedestrian movement logic in code.php

--- Pedestrian Traffic/code.php
+++ Pedestrian Traffic/code.php
@@ -7,126 +7,96 @@ $blank = str_repeat("o", $n);
 
 error_log($path[0] . "\n" . $path[1] . "\n");
 
-$turn = 1;
+$turn = 0;
 
 while(true) {
-    $updatedPath = [$blank, $blank];
-    $moves = [];
-    $pedestrians = [];
+    if($path[0] == $blank && $path[1] == $blank) break;
+
+    $moves = [1 => [], 2 => [], 3 => []];
 
     for($i = 0; $i < $n; ++$i) {
-        if($path[0][$i] != 'o') {
-            $pedestrians[0][$i] = 1;
+        if($path[0][$i] == 'R') {
+            //The first move is going right
+            $moves[1][] = [0, $i, 0, $i + 1];
+        } elseif($path[0][$i] == 'L') {
+            //The first move is going down
+            $moves[2][] = [0, $i, 1, $i];
 
-            if($path[0][$i] == 'R') {
-                //The first move is going right
-                $moves[0][$i + 1][] = [0, $i, 1];
-            } elseif($path[0][$i] == 'L') {
-                //The first move is going down
-                $moves[1][$i][] = [0, $i, 2];
-
-                //The fallback is going left
-                $moves[0][$i - 1][] = [0, $i, 3];
-            }
+            //The fallback is going left
+            $moves[3][] = [0, $i, 0, $i - 1];
         }
 
-        if($path[1][$i] != 'o') {
-            $pedestrians[1][$i] = 1;
+        if($path[1][$i] == 'L') {
+            //The first move is going left
+            $moves[1][] = [1, $i, 1, $i - 1];
+        } elseif($path[1][$i] == 'R') {
+            //The first move is going up
+            $moves[2][] = [1, $i, 0, $i];
 
-            if($path[1][$i] == 'L') {
-                //The first move is going left
-                $moves[1][$i - 1][] = [1, $i, 1];
-            } elseif($path[1][$i] == 'R') {
-                //The first move is going up
-                $moves[0][$i][] = [1, $i, 2];
-
-                //The fallback is going right
-                $moves[1][$i + 1][] = [1, $i, 3];
-            }
+            //The fallback is going right
+            $moves[3][] = [1, $i, 1, $i + 1];
         }
     }
 
     // error_log(var_export($moves, 1));
 
-    //Move everything with priority 1
-    foreach($moves as $line => $list) {
-        foreach($list as $index => $candidates) {
-            foreach($candidates as [$cLine, $cIndex, $priority]) {
-                if($priority == 1) {
-                    if($index >= 0 && $index < $n) $updatedPath[$line][$index] = $path[$cLine][$cIndex];
+    while(true) {
 
-                    unset($moves[$line][$index]);
-                    unset($pedestrians[$cLine][$cIndex]);
+        for($i = 1; $i <= 3; ++$i) {
+            $moved = false;
 
-                    continue 2;
+            foreach($moves[$i] as $index => [$l1, $i1, $l2, $i2]) {
+                //We are moving out of the path
+                if($i2 < 0 || $i2 >= $n) {
+                    $path[$l1][$i1] = 'o';
+                    unset($moves[$i][$index]);
+                    $moved = true;
+                }
+                //Nobody can move there anymore this turn
+                elseif($path[$l2][$i2] == 'l' || $path[$l2][$i2] == 'r') {
+                    unset($moves[$i][$index]);
+                }
+                //The person there has already moved
+                elseif($path[$l1][$i1] != 'L' && $path[$l1][$i1] != 'R') {
+                    unset($moves[$i][$index]);
+                }
+                //The move is possible, the spot is open
+                elseif($path[$l2][$i2] == 'o') {
+                    $path[$l2][$i2] = strtolower($path[$l1][$i1]);
+                    $path[$l1][$i1] = 'o';
+                    unset($moves[$i][$index]);
+                    $moved = true;
+                }
+                //Swapping up and down
+                elseif($i == 2) {
+                    if($path[$l1][$i1] == 'L' && $path[$l2][$i2] == 'R') {
+                        $path[$l1][$i1] = 'r';
+                        $path[$l2][$i2] = 'l';
+
+                        unset($moves[$i][$index]);
+                        $moved = true;
+                    } elseif($path[$l1][$i1] == 'R' && $path[$l2][$i2] == 'L') {
+                        $path[$l1][$i1] = 'l';
+                        $path[$l2][$i2] = 'r';
+                        
+                        unset($moves[$i][$index]);
+                        $moved = true;
+                    }
                 }
             }
+
+            if($moved) continue 2;
         }
+
+        break;
     }
 
-    error_log("Step 1:");
-    error_log($updatedPath[0] . "\n" . $updatedPath[1] . "\n");
-
-    //Move everything with priority 2
-    foreach($moves as $line => $list) {
-        foreach($list as $index => $candidates) {
-            foreach($candidates as [$cLine, $cIndex, $priority]) {
-                if($priority == 2 && isset($pedestrians[$cLine][$cIndex])) {
-                    // error_log("level 2 moving $cLine $cIndex to $line $index");
-
-                    $updatedPath[$line][$index] = $path[$cLine][$cIndex];
-
-                    unset($moves[$line][$index]);
-                    unset($pedestrians[$cLine][$cIndex]);
-
-                    $moved[$cLine][$cIndex] = 1;
-
-                    continue 2;
-                }
-            }
-        }
-    }
-
-    error_log("Step 2:");
-    error_log($updatedPath[0] . "\n" . $updatedPath[1] . "\n");
-
-    //Move everything with priority 3
-    foreach($moves as $line => $list) {
-        foreach($list as $index => $candidates) {
-            foreach($candidates as [$cLine, $cIndex, $priority]) {
-                if($priority == 3 && isset($pedestrians[$cLine][$cIndex])) {
-                    // error_log("level 3 moving $cLine $cIndex to $line $index");
-
-                    if($index >= 0 && $index < $n) $updatedPath[$line][$index] = $path[$cLine][$cIndex];
-
-                    unset($pedestrians[$cLine][$cIndex]);
-
-                    continue 2;
-                }
-            }
-        }
-    }
-
-    error_log("Step 3:");
-    error_log($updatedPath[0] . "\n" . $updatedPath[1] . "\n");
-
-    //The ones that can't move this turn
-    foreach($pedestrians as $line => $list) {
-        foreach($list as $index => $_) {
-            $updatedPath[$line][$index] = $path[$line][$index];
-        }
-    }
-
-    $path = $updatedPath;
-
-    error_log("Step 4:");
     error_log($path[0] . "\n" . $path[1] . "\n");
 
-    // die();
+    $path[0] = strtr($path[0], "lr", "LR");
+    $path[1] = strtr($path[1], "lr", "LR");
 
-    if($path[0] == $blank && $path[1] == $blank) break;
-
-    if(++$turn == 20) break;
+    if(++$turn > 1001) exit("Congestion");
 }
 
 echo $turn . PHP_EOL;
