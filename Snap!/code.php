4cb86349c09768afdff1b932ebacb7c662ffe68f
cedricdd <c-d-d@hotmail.fr>
2026-02-18 19:20:27 +0100

    Refactor card handling in Snap game logic

--- Snap!/code.php
+++ Snap!/code.php
@@ -1,22 +1,24 @@
 <?php
 
+const PRECEDENCE = ['C' => 1, 'D' => 2, 'H' => 3, 'S' => 4];
+
 $p0 = [];
 $p1 = [];
 
 for ($i = trim(fgets(STDIN)); $i--;) {
     fscanf(STDIN, "%s", $card);
 
-    preg_match("/(.+)./", $card, $match);
+    preg_match("/(.+)(.)/", $card, $match);
 
-    $p0[] = $match[1];
+    $p0[] = [$match[1], $match[2]];
 }
 
 for ($i = trim(fgets(STDIN)); $i--;) {
     fscanf(STDIN, "%s", $card);
 
-    preg_match("/(.+)./", $card, $match);
+    preg_match("/(.+)(.)/", $card, $match);
 
-    $p1[] = $match[1];
+    $p1[] = [$match[1], $match[2]];
 }
 
 if(count($p0) + count($p1) == 0) exit("Go get some cards!");
@@ -27,14 +29,15 @@ $pile = [];
 $index = 0;
 
 while(true) {
-    $card = array_shift(${"p".$index});
-
-    array_unshift($pile, $card); //Add the card to the deck
+    array_unshift($pile, array_shift(${"p".$index})); //Add the card to the deck
 
     //We have a snap
-    if(count($pile) > 1 && $pile[0] == $pile[1]) {
+    if(count($pile) > 1 && $pile[0][0] == $pile[1][0]) {
+        if(PRECEDENCE[$pile[0][1]] < PRECEDENCE[$pile[1][1]]) $index ^= 1; //It's the other player who won the snap
+            
+        ${"p".$index} = array_merge(${"p".$index}, array_reverse($pile)); //Add the pile to the player's cards
 
-        ${"p".$index} = array_merge(${"p".$index}, $pile); //Add the pile to the player's cards
+        if(count(${"p". ($index ^ 1)}) == 0) exit("Winner: Player " . ($index + 1) . "\n" . count(${"p" . $index}));
 
         $pile = [array_shift(${"p".$index})];
     }
