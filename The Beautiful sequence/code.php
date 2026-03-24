a6601f9ef9000a4353c349b5a17be5d33c1e398b
cedricdd <c-d-d@hotmail.fr>
2026-03-24 21:26:28 +0100

    O(N²) => O(N)

--- The Beautiful sequence/code.php
+++ The Beautiful sequence/code.php
@@ -3,21 +3,36 @@
 fscanf(STDIN, "%d", $n);
 
 $values = explode(' ', trim(fgets(STDIN)));
+
+//Arrays to store boundaries
+$left = array_fill(0, $n, -1);
+$right = array_fill(0, $n, $n);
+
+$stack = [];
+
+//Compute left boundaries
+for ($i = 0; $i < $n; $i++) {
+    while (!empty($stack) && $values[end($stack)] >= $values[$i]) array_pop($stack);
+
+    $left[$i] = empty($stack) ? -1 : end($stack);
+    $stack[] = $i;
+}
+
+$stack = [];
+
+//Compute right boundaries
+for ($i = $n - 1; $i >= 0; $i--) {
+    while (!empty($stack) && $values[end($stack)] >= $values[$i]) array_pop($stack);
+    
+    $right[$i] = empty($stack) ? $n : end($stack);
+    $stack[] = $i;
+}
+
 $result = 0;
 
-for($i = 0; $i < $n; ++$i) {
-    $count = 1; 
-
-    for($j = $i - 1; $j >= 0; --$j) {
-        if($values[$j] >= $values[$i]) ++$count;
-        else break;
-    }
-    for($j = $i + 1; $j < $n; ++$j) {
-        if($values[$j] >= $values[$i]) ++$count;
-        else break;
-    }
-
-    if($count * $values[$i] > $result) $result = $count * $values[$i];
+for ($i = 0; $i < $n; $i++) {
+    $length = $right[$i] - $left[$i] - 1; //Length of the subsequence with the element $i being the smallest element
+    $result = max($result, $values[$i] * $length);
 }
 
 echo $result . PHP_EOL;
