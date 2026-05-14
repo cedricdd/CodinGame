401c77e0d27108a42e191929918618c7e0e8c4ea
cedricdd <c-d-d@hotmail.fr>
2026-05-14 23:20:54 +0200

    Update code.php

--- Rocket mice/code.php
+++ Rocket mice/code.php
@@ -25,27 +25,26 @@ $turns = [];
 for ($i = 0; $i < $players; $i++) {
     fscanf(STDIN, "%d %d", $rX, $rY);
 
-    error_log("Rocket at $rX $rY");
-
     $rockets[$rY][$rX] = $i;
     $scores[$i] = 0;
 }
 
+//Get all the doors where new animals enter the grid
 for ($i = 0; $i < $doorCount; $i++) {
     fscanf(STDIN, "%d %s", $coord, $wall);
 
     switch($wall) {
         case 'N':
-            $doors[] = [$coord, 0, 'S'];
+            $doors[] = [$coord, -1, 'S'];
             break;
         case 'S':
-            $doors[] = [$coord, $height - 1, 'N'];
+            $doors[] = [$coord, $height, 'N'];
             break;
         case 'E':
-            $doors[] = [$width - 1, $coord, 'W'];
+            $doors[] = [$width, $coord, 'W'];
             break;
         case 'W':
-            $doors[] = [0, $coord, 'E'];
+            $doors[] = [-1, $coord, 'E'];
             break;
         default: exit("Invalid Wall Info");
     }
@@ -54,40 +53,28 @@ for ($i = 0; $i < $doorCount; $i++) {
 $cats = [];
 $mouses = [];
 
-for ($i = 1; $i <= $turnCount; $i++) {
-    fscanf(STDIN, "%d %d %s", $tX, $tY, $direction);
-
-    $historyTurns[$i] = [$tX, $tY, $direction];
-    
+for ($i = 0; ;$i++) {
+    //An animal is produced behind each door
     foreach($doors as [$x, $y, $d]) {
-        $d = $turns[$y][$x] ?? $d;
-
-        if($i % 10 == 0) $cats[$y][$x][$d] = ($cats[$y][$x][$d] ?? 0) + 1;
+        if(($i + 1) % 10 == 0) $cats[$y][$x][$d] = ($cats[$y][$x][$d] ?? 0) + 1;
         else $mouses[$y][$x][$d] = ($mouses[$y][$x][$d] ?? 0) + 1;
     }
-
-    // error_log(var_export($mouses, 1));
 
     $catsAfterMove = [];
     $mousesAfterMove = [];
 
+    //All mouses move forward one square
     foreach($mouses as $y => $i1) {
         foreach($i1 as $x => $i2) {
             foreach($i2 as $d => $count) {
-                $d = $turns[$y][$x] ?? $d;
-
                 $x2 = $x + MOVES[$d][0];
                 $y2 = $y + MOVES[$d][1];
 
-                if($i == 1 || $i == 2) error_log("we have $count mouse at $x $y with direction $d moving to $x2 $y2");
-
                 if(isset($pits[$y2][$x2])) continue; //Mouse is dying in a pit
-                elseif(isset($rockets[$y2][$x2])) {
-                    // error_log("$count mouses reached rocket at $x2 $y2");
-                    $scores[$rockets[$y2][$x2]] += $count;
-                }
+                elseif(isset($rockets[$y2][$x2])) $scores[$rockets[$y2][$x2]] += $count; //Mouse enters a rocket
                 //The mouse won't get eaten by a cat during the move
                 elseif(!isset($cats[$y2][$x2][INV_MOVE[$d]])) {
+                    //Check if the mouse is redirected by a wall
                     if($d == 'W' && $x2 == 0) $d = 'S';
                     elseif($d == 'S' && $y2 == $height - 1) $d = 'E';
                     elseif($d == 'E' && $x2 == $width - 1) $d = 'N';
@@ -99,21 +86,19 @@ for ($i = 1; $i <= $turnCount; $i++) {
         }
     }
 
+    //All cats move forward one square
     foreach($cats as $y => $i1) {
         foreach($i1 as $x => $i2) {
             foreach($i2 as $d => $count) {
-                $d = $turns[$y][$x] ?? $d;
-
                 $x2 = $x + MOVES[$d][0];
                 $y2 = $y + MOVES[$d][1];
 
-                // error_log("we have $count cat at $x $y with direction $d moving to $x2 $y2");
-
                 if(isset($pits[$y2][$x2])) continue; //Cat is dying in a pit
-                elseif(isset($rockets[$y2][$x2])) $scores[$rockets[$y2][$x2]] = max(0, $scores[$rockets[$y2][$x2]] - $count * 10);
+                elseif(isset($rockets[$y2][$x2])) $scores[$rockets[$y2][$x2]] = max(0, $scores[$rockets[$y2][$x2]] - $count * 10); //Cat enters a rocket
                 else {
                     unset($mousesAfterMove[$y2][$x2]); //Cat eats all the mouse on that position
 
+                    //Check if the cat is redirected by a wall
                     if($d == 'W' && $x2 == 0) $d = 'S';
                     elseif($d == 'S' && $y2 == $height - 1) $d = 'E';
                     elseif($d == 'E' && $x2 == $width - 1) $d = 'N';
@@ -125,25 +110,32 @@ for ($i = 1; $i <= $turnCount; $i++) {
         }
     }
 
-    // error_log(var_export($mousesAfterMove, 1));
+    if($i == $turnCount) break;
+    
+    //All animals on an arrow are redirected
+    foreach($turns as [$x, $y, $d]) {
+        if(isset($mousesAfterMove[$y][$x])) {
+            $count = 0;
 
-    //Arrows cannot be placed onto another arrow, a rocket, or a pit
-    if(!isset($pits[$tY][$tX]) && !isset($rockets[$tY][$tX]) && !isset($turns[$tY][$tX])) {
-        $turns[$tY][$tX] = $direction;
+            foreach($mousesAfterMove[$y][$x] as $c) $count += $c;
 
-        error_log("Turn $i - at $tX $tY turns to $direction");
+            $mousesAfterMove[$y][$x] = [$d => $count];
+        }
+
+        if(isset($catsAfterMove[$y][$x])) {
+            $count = 0;
+
+            foreach($catsAfterMove[$y][$x] as $c) $count += $c;
+
+            $catsAfterMove[$y][$x] = [$d => $count];
+        }
     }
 
-    if(isset($historyTurns[$i - ($players * 3)])) {
-        // error_log(var_export($historyTurns[$i - ($players * 3)], 1));
+    unset($turns[$i - ($players * 3)]); //Each player can only have 3 arrows at the same time
 
-        [$x, $y, ] = $historyTurns[$i - ($players * 3)];
+    fscanf(STDIN, "%d %d %s", $tX, $tY, $direction);
 
-        error_log("we need to remove turn at $x $y");
-
-        unset($turns[$y][$x]);
-    }
-
+    $turns[$i] = [$tX, $tY, $direction];
     $mouses = $mousesAfterMove;
     $cats = $catsAfterMove;
 }
